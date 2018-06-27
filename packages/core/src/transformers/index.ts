9f1039487659b6c3c130456687e0a1f5002b757b
Tanguy Antoine <antoine.tanguy@apizi.com>
2018-06-27 14:31:29 +0200

    Add typing to transformers

--- packages/core/src/transformers/index.ts
+++ packages/core/src/transformers/index.ts
@@ -1,8 +1,8 @@
 import * as ts from 'typescript'
 import decoratorTransformers from './decorator-transformer'
 
-export function Transformers() {
-  return [decoratorTransformers()]
+export function Transformers(options): ts.TransformerFactory<ts.SourceFile>[] {
+  return [decoratorTransformers(options)]
 }
 
 export default Transformers
