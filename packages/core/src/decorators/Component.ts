3309ea527eac067c55892c8b8dc0b25c957cf985
Tanguy Antoine <antoine.tanguy@apizi.com>
2018-06-27 14:30:54 +0200

    Deprecated BearerComponent

--- packages/core/src/decorators/Component.ts
+++ packages/core/src/decorators/Component.ts
@@ -2,6 +2,10 @@
 export function BearerComponent<T extends { new (...args: any[]): {} }>(
   constructor: T
 ) {
+  console.warn(
+    '[BEARER]',
+    'Please remove BearerComponent decorator and use tranformers'
+  )
   // Do not remove : will be replace in the front.
   constructor.prototype['SCENARIO_ID'] = 'BEARER_SCENARIO_ID'
   return constructor
