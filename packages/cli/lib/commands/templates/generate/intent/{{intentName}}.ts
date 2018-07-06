5f3cfcc9a966635ed95c57e2b1a3f747a8b4406e
Cédric Fabianski <cfabianski@me.com>
2018-07-06 14:08:05 +0200

    Add type to context

--- packages/cli/lib/commands/templates/generate/intent/{{intentName}}.ts
+++ packages/cli/lib/commands/templates/generate/intent/{{intentName}}.ts
@@ -1,10 +1,15 @@
 import { {{intentType}} } from '@bearer/intents'
 
+type TContext = {
+  accessToken: string,
+  [key: string]: any
+}
+
 export default class {{intentName}}Intent {
   static intentName: string = '{{intentName}}'
   static intentType: any = {{intentType}}
 
-  static action(context: any, params: any, callback: (params: any) => void) {
+  static action(context: TContext, params: any, callback: (params: any) => void) {
     //... your code goes here
     // sample code for type GetCollection
     // callback({ collection: ["Christopher Robin", "Kanga", "Tigger", "heffalump", "kessie"] })
