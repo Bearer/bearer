6be0483bc3da0c8649799bed3953dc1dd4a7bf96
Tanguy Antoine <antoine@bearer.sh>
2018-07-12 15:37:51 +0200

    Add update stories with kinds

--- packages/ui/src/components/Alert/Alert.stories.tsx
+++ packages/ui/src/components/Alert/Alert.stories.tsx
@@ -1,10 +1,20 @@
-import {
-  storiesOf
-} from '@storybook/html';
+import { storiesOf } from '@storybook/html'
 
-storiesOf('Alert', module)
-  .add('Example', () => {
-    return (
-      `<bearer-alert>My alert message 2</bearer-alert>`
+storiesOf('Alert', module).add('Kind', () => {
+  const kinds = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark'
+  ]
+  return kinds
+    .map(
+      kind =>
+        `<bearer-alert kind="${kind}">Alert message: ${kind}</bearer-alert>`
     )
-  })
+    .join('<br />')
+})
--- packages/ui/src/components/Button/Button.stories.tsx
+++ packages/ui/src/components/Button/Button.stories.tsx
@@ -1,10 +1,20 @@
-import {
-  storiesOf
-} from '@storybook/html';
+import { storiesOf } from '@storybook/html'
 
-storiesOf('Button', module)
-  .add('Example', () => {
-    return (
-      `<bearer-button>Sponge bob</bearer-button>`
+storiesOf('Button', module).add('kind', () => {
+  const kinds = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark'
+  ]
+  return kinds
+    .map(
+      kind =>
+        `<div><bearer-button kind="${kind}">Button: ${kind}</bearer-button></div>`
     )
-  })
+    .join('<br />')
+})
