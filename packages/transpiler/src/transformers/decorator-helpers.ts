b76b01fd28577dc8fbebfd61d0f6e5170a0d6157
Tanguy Antoine <antoine@bearer.sh>
2018-07-19 16:37:44 +0200

    Add missing helpers

--- packages/transpiler/src/transformers/decorator-helpers.ts
+++ packages/transpiler/src/transformers/decorator-helpers.ts
@@ -12,9 +12,11 @@ export function name(node: ts.Decorator): string {
   }
   return name
 }
+
 export function hasName(node: ts.Decorator, tsDecoratorName: string) {
   return name(node) === tsDecoratorName
 }
+
 export function classDecoratedWithName(
   node: ts.ClassDeclaration,
   decoratorName: string
@@ -27,6 +29,49 @@ export function classDecoratedWithName(
     }
   })
   return hasComponentDecorator
+}
+
+export function hasPropDecoratedWithName(
+  classNode: ts.ClassDeclaration,
+  decoratorName: string
+): boolean {
+  return Boolean(propDecoratedWithName(classNode, decoratorName).length)
+}
+
+export function propDecoratedWithName(
+  node: ts.ClassDeclaration,
+  decoratorName: string
+): Array<ts.PropertyDeclaration | null> {
+  const props: Array<ts.PropertyDeclaration | null> = []
+  ts.forEachChild(node, node => {
+    if (
+      ts.isPropertyDeclaration(node) &&
+      hasDecoratorNamed(node as ts.PropertyDeclaration, decoratorName)
+    ) {
+      props.push(node)
+    }
+  })
+  return props
+}
+
+function hasDecoratorNamed(
+  propDeclaration: ts.PropertyDeclaration,
+  name: string
+): boolean {
+  let has = false
+  ts.forEachChild(propDeclaration, anode => {
+    if (ts.isDecorator(anode)) {
+      ts.forEachChild(anode, node => {
+        if (
+          ts.isCallExpression(node) &&
+          node.expression['escapedText'] === name
+        ) {
+          has = true
+        }
+      })
+    }
+  })
+  return has
 }
 
 export function using(node: ts.Node, decoratorName: string): boolean {
