ba1291635cfaf0f62c620fb2b9e6d0b84f6e1770
Tanguy Antoine <antoine@bearer.sh>
2018-07-31 15:24:02 +0200

    test(transpiler): mark as todo edge cases
    We don't have time for the moment to implement these 2 use cases

--- packages/transpiler/__test__/__fixtures__/unit/bearer-state-injector/do-not-rewatch-property.tsx
+++ packages/transpiler/__test__/__fixtures__/unit/bearer-state-injector/do-not-rewatch-property.tsx
@@ -3,6 +3,7 @@ import { BearerState, Watch } from '@bearer/core'
 class UpdateExistingPropertyWatcher {
   @BearerState() pullRequests: Array<any> = []
 
+  // TODO: handle this use case
   @Watch('pullRequests')
   pullRequestsChangeHandler(newValue, oldValue) {
     console.log('Prepend stuff')
--- packages/transpiler/__test__/__fixtures__/unit/bearer-state-injector/does-not-re-add-component-lifecycle-methods.tsx
+++ packages/transpiler/__test__/__fixtures__/unit/bearer-state-injector/does-not-re-add-component-lifecycle-methods.tsx
@@ -3,6 +3,8 @@ import { BearerState } from '@bearer/core'
 class UpdateExistingComponentLifecycleMethods {
   @BearerState() pullRequests: Array<any> = []
 
+  // TODO: handle these use cases
+
   componentWillLoad() {
     console.log('componentWillLoad')
   }
