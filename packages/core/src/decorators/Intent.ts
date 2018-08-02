c87a9db26c40e6ae7569fdbd98ce61cf15d3670f
Tanguy Antoine <antoine@bearer.sh>
2018-08-02 14:26:04 +0200

    refactor(core): rewrite intents with with new intent decorator feature

--- packages/core/src/decorators/Intent.ts
+++ packages/core/src/decorators/Intent.ts
@@ -49,7 +49,7 @@ export const IntentSaved = 'BearerStateSaved'
 export const BearerStateSavedEvent = 'bearer:StateSaved'
 
 /**
- * Intents
+ * Intent
  */
 
 // Usage
@@ -59,21 +59,53 @@ export const BearerStateSavedEvent = 'bearer:StateSaved'
 export function Intent(intentName: string, type: IntentType = IntentType.GetCollection): IDecorator {
   return function(target: BearerComponent, key: string): void {
     const getter = (): BearerFetch => {
-      return function(this: BearerComponent, params = {}, init = {}) {
+      return function(this: BearerComponent, params = {}) {
         // NOTE: here we have to use target. Not sure why
         const scenarioId = target.SCENARIO_ID
         if (!scenarioId) {
           return missingScenarioId()
         }
+
         const intent = intentRequest<TFetchBearerResult>({
           intentName,
           scenarioId,
           [setupId]: retrieveSetupId(target)
         })
+
+        // prepare params and body
+
         const referenceId = retrieveReferenceId(this)
+        if (type === IntentType.RetrieveState && !referenceId) {
+          return missingReferenceId()
+        }
+
+        const { body, ...queryParams } = params
         const baseQuery = referenceId ? { referenceId } : {}
-        const promise: Promise<TFetchBearerResult> = intent.apply(null, [{ ...params, ...baseQuery }, init])
-        return IntentPromise(promise)
+        const query = { ...baseQuery, ...queryParams }
+        const InitMapper = {
+          [IntentType.SaveState]: { method: 'PUT', body: JSON.stringify(body) }
+        }
+        const init = InitMapper[type] || {}
+
+        // Build promise
+        const promise: Promise<TFetchBearerData> = IntentPromise(intent(query, init))
+
+        if (type === IntentType.SaveState) {
+          // we want to trigger special event if it is a SaveState intent
+          return new Promise((resolve, reject) => {
+            // It does not make sense to use collection here.
+            promise
+              .then((payload: TEventPayload) => {
+                if (this.el) {
+                  this.el.dispatchEvent(new CustomEvent(BearerStateSavedEvent, { detail: payload }))
+                }
+                resolve(payload)
+                return payload
+              })
+              .catch(reject)
+          })
+        }
+        return promise
       }
     }
 
@@ -90,96 +122,22 @@ type TEventPayload = {
 // or
 // @SaveStateIntent(IntentType.GetResource ) propertyName: BearerFetch
 export function SaveStateIntent(): IDecorator {
-  return function(target: BearerComponent, key: string): void {
-    const getter = (): BearerFetch => {
-      return function(this: BearerComponent, params: { body?: any; [key: string]: any } = {}, init: Object = {}) {
-        const scenarioId = this.SCENARIO_ID
-        if (!scenarioId) {
-          return missingScenarioId()
-        }
-
-        const { body, ...query } = params
-        const intent = intentRequest<TFetchBearerResult>({
-          intentName: IntentNames.SaveState,
-          scenarioId,
-          [setupId]: retrieveSetupId(this)
-        })
-        const referenceId = retrieveReferenceId(this)
-        const baseQuery = referenceId ? { referenceId } : {}
-        const promise: Promise<TFetchBearerResult> = intent.apply(null, [
-          { ...query, ...baseQuery },
-          { ...init, method: 'PUT', body: JSON.stringify(body) }
-        ])
-
-        return new Promise((resolve, reject) => {
-          // It does not make sense to use collection here.
-          IntentPromise(promise)
-            .then((payload: TEventPayload) => {
-              if (this.el) {
-                this.el.dispatchEvent(new CustomEvent(BearerStateSavedEvent, { detail: payload }))
-              }
-              resolve(payload)
-              return payload
-            })
-            .catch(reject)
-        })
-      }
-    }
-    defineIntentProp(target, key, getter)
-  }
+  console.warn(
+    'SaveStateIntent decorator is deprecated, please use @Intent("INTENT_NAME", IntentType.SaveState) instead'
+  )
+  return Intent(IntentNames.SaveState, IntentType.SaveState)
 }
 
 // Usage
 // @RetrieveStateIntent() propertyName: BearerFetch
 // or
 // @RetrieveStateIntent(IntentType.GetResource ) propertyName: BearerFetch
-export function RetrieveStateIntent(type: IntentType = IntentType.GetCollection): IDecorator {
-  return function(target: BearerComponent, key: string): void {
-    const getter = (): BearerFetch => {
-      return function(this: BearerComponent, params: { body?: any; [key: string]: any } = {}, init: Object = {}) {
-        const scenarioId = this.SCENARIO_ID
-        if (!scenarioId) {
-          return missingScenarioId()
-        }
-
-        const referenceId = retrieveReferenceId(this)
-        if (!referenceId) {
-          return missingReferenceId()
-        }
-
-        const { body, ...query } = params
-        const intent = intentRequest<TFetchBearerResult>({
-          intentName: IntentNames.RetrieveState,
-          scenarioId,
-          [setupId]: retrieveSetupId(this)
-        })
-        const promise: Promise<TFetchBearerResult> = intent.apply(null, [
-          { ...query, referenceId },
-          { ...init, method: 'PUT', body: JSON.stringify(body) }
-        ])
-        return IntentPromise(promise)
-      }
-    }
-
-    defineIntentProp(target, key, getter)
-  }
+export function RetrieveStateIntent(_type: IntentType = IntentType.GetCollection): IDecorator {
+  console.warn(
+    'RetrieveStateIntent decorator is deprecated, please use @Intent("INTENT_NAME", IntentType.RetrieveState) instead'
+  )
+  return Intent(IntentNames.RetrieveState, IntentType.RetrieveState)
 }
-
-// Common
-// => reject if setupId/scenarioId/integrationId/query params is missing
-//
-
-// Intent
-// => pass setupId/scenarioId/integrationId/query params
-// => pass query params
-
-// SaveStateIntent
-// => pass setupId/scenarioId/integrationId/query params
-// => trigger Save
-
-// RetrieveStateIntent
-// => do not perform if no referenceId
-// => pass setupId/scenarioId/integrationId/query params
 
 export function IntentPromise(promise: Promise<TFetchBearerResult>): Promise<TFetchBearerData> {
   return new Promise((resolve, reject) => {
@@ -208,15 +166,15 @@ function missingReferenceId(): Promise<any> {
   return Promise.reject(new BearerMissingReferenceId())
 }
 
-function retrieveSetupId(target: BearerComponent) {
+function retrieveSetupId(target: BearerComponent): string {
   return target.setupId || (target[BearerContext] && target[BearerContext][setupId])
 }
 
-function retrieveReferenceId(target: BearerComponent) {
+function retrieveReferenceId(target: BearerComponent): string {
   return target.referenceId
 }
 
-function defineIntentProp(target: BearerComponent, key: string, getter: any) {
+function defineIntentProp(target: BearerComponent, key: string, getter: any): void {
   const setter = () => {}
 
   if (delete target[key]) {
