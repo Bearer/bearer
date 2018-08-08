ae09b38a342ec17ed37a4dd3c61f7ee1eb2685a9
Tanguy Antoine <antoine@bearer.sh>
2018-08-08 11:07:10 +0200

    refactor(cli): remove useless async

--- packages/cli/src/BaseLegacyCommand.ts
+++ packages/cli/src/BaseLegacyCommand.ts
@@ -3,7 +3,7 @@ import { spawn } from 'child_process'
 import * as path from 'path'
 
 export default abstract class extends Command {
-  async runLegacy(cmdArgs: any[]) {
+  runLegacy(cmdArgs: any[]) {
     const cliEntry = path.join(__dirname, '../node_modules/@bearer/bearer-cli/dist/bin/index.js')
     this.debug('Legacy command arguments', JSON.stringify(cmdArgs))
     spawn('node', [cliEntry, ...cmdArgs], { stdio: 'inherit' })
