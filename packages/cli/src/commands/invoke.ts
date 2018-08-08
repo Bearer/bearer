3b572dbe2bbf010b9b68a964aa773263beacc215
Tanguy Antoine <antoine@bearer.sh>
2018-08-08 10:47:31 +0200

    refactor(cli): introduce invoke command

---
+++ packages/cli/src/commands/invoke.ts
@@ -0,0 +1,23 @@
+import { flags } from '@oclif/command'
+
+import BaseLegacyCommand from '../BaseLegacyCommand'
+
+export default class Invoke extends BaseLegacyCommand {
+  static description = 'Invoke Intent locally'
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    path: flags.string({ char: 'p' })
+  }
+
+  static args = [{ name: 'Intent_Name', required: true }]
+
+  async run() {
+    const { args, flags } = this.parse(Invoke)
+    const cmdArgs = [args.Intent_Name]
+    if (flags.path) {
+      cmdArgs.push(`--path=${flags.path}`)
+    }
+    this.runLegacy(['invoke', ...cmdArgs])
+  }
+}
