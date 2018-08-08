d29a16ad11d8b9576ed501bbd64486344888e775
Tanguy Antoine <antoine@bearer.sh>
2018-08-08 10:38:45 +0200

    refactor(cli): add start command

---
+++ packages/cli/src/commands/start.ts
@@ -0,0 +1,35 @@
+import { flags } from '@oclif/command'
+
+import BaseLegacyCommand from '../BaseLegacyCommand'
+
+const noOpen = 'no-open'
+const noInstall = 'no-install'
+const noWatcher = 'no-watcher'
+
+export default class Start extends BaseLegacyCommand {
+  static description = 'Start local development environment'
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    [noOpen]: flags.boolean({}),
+    [noInstall]: flags.boolean({}),
+    [noWatcher]: flags.boolean({ hidden: true })
+  }
+
+  static args = []
+
+  async run() {
+    const { flags } = this.parse(Start)
+    const cmdArgs = []
+    if (flags[noOpen]) {
+      cmdArgs.push(`--${noOpen}`)
+    }
+    if (flags[noInstall]) {
+      cmdArgs.push(`--${noInstall}`)
+    }
+    if (flags[noWatcher]) {
+      cmdArgs.push(`--${noWatcher}`)
+    }
+    this.runLegacy(['start', ...cmdArgs])
+  }
+}
