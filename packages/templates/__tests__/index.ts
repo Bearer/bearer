04766dace48af471af5f15a12470f92874eca70a
Tanguy Antoine <antoine@bearer.sh>
2018-08-13 10:38:32 +0200

    test(templates): add snapshot testing

--- packages/templates/__tests__/index.ts
+++ packages/templates/__tests__/index.ts
@@ -1,3 +1,25 @@
-test('it works', () => {
-  expect(1).toBe(1)
-})
+import { apiKey, basicAuth, noAuth, oauth2 } from '../src/index'
+
+const intents = {
+  apiKey, basicAuth, noAuth, oauth2
+}
+
+describe('Templates', () => {
+  Object.keys(intents).forEach((key) => {
+    const intent = intents[key]
+
+    describe(key, () => {
+      it('has SaveState', () => {
+        expect(intent.SaveState).toMatchSnapshot()
+      })
+
+      it('has RetrieveState', () => {
+        expect(intent.RetrieveState).toMatchSnapshot()
+      })
+
+      it('has FetchData', () => {
+        expect(intent.FetchData).toMatchSnapshot()
+      })
+    })
+  })
+});
