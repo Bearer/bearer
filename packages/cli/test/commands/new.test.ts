89f55698752001924192553597d1f8de687c5b07
Tanguy Antoine <antoine@bearer.sh>
2018-08-14 18:02:32 +0200

    test(cli): add simple test

--- packages/cli/test/commands/new.test.ts
+++ packages/cli/test/commands/new.test.ts
@@ -1,10 +1,10 @@
-// import { expect, test } from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
 describe('new', () => {
-  // test
-  //   .stdout()
-  //   .command(['new'])
-  //   .it('runs hello', ctx => {
-  //     expect(ctx.stdout).to.contain('hello world')
-  //   })
+  test
+    .stdout()
+    .command(['new', '.bearer/SpongeBobScenario', '-a', 'oauth2'])
+    .it('generates a scenario without any prompt', ctx => {
+      expect(ctx.stdout).to.contain('Scenario initialized, name: SpongeBobScenario, authentication type: OAuth2')
+    })
 })
