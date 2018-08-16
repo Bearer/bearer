4e765cd97d445fe4343ba5825dcef171460f0d1b
Tanguy Antoine <antoine@bearer.sh>
2018-08-16 10:06:24 +0200

    test(cli): add simple test for new command

--- packages/cli/test/commands/new.test.ts
+++ packages/cli/test/commands/new.test.ts
@@ -1,10 +1,18 @@
 import { expect, test } from '@oclif/test'
 
-describe('new', () => {
-  test
-    .stdout()
-    .command(['new', '.bearer/SpongeBobScenario', '-a', 'oauth2', '--skipInstall'])
-    .it('generates a scenario without any prompt', ctx => {
-      expect(ctx.stdout).to.contain('Scenario initialized, name: SpongeBobScenario, authentication type: OAuth2')
-    })
+describe('new command', () => {
+  it('generates files and print help', () => {
+    test
+      .stdout()
+      .command(['new', '.bearer/SpongeBobScenario', '-a', 'oauth2', '--skipInstall'])
+      .it('generates a scenario without any prompt', ctx => {
+        console.log('out', ctx.stdout)
+        expect(ctx.stdout).to.contain(`Generate files:`)
+        expect(ctx.stdout).to.contain(`create: .bearer/SpongeBobScenario/LICENSE`)
+        expect(ctx.stdout).to.contain(
+          `Scenario initialized, name: .bearer/SpongeBobScenario, authentication type: OAuth2`
+        )
+        expect(ctx.stdout).to.contain(`What's next?`)
+      })
+  })
 })
