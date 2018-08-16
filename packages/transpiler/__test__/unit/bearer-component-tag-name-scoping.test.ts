c6ab859add9f8ae069afc43859c4c2a2cd8d731d
Tanguy Antoine <antoine@bearer.sh>
2018-08-16 18:09:04 +0200

    refactor(transpiler): add prefix and suffix to Component

--- packages/transpiler/__test__/unit/bearer-component-tag-name-scoping.test.ts
+++ packages/transpiler/__test__/unit/bearer-component-tag-name-scoping.test.ts
@@ -5,6 +5,7 @@ const TEST_NAME = 'bearer-component-tag-scoping'
 describe('Scope element tag names', () => {
   runUnitOn(TEST_NAME)
 })
+
 describe('With provided scenario ID', () => {
   runUnitOn(TEST_NAME, { tagNamePrefix: 'bearer', tagNameSuffix: 'xyz' })
 })
--- packages/transpiler/src/transformers/gather-metadata.ts
+++ packages/transpiler/src/transformers/gather-metadata.ts
@@ -6,6 +6,17 @@ import { getDecoratorNamed, getExpressionFromDecorator, hasDecoratorNamed } from
 import { TransformerOptions } from '../types'
 
 export default function GatherMetadata({ metadata }: TransformerOptions = {}): ts.TransformerFactory<ts.SourceFile> {
+  function getTagNames(tagName: string): { initialTagName: string; finalTagName: string } {
+    const finalTag =
+      metadata.prefix && metadata.suffix
+        ? [Case.kebab(metadata.prefix), tagName, Case.kebab(metadata.suffix)].join('-')
+        : tagName
+    return {
+      initialTagName: tagName,
+      finalTagName: finalTag
+    }
+  }
+
   return _transformContext => {
     function visit(node: ts.Node): ts.Node {
       // Found Component
@@ -15,8 +26,7 @@ export default function GatherMetadata({ metadata }: TransformerOptions = {}): t
         metadata.components.push({
           classname: node.name.text,
           isRoot: false,
-          initialTagName: tag.text,
-          finalTagName: tag.text
+          ...getTagNames(tag.text)
         })
         return node
       }
@@ -29,15 +39,10 @@ export default function GatherMetadata({ metadata }: TransformerOptions = {}): t
         const groupExpression = getExpressionFromDecorator<ts.StringLiteral>(component, 'group')
         const group = groupExpression ? groupExpression.text : ''
         const tag = [Case.kebab(group), name].join('-')
-        const finalTag =
-          metadata.prefix && metadata.suffix
-            ? [Case.kebab(metadata.prefix), tag, Case.kebab(metadata.suffix)].join('-')
-            : tag
         metadata.components.push({
           classname: node.name.text,
           isRoot: true,
-          initialTagName: tag,
-          finalTagName: finalTag,
+          ...getTagNames(tag),
           group
         })
         return node
