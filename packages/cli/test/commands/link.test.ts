ed577111e90be2a306c413374978006d355441fa
Tanguy Antoine <antoine@bearer.sh>
2018-08-17 19:33:22 +0200

    test(cli): better link testing

--- packages/cli/test/commands/link.test.ts
+++ packages/cli/test/commands/link.test.ts
@@ -1,12 +1,26 @@
 import { test } from '@oclif/test'
 import { expect } from 'fancy-test'
+import * as sinon from 'sinon'
 
-describe('login', () => {
-  describe('Prompt for token', () => {
+import * as setup from '../../src/utlis/setupConfig'
+
+describe('Link', () => {
+  describe('Update', () => {
+    const update = sinon.spy()
+    const stub = sinon.stub(setup, 'default')
+    stub.returns({
+      setScenarioConfig: update
+    })
+
     test
       .stdout()
       .command(['link', '123-scenario-id'])
       .it('Display success message', ctx => {
+        expect(update.args[0][0]).to.include({
+          orgId: '123',
+          scenarioId: 'scenario-id'
+        })
+        stub.restore()
         expect(ctx.stdout).to.contain('Scenario successfully linked')
       })
   })
