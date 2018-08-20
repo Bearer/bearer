1a15d67f0298deec58b340ca29a01a8d09bf31ce
Tanguy Antoine <antoine@bearer.sh>
2018-08-20 18:36:02 +0200

    feat(cli): backport generate setup

--- packages/cli/src/commands/generate/setup.ts
+++ packages/cli/src/commands/generate/setup.ts
@@ -1,20 +1,60 @@
 import { flags } from '@oclif/command'
+import * as fs from 'fs'
+import * as path from 'path'
 
 import BaseCommand from '../../BaseCommand'
 import { RequireScenarioFolder } from '../../utils/decorators'
+import { copyFiles } from '../../utils/helpers'
+
+const setupKey = 'setupViews'
 
 export default class GenerateSetup extends BaseCommand {
   static description = 'Generate a Bearer Setup'
   static hidden = true
   static flags = {
-    help: flags.help({ char: 'h' })
+    help: flags.help({ char: 'h' }),
+    force: flags.boolean({})
   }
 
-  static args = [{ name: 'name', required: true }]
+  static args = []
 
   @RequireScenarioFolder()
   async run() {
-    const { args } = this.parse(GenerateSetup)
-    this.log(`Generate Setup ${args.name}`)
+    const { flags } = this.parse(GenerateSetup)
+    if (flags.force || !setupExists(this.locator.srcViewsDir)) {
+      const fields = this.scenarioAuthConfig[setupKey]
+      if (fields && fields.length) {
+        try {
+          await copyFiles(
+            this,
+            'generate/setup',
+            this.locator.srcViewsDir,
+            this.getVars(this.bearerConfig.scenarioConfig.scenarioTitle, fields)
+          )
+          this.success('Setup components successfully generated! 🎉')
+        } catch (e) {
+          this.error(e)
+        }
+      } else {
+        this.warn(`No ${setupKey} key found within auth.config.json file. skipping`)
+      }
+    } else {
+      this.warn('Setup files already exists, use --force flag to overwrite existing ones.')
+    }
   }
+
+  getVars(scenarioTitle: string, fields: any) {
+    return {
+      componentName: this.case.pascal(scenarioTitle),
+      componentTagName: this.case.kebab(scenarioTitle),
+      fields: JSON.stringify(fields)
+    }
+  }
+}
+
+// Note: using Or condition in case the developer delete one but customized the other component
+function setupExists(location: string): boolean {
+  return (
+    fs.existsSync(path.join(location, 'setup-action.tsx')) || fs.existsSync(path.join(location, 'setup-display.tsx'))
+  )
 }
