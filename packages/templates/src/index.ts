c8ba7bb5128197402082a47d036270aebb5cbb5d
Tanguy Antoine <antoine@bearer.sh>
2018-08-20 18:08:44 +0200

    refactor(templates): using enum as key

--- packages/templates/src/index.ts
+++ packages/templates/src/index.ts
@@ -1,6 +1,21 @@
-import apiKey from './apikey'
-import basicAuth from './basicauth'
-import noAuth from './noauth'
+import Authentication from "@bearer/types/lib/Authentications";
+
+import apikey from './apikey'
+import basicauth from './basicauth'
+import noauth from './noauth'
 import oauth2 from './oauth2'
 
-export { oauth2, apiKey, noAuth, basicAuth }
+type  TTemplate = {
+  SaveState: string,
+  RetrieveState: string,
+  FetchData: string
+}
+
+export const templates: Record<Authentication, TTemplate> = {
+  [Authentication.OAuth2]: oauth2,
+  [Authentication.ApiKey]: apikey,
+  [Authentication.Basic]: basicauth,
+  [Authentication.NoAuth]: noauth,
+}
+
+export default templates
