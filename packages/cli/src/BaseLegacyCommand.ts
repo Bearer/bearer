be54da2cb375d006d5b6cf3ce5f77ad61b3739c2
TANGUY Antoine <antoine.tanguy35@gmail.com>
2018-08-20 22:31:23 +0200

    perf(cli): improve cli startup time

--- packages/cli/src/BaseLegacyCommand.ts
+++ packages/cli/src/BaseLegacyCommand.ts
@@ -1,8 +1,8 @@
-import cli from '@bearer/bearer-cli/dist/bin/index'
 import Command from '@oclif/command'
 
 export default abstract class extends Command {
   runLegacy(cmdArgs: any[]) {
+    const cli = require('@bearer/bearer-cli/dist/bin/index').default
     this.debug('Legacy command arguments', JSON.stringify(cmdArgs))
     cli(['null', 'null'].concat(cmdArgs))
   }
