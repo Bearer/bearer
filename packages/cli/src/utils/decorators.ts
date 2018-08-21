f3df3a3f5373df59fde2c10f8ae7b8090487b440
Tanguy Antoine <antoine@bearer.sh>
2018-08-21 16:16:09 +0200

    fix(cli): await async commands

--- packages/cli/src/utils/decorators.ts
+++ packages/cli/src/utils/decorators.ts
@@ -4,9 +4,9 @@ interface ICommand extends Command {}
 export function RequireScenarioFolder() {
   return function(_target: any, _propertyKey: string | symbol, descriptor: PropertyDescriptor) {
     let originalMethod = descriptor.value
-    descriptor.value = function(this: ICommand) {
+    descriptor.value = async function(this: ICommand) {
       if (this.bearerConfig.isScenarioLocation) {
-        originalMethod.apply(this, arguments)
+        await originalMethod.apply(this, arguments)
       } else {
         this.error('This command must be run within a scenario folder.')
       }
