0d6bd0c63869ca461f8b3afc0439081f5ea9e032
numidiasoft <tarik@bearer.sh>
2018-08-22 17:56:26 +0200

    Update deploy.sh

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -4,4 +4,4 @@ echo "init dependencies"
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 yarn install --frozen-lockfile
 yarn run lerna bootstrap --froken-lockfile
-ls -l ~/
+ls -la ~/
