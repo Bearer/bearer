983290be2df452d60c5dac6ab7f312d4a8a8680d
Tanguy Antoine <antoine@bearer.sh>
2018-08-23 13:22:34 +0200

    feat(ts-plugin): first pass to add autocompletion on intent names

--- packages/typescript-plugin-bearer/src/index.ts
+++ packages/typescript-plugin-bearer/src/index.ts
@@ -1,38 +1,109 @@
-import * as ts_module from 'typescript/lib/tsserverlibrary'
+import * as _ts_module from 'typescript/lib/tsserverlibrary'
 
-function init(_modules: { typescript: typeof ts_module }) {
-  // const ts = modules.typescript
+function init(modules: { typescript: typeof _ts_module }) {
+  const ts = modules.typescript
 
   function create(info: ts.server.PluginCreateInfo) {
+    function getNode(fileName: string, position: number): ts.Node | undefined {
+      const sourceFile = info.languageService.getProgram()!.getSourceFile(fileName)
+      if (!sourceFile) {
+        return
+      }
+      function find(node: ts.Node): ts.Node | undefined {
+        if (position >= node.getStart() && position < node.getEnd()) {
+          return ts.forEachChild(node, find) || node
+        }
+      }
+      return find(sourceFile)
+    }
+
     info.project.projectService.logger.info('BEARER typescript plugin initialization')
-    info.project.projectService.logger.msg('BEARER typescript plugin initialization')
+    // ensure compiler option has decorator enabled
+    info.project.setCompilerOptions({ ...info.project.getCompilerOptions(), experimentalDecorators: true })
+
     // Set up decorator
     const proxy: ts.LanguageService = Object.create(null)
     for (let k of Object.keys(info.languageService) as Array<keyof ts.LanguageService>) {
       const x = info.languageService[k]
       proxy[k] = (...args: Array<{}>) => (x as any).apply(info.languageService, args)
     }
+    info.languageServiceHost.getScriptFileNames()
+    // Autocompletion
 
-    // proxy.getQuickInfoAtPosition = (fileName: string, position: number) => {
-    //   // const node = Helper.getNode(fileName, position);
-    //   const prior = info.languageService.getQuickInfoAtPosition(fileName, position)!
+    function isIntentDecorator(decorator: ts.Decorator): boolean {
+      return ts.isCallExpression(decorator.expression) && decorator.expression.expression.getText() === 'Intent'
+    }
 
-    //   if (prior && (prior.kind === 'method' || prior.kind === 'property')) {
-    //     prior.displayParts!.push({
-    //       kind: 'text',
-    //       text: 'bearer bro'
-    //     })
-    //   }
+    function isIntentName(tsNode: ts.Node): boolean {
+      return Boolean(
+        ts.isPropertyDeclaration(tsNode) &&
+          tsNode.modifiers &&
+          tsNode.modifiers.find(m => m.kind === ts.SyntaxKind.StaticKeyword) &&
+          tsNode.name.getText() === 'intentName'
+      )
+    }
 
-    //   info.project.projectService.logger.info(`[test] QuickInfo "${JSON.stringify(prior, null, 2)}"`)
+    function isIntentType(tsNode: ts.Node): boolean {
+      return Boolean(
+        ts.isPropertyDeclaration(tsNode) &&
+          tsNode.modifiers &&
+          tsNode.modifiers.find(m => m.kind === ts.SyntaxKind.StaticKeyword) &&
+          tsNode.name.getText() === 'intentType'
+      )
+    }
 
-    //   return prior
-    // }
-    // proxy.getCompletionsAtPosition = (fileName, position) => {
-    //   // const prior = info.languageService.getCompletionsAtPosition(fileName, position, [])
-    //   // prior.entries = prior.entries.filter(e => e.name !== 'caller')
-    //   return prior
-    // }
+    function retrieveIntentNames() {
+      return info.languageServiceHost.getScriptFileNames().reduce(
+        (acc, fileName) => {
+          const sourceFile = info.languageService.getProgram()!.getSourceFile(fileName)
+          if (sourceFile) {
+            const intentMeta = ts.forEachChild(sourceFile, tsNode => {
+              if (ts.isClassDeclaration(tsNode)) {
+                const metas: Record<string, string> = {}
+                ts.forEachChild(tsNode, child => {
+                  if (isIntentName(child)) {
+                    metas.name = ((child as ts.PropertyDeclaration).initializer as ts.StringLiteral).text
+                  } else if (isIntentType(child)) {
+                    metas.type = ((child as ts.PropertyDeclaration).initializer as ts.Identifier).text || 'type'
+                  }
+                })
+                return metas.name
+                  ? {
+                      name: `${metas.name} - ${metas.type}`,
+                      kind: ts.ScriptElementKind.string,
+                      sortText: metas.name,
+                      insertText: metas.name
+                    }
+                  : null
+              }
+            })
+            if (intentMeta) {
+              return [...acc, intentMeta]
+            }
+          }
+          return acc
+        },
+        [] as any[]
+      )
+    }
+
+    proxy.getCompletionsAtPosition = (fileName: string, position) => {
+      const node = getNode(fileName, position)
+      const prior = info.languageService.getCompletionsAtPosition(fileName, position, {})
+      if (prior && node && node.parent) {
+        const entries = retrieveIntentNames()
+
+        const quotedEntries = entries.map(e => ({ ...e, insertText: `"${e.insertText}"` }))
+
+        // has quote (single or double)
+        if (isIntentDecorator(node.parent as ts.Decorator)) {
+          prior.entries = [...quotedEntries, ...prior.entries]
+        } else if (ts.isDecorator(node.parent.parent) && isIntentDecorator(node.parent.parent)) {
+          prior.entries = [...entries, ...prior.entries]
+        }
+      }
+      return prior
+    }
     return proxy
   }
 
