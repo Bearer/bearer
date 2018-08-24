f7f78d97084de1687cbfe1bc36c92ffeb8f48c6d
Tanguy Antoine <antoine@bearer.sh>
2018-08-24 11:10:29 +0200

    test(cli): update login test

--- packages/cli/test/commands/login.test.ts
+++ packages/cli/test/commands/login.test.ts
@@ -1,60 +1,44 @@
-import { test } from '@oclif/test'
-import { expect } from 'fancy-test'
 import * as inquirer from 'inquirer'
-import * as sinon from 'sinon'
+import * as nock from 'nock'
+
+import LoginCommand from '../../src/commands/login'
 
 const email = 'spongebob@bearer.sh'
 
 describe('login', () => {
-  afterEach(() => (inquirer.prompt as any).restore())
+  let result: Array<string>
+
   describe('email provided', () => {
     beforeEach(() => {
-      sinon
-        .stub(inquirer, 'prompt')
-        .onCall(0)
-        .resolves({ token: 'ok' })
-    })
-    describe('Prompt for token', () => {
-      // it('works', done => {
-      test
-        .nock('https://int.bearer.sh', api =>
-          api.post('/api/v1/login', { Username: email, Password: 'ok' }).reply(200, {
-            authorization: {
-              AuthenticationResult: {
-                ExpiresIn: 3600
-              }
-            }
-          })
-        )
-        .stdout()
-        .command(['login', '--email', email])
-        .it('Display success message', ctx => {
-          expect(ctx.stdout).to.contain('Successfully logged in as spongebob@bearer.sh')
-        })
-    })
-  })
+      result = []
+      jest.spyOn(process.stdout, 'write').mockImplementation(val => result.push(val))
 
-  describe('email not provided', () => {
-    beforeEach(() => {
-      const stub = sinon.stub(inquirer, 'prompt')
-      stub.resolves(Promise.resolve({ string: email, token: 'ok' }))
-    })
-    describe('Prompt for email and token', () => {
-      test
-        .nock('https://int.bearer.sh', api =>
-          api.post('/api/v1/login', { Username: email, Password: 'ok' }).reply(200, {
-            authorization: {
-              AuthenticationResult: {
-                ExpiresIn: 3600
-              }
+      nock('https://int.bearer.sh')
+        .post('/api/v1/login', { Username: email, Password: 'ok' })
+        .reply(200, {
+          authorization: {
+            AuthenticationResult: {
+              ExpiresIn: 3600
             }
-          })
-        )
-        .stdout()
-        .command(['login'])
-        .it('Display success message', ctx => {
-          expect(ctx.stdout).to.contain('Successfully logged in as spongebob@bearer.sh')
+          }
         })
+    })
+
+    describe('Prompt for token', () => {
+      it('logs successfully', async () => {
+        jest.spyOn(inquirer, 'prompt').mockImplementation(() => Promise.resolve({ token: 'ok' }))
+        await LoginCommand.run(['--email', email])
+        expect(result.join()).toContain('Successfully logged in as spongebob@bearer.sh')
+      })
+    })
+
+    describe('Prompt for email and token', () => {
+      it('logs successfully', async () => {
+        jest.spyOn(inquirer, 'prompt').mockImplementation(() => Promise.resolve({ string: email, token: 'ok' }))
+
+        await LoginCommand.run([])
+        expect(result.join()).toContain('Successfully logged in as spongebob@bearer.sh')
+      })
     })
   })
 })
