01baf13bb0c3c7d6169bc9424152d57852f8975f
Tanguy Antoine <antoine@bearer.sh>
2018-08-24 09:21:16 +0200

    test(cli): update new tests

--- packages/cli/test/commands/new.test.ts
+++ packages/cli/test/commands/new.test.ts
@@ -1,8 +1,11 @@
-import { expect, test } from '@oclif/test'
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
+import NewCommand from '../../src/commands/new'
+import { readFile as _readFile } from '../helpers/utils'
+
 const destination = path.join(__dirname, '..', '.bearer/init')
+const AUTHCONFIG = 'auth.config.json'
 
 function emptyInitFolders() {
   if (fs.existsSync(destination)) {
@@ -13,66 +16,24 @@ function emptyInitFolders() {
 }
 
 function readFile(folder: string, filename: string): string {
-  return fs.readFileSync(path.join(destination, folder, filename), { encoding: 'utf8' })
+  return _readFile(destination, folder, filename)
 }
 
-const AUTHCONFIG = 'auth.config.json'
-
 describe('new command', () => {
-  emptyInitFolders()
-  test
-    .stdout()
-    .command(['new', 'Oauth2Scenario', '-a', 'OAUTH2', '--skipInstall', '--path', path.join(destination, 'oauth2')])
-    .it('generates a scenario without any prompt and OAuth2', ctx => {
-      console.log('out', ctx.stdout)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/oauth2/LICENSE`)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/oauth2/intents/tsconfig.json`)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/oauth2/auth.config.json`)
-      expect(ctx.stdout).to.contain(`Create setup files`)
-      expect(ctx.stdout).to.contain(`What's next?`)
+  let result: Array<string>
 
-      expect(readFile('oauth2', AUTHCONFIG)).to.contain('authType": "OAUTH2",')
+  beforeEach(() => {
+    result = []
+    jest.spyOn(process.stdout, 'write').mockImplementation(val => result.push(val))
+    emptyInitFolders()
+  })
+  const auths = ['OAUTH2', 'BASIC', 'APIKEY', 'NONE']
+  auths.map(auth => {
+    it(`generates a scenario without any prompt and ${auth}`, async () => {
+      await NewCommand.run([`${auth}Scenario`, '-a', auth, '--skipInstall', '--path', path.join(destination, auth)])
+      const outPut = result.join()
+      expect(outPut).toMatchSnapshot()
+      expect(readFile(auth, AUTHCONFIG)).toMatchSnapshot()
     })
-
-  test
-    .stdout()
-    .command(['new', 'NoAuthScenario', '-a', 'NONE', '--skipInstall', '--path', path.join(destination, 'none')])
-    .it('generates a scenario without any prompt  and NoAuth', ctx => {
-      console.log('out', ctx.stdout)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/none/LICENSE`)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/none/intents/tsconfig.json`)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/none/auth.config.json`)
-      expect(ctx.stdout).to.contain(`Create setup files`)
-      expect(ctx.stdout).to.contain(`What's next?`)
-
-      expect(readFile('none', AUTHCONFIG)).to.contain('authType": "NONE"')
-    })
-
-  test
-    .stdout()
-    .command(['new', 'NoAuthScenario', '-a', 'APIKEY', '--skipInstall', '--path', path.join(destination, 'apikey')])
-    .it('generates a scenario without any prompt and ApiKey', ctx => {
-      console.log('out', ctx.stdout)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/apikey/LICENSE`)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/apikey/intents/tsconfig.json`)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/apikey/auth.config.json`)
-      expect(ctx.stdout).to.contain(`Create setup files`)
-      expect(ctx.stdout).to.contain(`What's next?`)
-
-      expect(readFile('apikey', AUTHCONFIG)).to.contain('authType": "APIKEY"')
-    })
-
-  test
-    .stdout()
-    .command(['new', 'NoAuthScenario', '-a', 'BASIC', '--skipInstall', '--path', path.join(destination, 'basic')])
-    .it('generates a scenario without any prompt and Basic Auth', ctx => {
-      console.log('out', ctx.stdout)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/basic/LICENSE`)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/basic/intents/tsconfig.json`)
-      expect(ctx.stdout).to.contain(`create: test/.bearer/init/basic/auth.config.json`)
-      expect(ctx.stdout).to.contain(`Create setup files`)
-      expect(ctx.stdout).to.contain(`What's next?`)
-
-      expect(readFile('basic', AUTHCONFIG)).to.contain('authType": "BASIC"')
-    })
+  })
 })
