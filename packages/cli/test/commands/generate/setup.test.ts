43acf1f4727750edab889dd42de0774b52234bcd
TANGUY Antoine <antoine.tanguy35@gmail.com>
2018-08-24 23:28:50 +0200

    test(cli): fix flaky tests

--- packages/cli/test/commands/generate/setup.test.ts
+++ packages/cli/test/commands/generate/setup.test.ts
@@ -5,81 +5,76 @@ import GenerateSetup from '../../../src/commands/generate/setup'
 import { ensureBearerStructure } from '../../helpers/setup'
 import { readFile } from '../../helpers/utils'
 
-describe('Generate', () => {
+describe('generate:setup', () => {
   let bearerPath: string
-  let result: Array<string>
+  let result: any[]
 
-  const sampleConfig = [
-    {
-      authType: 'APIKEY',
-      setupViews: [
-        {
-          type: 'password',
-          label: 'Api Key',
-          controlName: 'apiKey'
-        }
-      ]
-    },
-    {
-      authType: 'OAUTH2',
-      setupViews: [
-        {
-          type: 'text',
-          label: 'Client ID',
-          controlName: 'clientID'
-        },
-        {
-          type: 'password',
-          label: 'Client Secret',
-          controlName: 'clientSecret'
-        }
-      ]
-    },
-    {
-      authType: 'BASIC',
-      setupViews: [
-        {
-          type: 'text',
-          label: 'Username',
-          controlName: 'username'
-        },
-        {
-          type: 'password',
-          label: 'Password',
-          controlName: 'password'
-        }
-      ]
-    }
-  ]
-
-  describe('generate:setup', () => {
-    beforeEach(() => {
-      jest.spyOn(process.stdout, 'write').mockImplementation(val => {
-        result.push(val)
-      })
+  beforeAll(() => {
+    jest.spyOn(process.stdout, 'write').mockImplementation(val => {
+      result.push(val)
     })
-    sampleConfig.map(auth => {
-      describe(auth.authType, () => {
-        beforeEach(() => {
-          result = []
-          bearerPath = ensureBearerStructure({ authConfig: auth, folderName: auth.authType })
-        })
-        it('create setup files ', async () => {
-          await GenerateSetup.run(['--force', '--path', bearerPath])
-          expect(readFile(bearerPath, 'views', 'setup-action.tsx')).toMatchSnapshot()
-          expect(readFile(bearerPath, 'views', 'setup-display.tsx')).toMatchSnapshot()
-          expect(result.join()).toContain('Setup components successfully generated!')
-        })
-      })
-    })
+  })
 
-    describe('No auth', () => {
-      it('create setup files ', async () => {
-        bearerPath = ensureBearerStructure({ authConfig: { authType: 'NONE' }, folderName: 'none' })
-        await GenerateSetup.run(['--path', bearerPath])
-        expect(fs.existsSync(path.join(bearerPath, 'views', 'setup-action.tsx'))).toBeFalsy()
-        expect(fs.existsSync(path.join(bearerPath, 'views', 'setup-display.tsx'))).toBeFalsy()
-      })
+  describe('BASIC', () => {
+    it('create setup files ', async () => {
+      const auth = {
+        authType: 'BASIC',
+        setupViews: [
+          { type: 'text', label: 'Username', controlName: 'username' },
+          { type: 'password', label: 'Password', controlName: 'password' }
+        ]
+      }
+      result = []
+      bearerPath = ensureBearerStructure({ authConfig: auth, folderName: 'BASIC' })
+
+      await GenerateSetup.run(['--force', '--path', bearerPath])
+      expect(readFile(bearerPath, 'views', 'setup-action.tsx')).toMatchSnapshot()
+      expect(readFile(bearerPath, 'views', 'setup-display.tsx')).toMatchSnapshot()
+      expect(result.join()).toContain('Setup components successfully generated!')
+    })
+  })
+
+  describe('OAUTH2', () => {
+    it('create setup files ', async () => {
+      const auth = {
+        authType: 'OAUTH2',
+        setupViews: [
+          { type: 'text', label: 'Client ID', controlName: 'clientID' },
+          { type: 'password', label: 'Client Secret', controlName: 'clientSecret' }
+        ]
+      }
+      result = []
+      bearerPath = ensureBearerStructure({ authConfig: auth, folderName: 'OAUTH2' })
+
+      await GenerateSetup.run(['--force', '--path', bearerPath])
+      expect(readFile(bearerPath, 'views', 'setup-action.tsx')).toMatchSnapshot()
+      expect(readFile(bearerPath, 'views', 'setup-display.tsx')).toMatchSnapshot()
+      expect(result.join()).toContain('Setup components successfully generated!')
+    })
+  })
+
+  describe('APIKEY', () => {
+    it('create setup files ', async () => {
+      const auth = {
+        authType: 'APIKEY',
+        setupViews: [{ type: 'password', label: 'Api Key', controlName: 'apiKey' }]
+      }
+      result = []
+      bearerPath = ensureBearerStructure({ authConfig: auth, folderName: 'APIKEY' })
+
+      await GenerateSetup.run(['--force', '--path', bearerPath])
+      expect(readFile(bearerPath, 'views', 'setup-action.tsx')).toMatchSnapshot()
+      expect(readFile(bearerPath, 'views', 'setup-display.tsx')).toMatchSnapshot()
+      expect(result.join()).toContain('Setup components successfully generated!')
+    })
+  })
+
+  describe('No auth', () => {
+    it('create setup files ', async () => {
+      bearerPath = ensureBearerStructure({ authConfig: { authType: 'NONE' }, folderName: 'none' })
+      await GenerateSetup.run(['--path', bearerPath])
+      expect(fs.existsSync(path.join(bearerPath, 'views', 'setup-action.tsx'))).toBeFalsy()
+      expect(fs.existsSync(path.join(bearerPath, 'views', 'setup-display.tsx'))).toBeFalsy()
     })
   })
 })
