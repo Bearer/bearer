1b6a92cfa3b13c098930d6ffdcf9a3788037f8a7
Tanguy Antoine <antoine@bearer.sh>
2018-08-28 14:20:07 +0200

    fix(transpiler): forgotten path

--- packages/legacy-cli/src/lib/startTranspiler.js
+++ packages/legacy-cli/src/lib/startTranspiler.js
@@ -1,3 +1,3 @@
-const transpiler = require('@bearer/transpiler/dist/bin/bearer-tst.js').default
+const transpiler = require('@bearer/transpiler/lib/bin/bearer-tst.js').default
 transpiler(process.argv.slice(2))
 process.send({ event: 'transpiler:initialized' })
