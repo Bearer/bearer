a71a491be9929678da6e9692b7420153886da6d3
Tanguy Antoine <antoine@bearer.sh>
2018-08-28 13:45:50 +0200

    chore(ui): remove storybook and demo app

--- packages/ui/.storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/html'
-
-const req = require.context('../src/components', true, /.stories.tsx$/)
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename))
-}
-
-configure(loadStories, module)
--- packages/ui/src/components/Alert/Alert.stories.tsx
+++
@@ -1,6 +0,0 @@
-import { storiesOf } from '@storybook/html'
-
-storiesOf('Alert', module).add('Kind', () => {
-  const kinds = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark']
-  return kinds.map(kind => `<bearer-alert kind="${kind}">Alert message: ${kind}</bearer-alert>`).join('<br />')
-})
