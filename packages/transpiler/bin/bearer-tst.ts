b4eabfff8d2a3948b007805fd500a12e01b8b0f6
Tanguy Antoine <antoine@bearer.sh>
2018-08-31 18:21:23 +0200

    feat(transpiler): prevent exiting same process

--- packages/transpiler/bin/bearer-tst.ts
+++ packages/transpiler/bin/bearer-tst.ts
@@ -9,22 +9,28 @@ export default args => {
     { name: 'prefix-tag', type: String },
     { name: 'suffix-tag', type: String }
   ]
-  const options = commandLineArgs(optionsDefinitions, { camelCase: true, argv: args, partial: true })
+  const options = commandLineArgs(optionsDefinitions, {
+    camelCase: true,
+    argv: args,
+    partial: true
+  })
   const transpiler = new Transpiler({
     watchFiles: !options.noWatcher,
     tagNamePrefix: options.prefixTag,
     tagNameSuffix: options.suffixTag
   })
 
-  process.on('message', message => {
-    if (message === 'refresh') {
-      transpiler.refresh()
-    }
-  })
+  if (options.noProcess) {
+    process.on('message', message => {
+      if (message === 'refresh') {
+        transpiler.refresh()
+      }
+    })
 
-  transpiler.on('STOP', () => {
-    process.exit(0)
-  })
+    transpiler.on('STOP', () => {
+      process.exit(0)
+    })
+  }
 
   transpiler.run()
 }
