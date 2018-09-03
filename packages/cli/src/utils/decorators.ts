bb1de4648b921ee7794f18a66d6bef28ba52d08e
Tanguy Antoine <antoine@bearer.sh>
2018-09-03 11:31:38 +0200

    feat(cli): create require linked scenario decorator

--- packages/cli/src/utils/decorators.ts
+++ packages/cli/src/utils/decorators.ts
@@ -14,3 +14,20 @@ export function RequireScenarioFolder() {
     return descriptor
   }
 }
+
+export function RequireLinkedScenario() {
+  return function(_target: any, _propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+    let originalMethod = descriptor.value
+    descriptor.value = async function(this: ICommand) {
+      if (this.bearerConfig.hasScenarioLinked) {
+        await originalMethod.apply(this, arguments)
+      } else {
+        const error =
+          this.colors.bold('You scenario must be linked before running this command\n') +
+          this.colors.yellow(this.colors.italic('Please run: bearer link'))
+        this.error(error)
+      }
+    }
+    return descriptor
+  }
+}
