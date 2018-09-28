a408f5e1f2f4e826fdd036c81d97b528798f56fb
TANGUY Antoine <antoine.tanguy35@gmail.com>
2018-09-28 17:27:39 +0200

    test(transpiler): allow empty options

--- packages/transpiler/__test__/utils/helpers.ts
+++ packages/transpiler/__test__/utils/helpers.ts
@@ -2,13 +2,19 @@ import fs from 'fs'
 import path from 'path'
 
 import { TranpilerOptions } from '../../src/index'
-import { BuildUnitFixtureDirectory, UnitFixtureDirectory } from '../utils/location'
+import {
+  BuildUnitFixtureDirectory,
+  UnitFixtureDirectory
+} from '../utils/location'
 
 import { TranspilerFactory } from './transpiler'
 
 const fixtures = path.join(__dirname, '__fixtures__')
 
-export function runUnitOn(name: string, transpilerOptions: Partial<TranpilerOptions>) {
+export function runUnitOn(
+  name: string,
+  transpilerOptions: Partial<TranpilerOptions> = {}
+) {
   const srcDirectory = UnitFixtureDirectory(name)
   const buildDirectory = BuildUnitFixtureDirectory(name)
 
@@ -20,13 +26,17 @@ export function runUnitOn(name: string, transpilerOptions: Partial<TranpilerOpti
     describe(file.replace(/\.tsx?/, '').replace(/\-/g, ' '), () => {
       it('match snapshot', async done => {
         expect.assertions(1)
-        fs.readFile(path.join(buildDirectory, file), 'utf8', (_e, postContent) => {
-          expect({
-            postContent,
-            file
-          }).toMatchSnapshot()
-          done()
-        })
+        fs.readFile(
+          path.join(buildDirectory, file),
+          'utf8',
+          (_e, postContent) => {
+            expect({
+              postContent,
+              file
+            }).toMatchSnapshot()
+            done()
+          }
+        )
       })
     })
   })
