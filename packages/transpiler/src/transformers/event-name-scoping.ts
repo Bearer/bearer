1e9a41d5065000a1f61fab0e121a407e7af151d1
Tanguy Antoine <antoine@bearer.sh>
2018-10-08 13:17:31 +0200

    feat(cli): scope event name with bearer

--- packages/transpiler/src/transformers/event-name-scoping.ts
+++ packages/transpiler/src/transformers/event-name-scoping.ts
@@ -3,17 +3,28 @@
  */
 import * as ts from 'typescript'
 
-import { Decorators } from '../constants'
-import { hasDecoratorNamed } from '../helpers/decorator-helpers'
+import { Decorators, Env } from '../constants'
+import { decoratorNamed, hasDecoratorNamed } from '../helpers/decorator-helpers'
 type TransformerOptions = {
   verbose?: true
 }
 
-function updateEventDecorator(tsProperty: ts.PropertyDeclaration): ts.PropertyDeclaration {
+function updateEventDecorator(tsProperty: ts.PropertyDeclaration, scope: string): ts.PropertyDeclaration {
   // const expression: ts.CallExpression = tsDecorator.expression as ts.CallExpression
+  const decorator = ts.createDecorator(
+    ts.createCall(ts.createIdentifier(Decorators.Event), undefined, [
+      ts.createObjectLiteral([
+        ts.createPropertyAssignment(
+          'eventName',
+          ts.createLiteral(['bearer', process.env[Env.BEARER_SCENARIO_ID], scope, tsProperty.name.getText()].join(':'))
+        )
+      ])
+    ])
+  )
+
   return ts.updateProperty(
     tsProperty,
-    tsProperty.decorators,
+    [...tsProperty.decorators.map(deco => (decoratorNamed(deco, Decorators.Event) ? decorator : deco))],
     tsProperty.modifiers,
     tsProperty.name.getText(),
     tsProperty.questionToken || tsProperty.exclamationToken,
@@ -24,14 +35,16 @@ function updateEventDecorator(tsProperty: ts.PropertyDeclaration): ts.PropertyDe
 
 export default function EventNameScoping(_params: TransformerOptions = {}): ts.TransformerFactory<ts.SourceFile> {
   return _transformContext => {
-    function visit(tsNode: ts.Node): ts.VisitResult<ts.Node> {
-      if (ts.isPropertyDeclaration(tsNode) && hasDecoratorNamed(tsNode, Decorators.Event)) {
-        return updateEventDecorator(tsNode)
-      }
-      return ts.visitEachChild(tsNode, visit, _transformContext)
-    }
-
     return tsSourceFile => {
+      const groupName: string = 'group'
+
+      function visit(tsNode: ts.Node): ts.VisitResult<ts.Node> {
+        if (ts.isPropertyDeclaration(tsNode) && hasDecoratorNamed(tsNode, Decorators.Event)) {
+          return updateEventDecorator(tsNode, groupName)
+        }
+        return ts.visitEachChild(tsNode, visit, _transformContext)
+      }
+
       return ts.visitEachChild(tsSourceFile, visit, _transformContext)
     }
   }
