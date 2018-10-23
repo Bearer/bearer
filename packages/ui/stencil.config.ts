145594b63d6cd926347b9e353b5dcb98904db453
Tanguy Antoine <antoine@bearer.sh>
2018-10-23 15:58:20 +0200

    feat(ui): remove log statement

--- packages/ui/stencil.config.ts
+++ packages/ui/stencil.config.ts
@@ -4,20 +4,27 @@ import { sass } from '@stencil/sass'
 
 import * as autoprefixer from 'autoprefixer'
 import * as replace from 'rollup-plugin-replace'
+import * as strip from 'rollup-plugin-strip'
+
+const plugins: any = [
+  sass({
+    injectGlobalPaths: ['src/globals/base.scss']
+  }),
+  replace({
+    'process.env.BUILD': JSON.stringify(process.env.BUILD)
+  }),
+  postcss({
+    plugins: [autoprefixer()]
+  })
+]
+
+if (process.env.build === 'distribution') {
+  plugins.push(strip())
+}
 
 export const config: Config = {
   namespace: 'bearer-ui',
-  plugins: [
-    sass({
-      injectGlobalPaths: ['src/globals/base.scss']
-    }),
-    replace({
-      'process.env.BUILD': JSON.stringify(process.env.BUILD)
-    }),
-    postcss({
-      plugins: [autoprefixer()]
-    })
-  ],
+  plugins,
   outputTargets: [
     { type: 'dist', dir: 'lib' },
     {
