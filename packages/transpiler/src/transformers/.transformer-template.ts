bf9ca9e0aff1e7f8ffb907663d132d21cb702171
Tanguy Antoine <antoine@bearer.sh>
2018-11-13 13:55:56 +0100

    style(transpiler): update template boilerplate

--- packages/transpiler/src/transformers/.transformer-template.ts
+++ packages/transpiler/src/transformers/.transformer-template.ts
@@ -3,10 +3,9 @@
  */
 import * as ts from 'typescript'
 
-type TransformerOptions = {
-  verbose?: true
-}
-export default function CHANGEME({ verbose }: TransformerOptions = {}): ts.TransformerFactory<ts.SourceFile> {
+import { TransformerOptions } from '../types'
+
+export default function CHANGEME(_options: TransformerOptions = {}): ts.TransformerFactory<ts.SourceFile> {
   return _transformContext => {
     function visit(tsNode: ts.Node): ts.VisitResult<ts.Node> {
       return ts.visitEachChild(tsNode, visit, _transformContext)
