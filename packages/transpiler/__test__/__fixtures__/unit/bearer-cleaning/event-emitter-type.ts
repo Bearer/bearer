145122d5671674376a1faba21f76592b5fba5ef8
Tanguy Antoine <antoine@bearer.sh>
2018-12-04 16:50:11 +0100

    test(transpiler): add fixture

---
+++ packages/transpiler/__test__/__fixtures__/unit/bearer-cleaning/event-emitter-type.ts
@@ -0,0 +1,22 @@
+import { EventEmitter, Event, Prop, RootComponent } from '@bearer/core'
+type Goat = {
+  color: string
+}
+type Panda = {
+  panda: string
+}
+@RootComponent({
+  group: 'feature',
+  role: 'display'
+})
+export class Component {
+  @Event() feed: EventEmitter
+  // TODO: Keep the type if it is used with prop or method: ex goat
+  // note: keeping it aside until we really need it
+  @Event() keptAsIs: EventEmitter<{ goat: Goat }>
+  @Prop() goat: Goat
+
+  // transformed to any because isn't imported by Stencil later
+  @Event()
+  pandaIsAnyfied: EventEmitter<Panda>
+}
---
+++ packages/transpiler/__test__/unit/bearer-cleaning.test.ts
@@ -0,0 +1,7 @@
+import { runUnitOn } from '../utils/helpers'
+
+const TEST_NAME = 'bearer-cleaning'
+
+describe('bearer cleaning transformer', () => {
+  runUnitOn(TEST_NAME)
+})
