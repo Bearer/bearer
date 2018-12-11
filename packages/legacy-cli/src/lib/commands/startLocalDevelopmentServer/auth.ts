3595ab6840d6d60ca898fc563cf76929650090a5
Radek Molenda <radek.molenda@gmail.com>
2018-12-11 15:08:25 +0100

    fix(legacy-cli): add missing v2 endpoints to local dev server

--- packages/legacy-cli/src/lib/commands/startLocalDevelopmentServer/auth.ts
+++ packages/legacy-cli/src/lib/commands/startLocalDevelopmentServer/auth.ts
@@ -10,7 +10,33 @@ router.get('v1/user/initialize', ctx => {
   ctx.body += '</script></body></html>'
 })
 
-router.get('v1/auth/:integration_uuid', async ctx => {
+router.get('v2/user/initialize', ctx => {
+  ctx.body = '<html><head></head><body><script>'
+  ctx.body += fs.readFileSync(__dirname + '/iframe.js')
+  ctx.body += '</script></body></html>'
+})
+
+const authScenarioV1 = async ctx => {
+  ctx.body = `<html>
+  <head>
+    <script src="https://cdn.jsdelivr.net/npm/post-robot@8.0.28/dist/post-robot.min.js"></script>
+    <script type="application/javascript">
+      window.LOG_LEVEL = 'error'
+      localStorage.setItem('${ctx.request.query.setupId}|${ctx.params.integration_uuid}', true)
+      postRobot.send(window.opener, 'BEARER_AUTHORIZED', {
+        scenarioId: '${ctx.params.integration_uuid}',
+        authId: '${auth}'
+      })
+      setTimeout(function() {
+        window.close()
+      }, 500)
+    </script>
+  </head>
+  <body></body>
+</html>`
+}
+
+const authScenarioV2 = async ctx => {
   ctx.body = `<html>
   <head>
     <script src="https://cdn.jsdelivr.net/npm/post-robot@8.0.28/dist/post-robot.min.js"></script>
@@ -28,6 +54,8 @@ router.get('v1/auth/:integration_uuid', async ctx => {
   </head>
   <body></body>
 </html>`
-})
+}
+router.get('v1/auth/:integration_uuid', authScenarioV1)
+router.get('v2/auth/:integration_uuid', authScenarioV2)
 
 export default router
