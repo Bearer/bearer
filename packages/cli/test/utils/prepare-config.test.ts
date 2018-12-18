5555d4e41d17057a143439f3bce2caceccf45f2f
Tanguy Antoine <antoine@bearer.sh>
2018-12-18 17:59:26 +0100

    build: prevent conflicting package to be hoisted
    *  JSX.IntrinsecElement is defined twice in @types/react and @stencil/core which make TS be unhappy

--- packages/cli/test/utils/prepare-config.test.ts
+++ packages/cli/test/utils/prepare-config.test.ts
@@ -1,7 +1,9 @@
 import * as path from 'path'
 
-import prepareConfig from './prepare-config'
-const base = path.join(__dirname, '../../test/utils/__fixtures__/prepare-config')
+import prepareConfig from '../../src/utils/prepare-config'
+
+const base = path.join(__dirname, './__fixtures__/prepare-config')
+
 describe('prepareConfig', () => {
   it('works', async () => {
     const config = await prepareConfig(path.join(base, 'auth.json'), '123-ok', path.join(base, 'intents'))
