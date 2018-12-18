6f81a73281ec66d849a97e9d95ac14fffbe2a324
Radek Molenda <radek.molenda@gmail.com>
2018-12-18 13:13:35 +0100

    test(transpiler): faster way of testing

--- packages/transpiler/__test__/unit/input-decorator.test.ts
+++ packages/transpiler/__test__/unit/input-decorator.test.ts
@@ -1,6 +1,27 @@
-import { runUnitOn } from '../utils/helpers'
+import { runTransformers } from '../utils/helpers'
+import { UnitFixtureDirectory } from '../utils/location'
+
+import Metadata from '../../src/metadata'
+import GatherMetadata from '../../src/transformers/gather-metadata'
+import InputDecorator from '../../src/transformers/input-decorator'
+import * as fs from 'fs'
+import * as path from 'path'
 const TEST_NAME = 'input-decorator'
 
+const metadata = new Metadata("bearer", "test")
+
 describe('Input decorator units', () => {
-  runUnitOn(TEST_NAME)
+  it("generates stuff", (done) => {
+    const srcDirectory = UnitFixtureDirectory(TEST_NAME)
+
+    fs.readdirSync(srcDirectory).forEach(file => {
+      const filePath = path.join(srcDirectory, file)
+
+      fs.readFile(filePath, 'utf8', (_e, postContent) => {
+        const result = runTransformers(postContent, [GatherMetadata({ metadata }), InputDecorator({ metadata })])
+        expect(result).toMatchSnapshot()
+        done()
+      })
+    })
+  })
 })
