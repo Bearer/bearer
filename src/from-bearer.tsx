6e8ae6c68689b268730b16b74e3ebb2a62c57e9c
Tanguy Antoine <antoine@bearer.sh>
2018-12-18 12:51:27 +0100

    refactor: linting/build issue
    * check if eventRef is present
    * fix linting issues
    * transform function to getter

--- src/from-bearer.tsx
+++ src/from-bearer.tsx
@@ -3,14 +3,14 @@ import * as React from 'react'
 import { BearerContext } from './bearer-provider'
 const BEARER_EVENT_PROP_PREFIX = 'bearer-'
 
-function fromBearer<T>(TagName: string) {
+export default function fromBearer<T>(TagName: string) {
   const propSetEvent = `${TagName}-prop-set`
 
-  class Klass extends React.Component<T> {
+  return class extends React.Component<T> {
     static displayName = `Bearer(${TagName})`
     static contextType = BearerContext
     context!: React.ContextType<typeof BearerContext>
-    private readonly eventRef: React.RefObject<HTMLInputElement>
+    readonly eventRef: React.RefObject<HTMLInputElement>
 
     constructor(props: T) {
       super(props)
@@ -18,25 +18,29 @@ function fromBearer<T>(TagName: string) {
     }
 
     public componentDidMount() {
-      this.eventRef.current!.addEventListener(propSetEvent, this.prophandler)
-      // NB: This means once a component is mounted we cannot change its event handlers
-      // In the future it would be good to improve this to allow dynamic allocation
-      this.handlers().forEach(key => {
-        const anyProps = this.props as any
-        if (anyProps[key]) {
-          this.eventRef.current!.addEventListener(key, anyProps[key])
-        }
-      })
+      if (this.eventRef.current) {
+        this.eventRef.current.addEventListener(propSetEvent, this.prophandler)
+        // NB: This means once a component is mounted we cannot change its event handlers
+        // In the future it would be good to improve this to allow dynamic allocation
+        this.handlers.forEach(key => {
+          const anyProps = this.props as any
+          if (anyProps[key]) {
+            this.eventRef.current!.addEventListener(key, anyProps[key])
+          }
+        })
+      }
     }
 
     public componentWillUnmount() {
-      this.eventRef.current!.removeEventListener(propSetEvent, this.prophandler)
-      this.handlers().forEach(key => {
-        const anyProps = this.props as any
-        if (anyProps[key]) {
-          this.eventRef.current!.removeEventListener(key, anyProps[key])
-        }
-      })
+      if (this.eventRef.current) {
+        this.eventRef.current.removeEventListener(propSetEvent, this.prophandler)
+        this.handlers.forEach(key => {
+          const anyProps = this.props as any
+          if (anyProps[key]) {
+            this.eventRef.current!.removeEventListener(key, anyProps[key])
+          }
+        })
+      }
     }
 
     public render() {
@@ -44,23 +48,17 @@ function fromBearer<T>(TagName: string) {
         ...(this.context.getState ? this.context.getState() : {}),
         ...(this.props as any)
       }
-      return (
-        <div ref={this.eventRef}>
-          <TagName {...combinedProps} />
-        </div>
-      )
+      return <TagName {...combinedProps} ref={this.eventRef} />
     }
 
-    private readonly prophandler = (e: any) => {
+    readonly prophandler = (e: any) => {
       if (this.context.handlePropUpdates) {
         this.context.handlePropUpdates(e)
       }
     }
 
-    private readonly handlers = () =>
-      Object.keys(this.props).filter((key: string) => key.indexOf(BEARER_EVENT_PROP_PREFIX) === 0)
+    get handlers() {
+      return Object.keys(this.props).filter((key: string) => key.startsWith(BEARER_EVENT_PROP_PREFIX))
+    }
   }
-  return Klass
 }
-
-export default fromBearer
