1c9778bdf7fa87e14f50fff11e4112cd9d376986
Tanguy Antoine <antoine@bearer.sh>
2018-12-27 14:15:00 +0100

    feat(security): simplify usage for now

--- packages/security/__test__/cipher.test.ts
+++ packages/security/__test__/cipher.test.ts
@@ -2,26 +2,26 @@ import Cipher from '../src/index'
 
 const cipher = new Cipher('test')
 describe('Cipher', () => {
-  it('encrypts the data correctly', async () => {
+  it('encrypts the data correctly', () => {
     expect.assertions(1)
     const message = 'test message'
     const encrypted = cipher.encrypt(message)
-    return expect(encrypted).toBe('873e48ebd52490bdec6db078b26047f2')
+    expect(encrypted).toBe('873e48ebd52490bdec6db078b26047f2')
   })
 
-  it('decrypts the data correctly', async () => {
+  it('decrypts the data correctly', () => {
     const encrypted = '873e48ebd52490bdec6db078b26047f2'
-    const decrypted = await cipher.decrypt(encrypted)
-    return expect(decrypted).toBe('test message')
+    const decrypted = cipher.decrypt(encrypted)
+    expect(decrypted).toBe('test message')
   })
 
-  it('returns a digest', async () => {
+  it('returns a digest', () => {
     const expectedDigest = '27764562547d0665075c1fcf972ee5990db168ee87e6888b4aed2ba2c0f3085d'
     const body = {
       key1: 'value1',
       key2: 'value2'
     }
-    const digest = await cipher.digest(JSON.stringify(body))
+    const digest = cipher.digest(JSON.stringify(body))
     expect(digest).toBe(expectedDigest)
   })
 })
