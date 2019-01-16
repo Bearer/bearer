5a860059cfb4d26339bbe4b1eac50d6e7ba9e7ed
Tanguy Antoine <antoine@bearer.sh>
2019-01-16 11:41:42 +0100

    refactor(intents): update typing flexibility
    * creat new action types
    * allow actions to receive generics so that typing is stronger

--- packages/intents/src/declaration.ts
+++ packages/intents/src/declaration.ts
@@ -1,52 +1,63 @@
 import { AxiosResponse } from 'axios'
 
 /**
- * Intent callbacks
+ * Simplest error payload, better add code and message so that debugging is made easier
  */
-export type TFetchDataCallback = (payload: { data: any } | { error: any }) => void
-
-export type TRetrieveStateCallback = (payload: { data: any } | { error: any }) => void
+export type TErrorPayload = { error: any }
+export type TDataPayload<ReturnedData = any> = { data: ReturnedData }
 
 /**
- * when success, state represent the data you want to store within Bearer database
- * whereras data sent to the frontend could be different
+ * Intent callbacks
  */
-export type TSaveStateCallback = (payload: { state: any; data?: any } | { error: any }) => void
+export type TFetchPayload<ReturnedData = any> = (payload: TDataPayload<ReturnedData> | TErrorPayload) => void
+export type TRetrieveStatePayload<ReturnedData = any> = (payload: TDataPayload<ReturnedData> | TErrorPayload) => void
+
+export type TSaveStatePayload<State = any, ReturnedData = any> = {
+  state: State
+  data?: ReturnedData
+}
 
 /**
  * Intents
  */
-
-export type TBearerLambdaContext<T = TAuthContext> = T & {
+export type TBearerLambdaContext<AuthContext = TAuthContext> = AuthContext & {
   bearerBaseURL: string
 }
 
-export type ISaveStateIntentAction = (
-  context: TBearerLambdaContext,
-  _params: any,
+/**
+ * Save state action, let you store data into Bearer database without having to deal with database communication
+ * Later, data could be automatically loaded by passing a reference ID parameter
+ * terraformerId => will inject terrafomer object into context if found within Bearer database
+ */
+export type ISaveStateAction<AuthContext = TAuthContext, State = any> = (
+  context: TBearerLambdaContext<AuthContext>,
+  params: any,
   body: any,
-  state: any,
-  callback: TSaveStateCallback
-) => void
+  state: State
+) => Promise<TSaveStatePayload>
 
-export type IRetrieveStateIntentAction = (
-  context: TBearerLambdaContext,
-  _params: any,
-  state: any,
-  callback: TRetrieveStateCallback
-) => void
+/**
+ * Retrieve state action, let you retrieve data stored into Bearer database
+ * Alternatively, you can retrieve data from a fetch Intent by
+ */
+export type IRetrieveStateAction<AuthContext = TAuthContext, State = any> = (
+  context: TBearerLambdaContext<AuthContext>,
+  params: any,
+  state: State
+) => Promise<TFetchPayload>
 
-export type TFetchDataAction = (
-  context: TBearerLambdaContext,
+/**
+ * Fetch any data
+ */
+export type TFetchAction<AuthContext = TAuthContext, ReturnedData = any> = (
+  context: TBearerLambdaContext<AuthContext>,
   params: Record<string, any>,
-  body: Record<string, any>,
-  callback: TFetchDataCallback
-) => void
+  body: Record<string, any>
+) => Promise<TFetchPayload<ReturnedData>>
 
 /**
  * Auth definitions
  */
-
 type TBaseAuthContext<TAuthAccessContent> = {
   authAccess: TAuthAccessContent
   [key: string]: any
@@ -76,3 +87,50 @@ export type TLambdaEvent<T = TAuthContext> = {
 }
 
 export type TLambdaCallback = (error: any | null, data: any) => void
+
+/**
+ * Deprecated
+ */
+
+/**
+ * @deprecated since version beta5 please use ISaveStateAction
+ *
+ * Save state action, let you store data into Bearer database without having to deal with database communication
+ * Later, data could be automatically loaded by passing a reference ID parameter
+ * terraformerId => will inject terrafomer object into context if found within Bearer database
+ */
+export type ISaveStateIntentAction = (
+  context: TBearerLambdaContext,
+  _params: any,
+  body: any,
+  state: any,
+  callback: TSaveStateCallback
+) => void
+
+/**
+ * @deprecated since version beta5 please use IRetrieveStateAction
+ * Retrieve state action, let you retrieve data stored into Bearer database
+ * Alternatively, you can retrieve data from a fetch Intent by
+ */
+export type IRetrieveStateIntentAction = (
+  context: TBearerLambdaContext,
+  _params: any,
+  state: any,
+  callback: TRetrieveStatePayload
+) => void
+
+/**
+ * @deprecated since version beta5 please use IRetrieveStateAction
+ */
+export type TFetchDataAction = (
+  context: TBearerLambdaContext,
+  params: Record<string, any>,
+  body: Record<string, any>,
+  callback: TFetchPayload
+) => void
+
+/**
+ * when success, state represent the data you want to store within Bearer database
+ * whereras data sent to the frontend could be different
+ */
+export type TSaveStateCallback = (payload: TSaveStatePayload | TErrorPayload) => void
