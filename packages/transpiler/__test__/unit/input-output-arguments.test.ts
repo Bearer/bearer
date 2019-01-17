a11cd626faec1f66548f5a2c6801ff07fba7e187
Radek Molenda <radek.molenda@gmail.com>
2019-01-17 09:55:24 +0100

    refactor(transpiler): use faster tests

--- packages/transpiler/__test__/unit/input-output-arguments.test.ts
+++ packages/transpiler/__test__/unit/input-output-arguments.test.ts
@@ -1,6 +1,27 @@
-import { runUnitOn } from '../utils/helpers'
+import { runTransformers } from '../utils/helpers'
+import { UnitFixtureDirectory } from '../utils/location'
+
+import Metadata from '../../src/metadata'
+import GatherMetadata from '../../src/transformers/gather-metadata'
+import OutputDecorator from '../../src/transformers/output-decorator'
+import * as fs from 'fs'
+import * as path from 'path'
 const TEST_NAME = 'input-output-intent-arguments'
 
-describe('Input output intent arguments', () => {
-  runUnitOn(TEST_NAME)
+const metadata = new Metadata('bearer', 'test')
+
+describe('input and output arguments', () => {
+  it('generates the refIds properly', done => {
+    const srcDirectory = UnitFixtureDirectory(TEST_NAME)
+
+    fs.readdirSync(srcDirectory).forEach(file => {
+      const filePath = path.join(srcDirectory, file)
+
+      fs.readFile(filePath, 'utf8', (_e, postContent) => {
+        const result = runTransformers(postContent, [GatherMetadata({ metadata }), OutputDecorator({ metadata })])
+        expect(result).toMatchSnapshot()
+        done()
+      })
+    })
+  })
 })
