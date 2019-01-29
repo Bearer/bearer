db516009431487b72c3a203bde0764ccda14a18e
Tanguy Antoine <antoine@bearer.sh>
2019-01-29 16:22:29 +0100

    test(tranpiler): update existing examples to concrete use cases

--- packages/transpiler/__test__/__fixtures__/unit/event-normalization/component.tsx
+++ packages/transpiler/__test__/__fixtures__/unit/event-normalization/component.tsx
@@ -2,7 +2,14 @@ import { Event, Listen } from '@bearer/core'
 
 class Class {
   @Event() event: any
-  @Event({ eventName: 'SomeThinIwant_to_noramlize123-', somethingElseuntouched: 'Something' }) normalized: any
+  @Event({ eventName: 'body:SomeThinIDontwant_to_beeeeeeenoramlized123-', somethingElseuntouched: 'Something' })
+  bodyscopeNotNormalized: any
+  @Event({ eventName: 'SomeThinIDontwant_to_beeeeeeenoramlized123-', somethingElseuntouched: 'Something' })
+  totNormalized: any
+  @Event({ eventName: 'body:bearer-SomeThinIWant_to_beeeeeeenoramlized123-', somethingElseuntouched: 'Something' })
+  bodyscopedNormalized: any
+  @Event({ eventName: 'bearer-SomeThinIWant_to_beeeeeeenoramlized123-', somethingElseuntouched: 'Something' })
+  normalized: any
 
   @Listen('anUntouchedEvent') anEvent: any
   @Listen('body:bearer-somethingSdsfjkh_-') bodyScopednormalizedEvent: any
