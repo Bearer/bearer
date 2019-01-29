d979f4dc3a6311e58c77b6a1f2bd18476eb0a4a1
Tanguy Antoine <antoine@bearer.sh>
2019-01-29 15:13:40 +0100

    fix(transpiler): normalize bearer event names

--- packages/transpiler/__test__/__fixtures__/unit/event-normalization/component.tsx
+++ packages/transpiler/__test__/__fixtures__/unit/event-normalization/component.tsx
@@ -1,6 +1,10 @@
-import { Event } from '@bearer/core'
+import { Event, Listen } from '@bearer/core'
 
 class Class {
   @Event() event: any
   @Event({ eventName: 'SomeThinIwant_to_noramlize123-', somethingElseuntouched: 'Something' }) normalized: any
+
+  @Listen('anUntouchedEvent') anEvent: any
+  @Listen('body:bearer-somethingSdsfjkh_-') bodyScopednormalizedEvent: any
+  @Listen('bearer-somethingNeedsTobe-normalized') normalizedEvent: any
 }
--- packages/transpiler/src/transformers/event-name-normalizer.ts
+++ packages/transpiler/src/transformers/event-name-normalizer.ts
@@ -2,6 +2,7 @@ import * as ts from 'typescript'
 
 import { Decorators, Properties } from '../constants'
 import { decoratorNamed } from '../helpers/decorator-helpers'
+import { isBearerEvent } from './event-name-scoping'
 import * as Case from 'case'
 
 export default function eventNameNormalizer(): ts.TransformerFactory<ts.SourceFile> {
@@ -11,8 +12,21 @@ export default function eventNameNormalizer(): ts.TransformerFactory<ts.SourceFi
         ts.isPropertyAssignment(tsNode) &&
         (tsNode.name as ts.Identifier).escapedText.toString() === Properties.eventName
       ) {
-        const normalizedName = Case.kebab((tsNode.initializer as ts.StringLiteral).text.toString())
+        const normalizedName = normalize((tsNode.initializer as ts.StringLiteral).text.toString())
         return ts.createPropertyAssignment(tsNode.name, ts.createLiteral(normalizedName))
+      }
+      return ts.visitEachChild(tsNode, normalizeEventName, _transformContext)
+    }
+
+    function normalizeListenerEventName(tsNode: ts.Decorator): ts.VisitResult<ts.Node> {
+      const expression = tsNode.expression
+      if (ts.isCallExpression(expression)) {
+        const [eventName, ...rest] = expression.arguments
+        const normalizedName = normalize((eventName as ts.StringLiteral).text)
+        return ts.updateDecorator(
+          tsNode,
+          ts.createCall(expression.expression, expression.typeArguments, [ts.createLiteral(normalizedName), ...rest])
+        )
       }
       return ts.visitEachChild(tsNode, normalizeEventName, _transformContext)
     }
@@ -21,6 +35,10 @@ export default function eventNameNormalizer(): ts.TransformerFactory<ts.SourceFi
       if (ts.isDecorator(tsNode) && decoratorNamed(tsNode, Decorators.Event)) {
         return normalizeEventName(tsNode)
       }
+
+      if (ts.isDecorator(tsNode) && decoratorNamed(tsNode, Decorators.Listen)) {
+        return normalizeListenerEventName(tsNode)
+      }
       return ts.visitEachChild(tsNode, visit, _transformContext)
     }
 
@@ -28,4 +46,14 @@ export default function eventNameNormalizer(): ts.TransformerFactory<ts.SourceFi
       return ts.visitEachChild(tsSourceFile, visit, _transformContext)
     }
   }
+}
+const PREFIX = 'body:'
+function normalize(eventName: string): string {
+  if (!isBearerEvent(eventName)) {
+    return eventName
+  }
+  if (eventName.startsWith(PREFIX)) {
+    return `${PREFIX}${normalize(eventName.split(PREFIX)[1])}`
+  }
+  return Case.kebab(eventName)
 }
