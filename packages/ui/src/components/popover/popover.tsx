e8b6e59c8244460e57d4a24d8252465fbc7f5697
Corentin Brossault <corentin.brossault@bearer.sh>
2019-02-08 16:11:42 +0100

    refactor(ui): surcharge popover element

--- packages/ui/src/components/popover/popover.tsx
+++ packages/ui/src/components/popover/popover.tsx
@@ -16,9 +16,11 @@ export class BearerPopover {
   @Prop({ reflectToAttr: true }) direction: TDirection = 'right'
   @Prop({ reflectToAttr: true }) aligned: TAlignement = 'left'
 
-  @Prop() header: string
+  @Prop() button: string
+  @Prop() title: string
+  @Prop() content: string
+
   @Prop() backNav: boolean = false
-  @Prop() btnProps: JSXElements.BearerButtonAttributes = {}
 
   @State() _visible: boolean = false
 
@@ -72,23 +74,38 @@ export class BearerPopover {
   render() {
     return [
       // @ts-ignore
-      <slot name="popover-button" onClick={this.toggleDisplay}>
-        <bearer-button kind="primary">My Bearer's Integration</bearer-button>
+      <slot name="popover-toggler" onClick={this.toggleDisplay}>
+        <bearer-button kind="primary">{this.button}</bearer-button>
       </slot>,
 
       <div
         onClick={this.clickInsidePopover}
         class={`popover direction-${this.direction} ${!this.visible && 'hidden'} aligned-${this.aligned}`}
       >
-        {(this.backNav || this.header) && (
-          <h3 class="popover-header">
-            {this.backNav && <bearer-navigator-back class="header-arrow" />}
-            <span class="header">{this.header}</span>
-          </h3>
-        )}
-        <div class="popover-body">
-          <slot />
-        </div>
+        <slot name="popover-container">
+          <div class="popover-header">
+            <slot name="popover-header">
+              {this.backNav && <bearer-navigator-back class="header-arrow" />}
+              <div class="popover-title">
+                <slot name="popover-title">
+                  <h3>{this.title}</h3>
+                </slot>
+              </div>
+            </slot>
+          </div>
+          <div class="popover-content">
+            <slot name="popover-content">{this.content}</slot>
+            <div class="popover-actions">
+              <slot name="popover-actions" />
+            </div>
+          </div>
+          <div class="popover-copyright">
+            Powered by{' '}
+            <a href="https://bearer.sh" target="_blank">
+              Bearer.sh
+            </a>
+          </div>
+        </slot>
       </div>
     ]
   }
