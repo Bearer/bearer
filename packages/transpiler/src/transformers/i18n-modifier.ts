6e5c06021ebf542ad2a89f3aac5bd7b10199f96e
Tanguy Antoine <antoine@bearer.sh>
2019-02-12 14:17:01 +0100

    feat(transpiler): rename t/p to aliased version

--- packages/transpiler/src/transformers/i18n-modifier.ts
+++ packages/transpiler/src/transformers/i18n-modifier.ts
@@ -7,7 +7,7 @@ import * as ts from 'typescript'
 import { shouldProcessFile as hasAccessor } from './scenario-id-accessor-injector'
 
 import { TransformerOptions } from '../types'
-import { Component } from '../constants'
+import { Component, Module } from '../constants'
 
 const I18N_TAG = 'bearer-i18n'
 
@@ -29,11 +29,48 @@ function visitJsxSelfClosingElement(node: ts.JsxSelfClosingElement): ts.JsxSelfC
   )
 }
 
+function transformNamedBindings(tsImportSpecifier: ts.ImportSpecifier): ts.ImportSpecifier {
+  if (tsImportSpecifier.propertyName) {
+    switch ((tsImportSpecifier.propertyName as ts.Identifier).escapedText) {
+      case 't': {
+        return ts.createImportSpecifier(ts.createIdentifier('scopedT'), tsImportSpecifier.name)
+      }
+      case 'p': {
+        return ts.createImportSpecifier(ts.createIdentifier('scopedP'), tsImportSpecifier.name)
+      }
+    }
+  }
+  return tsImportSpecifier
+}
+
+function aliasImports(tsImport: ts.ImportDeclaration): ts.ImportDeclaration {
+  if ((tsImport.moduleSpecifier as ts.StringLiteral).text.toString() !== Module.BEARER_CORE_MODULE) {
+    return tsImport
+  }
+  const bindings = tsImport.importClause.namedBindings as ts.NamedImports
+
+  return ts.updateImportDeclaration(
+    tsImport,
+    tsImport.decorators,
+    tsImport.modifiers,
+    ts.createImportClause(
+      tsImport.importClause.name,
+      ts.createNamedImports(bindings.elements.map(transformNamedBindings))
+    ),
+    tsImport.moduleSpecifier
+  )
+}
+
 export default function i18nModifier(_options: TransformerOptions = {}): ts.TransformerFactory<ts.SourceFile> {
   return _transformContext => {
     function visit(tsNode: ts.Node): ts.VisitResult<ts.Node> {
-      // if bearer-i18n tag add scope
       switch (tsNode.kind) {
+        // alias imports
+        case ts.SyntaxKind.ImportDeclaration: {
+          return ts.visitEachChild(aliasImports(tsNode as ts.ImportDeclaration), visit, _transformContext)
+        }
+        // create
+        // if bearer-i18n tag add scope
         case ts.SyntaxKind.JsxSelfClosingElement:
           return ts.visitEachChild(
             visitJsxSelfClosingElement(tsNode as ts.JsxSelfClosingElement),
@@ -48,6 +85,7 @@ export default function i18nModifier(_options: TransformerOptions = {}): ts.Tran
       if (!hasAccessor(tsSourceFile)) {
         return tsSourceFile
       }
+
       return ts.visitEachChild(tsSourceFile, visit, _transformContext)
     }
   }
