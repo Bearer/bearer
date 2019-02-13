c130f8158249f94aaac629e8aa4fe839a77d435f
Tanguy Antoine <antoine@bearer.sh>
2019-02-13 17:43:20 +0100

    chore(ui): add logger tab

--- packages/ui/.storybook/config.ts
+++ packages/ui/.storybook/config.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import { configure } from '@storybook/html'
-
+import '@storybook/addon-console'
 // automatically import all files ending in *.stories.tsx
 // @ts-ignore
 const req = require.context('../stories', true, /.stories.tsx$/)
