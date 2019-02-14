ada733fe0daaabbf220d5c3fc77fd25b8121760d
Tanguy Antoine <antoine@bearer.sh>
2019-02-14 11:58:03 +0100

    chore(ui): add i18n stories

--- packages/ui/stories/i18n.stories.tsx
+++ packages/ui/stories/i18n.stories.tsx
@@ -1,6 +1,29 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 
-storiesOf('I18n', module).addWithJSX('colors', () => [
-  <bearer-i18n key="missing.key" var='{"ok": "asdasdasddas"}' default="defautl value" />
-])
+storiesOf('I18n translate', module)
+  .addWithJSX('Missing Key: default Value', () => [
+    <bearer-i18n _key="missing.key" var='{"ok": "Sponge bob"}' default="Default value : {{ok}}" />
+  ])
+  .addWithJSX('Existing Key', () => [
+    <bearer-i18n _key="existing.key" var='{"ok": "🐻"}' default="Not displayed : {{ok}}" />
+  ])
+
+storiesOf('I18n pluralize', module)
+  .addWithJSX('Missing Key: default Value', () => [
+    <scenario-placeholder>
+      <bearer-i18n _key="missing.key" count="0" default="A default text : {{count}}" />
+    </scenario-placeholder>
+  ])
+  .addWithJSX('Existing Key: 0', () => [
+    <bearer-i18n _key="withCount" count="0" default="A default text : {{count}}" />
+  ])
+  .addWithJSX('Existing Key: 1', () => [
+    <bearer-i18n _key="withCount" count="1" default="A default text : {{count}}" />
+  ])
+  .addWithJSX('Existing Key: 2', () => [
+    <bearer-i18n _key="withCount" count="2" default="A default text : {{count}}" />
+  ])
+  .addWithJSX('Existing Key: 3', () => [
+    <bearer-i18n _key="withCount" count="3" default="A default text : {{count}}" />
+  ])
