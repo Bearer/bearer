c286e14f3c72f5adf5cbe884f522b2c809dc66bb
numidiasoft <tarik@bearer.sh>
2019-02-27 13:02:13 +0100

    add jsdoc

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -3,11 +3,15 @@ export default (scope: string) => debug(`bearer:${scope}`)
 
 export class BearerIntentLogger {
   context: any
-
+  /**
+   * @param  {any} context
+   */
   constructor(context: any) {
     this.context = context
   }
-
+  /**
+   * @param  {any} data
+   */
   log = (data: any) => {
     debug('bearer:intents')('%j', {
       data,
