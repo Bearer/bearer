5f23dfc98df48a946a1f5e6e5f63257515c31af7
numidiasoft <tarik@bearer.sh>
2019-02-27 12:39:58 +0100

    add the intent logger

--- packages/logger/__tests__/logger.test.ts
+++ packages/logger/__tests__/logger.test.ts
@@ -1,7 +1,16 @@
-import debug from '../src/logger'
+import debug, { BearerIntentLogger } from '../src/logger'
+import { IDebug } from 'debug'
 
 describe('logger', () => {
   it('needs tests', () => {
     expect(debug).toBeInstanceOf(Function)
+  })
+})
+
+describe('intent logger', () => {
+  it('returns an intent logger', () => {
+    const logger = new BearerIntentLogger({})
+    expect(logger).toBeInstanceOf(BearerIntentLogger)
+    expect(logger.log).toBeInstanceOf(Function)
   })
 })
--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,2 +1,18 @@
 import debug from 'debug'
 export default (scope: string) => debug(`bearer:${scope}`)
+
+export class BearerIntentLogger {
+  context: any
+
+  constructor(context: any) {
+    this.context = context
+  }
+
+  log = (message: any) => {
+    debug('bearer:intents')('%j', {
+      data: message,
+      scenarioId: this.context.integrationUuid,
+      intentName: this.context.intentName
+    })
+  }
+}
