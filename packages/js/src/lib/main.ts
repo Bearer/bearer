2dbf8702ab497c55e9c9d6c2268afc2bd6a34029
Tanguy Antoine <antoine@bearer.sh>
2019-03-02 08:41:52 +0100

    feat(js): keep reference to bearer instance

--- packages/js/src/lib/main.ts
+++ packages/js/src/lib/main.ts
@@ -1,13 +1,17 @@
 import Bearer, { TBearerOptions } from './bearer'
 import i18n, { I18n } from './i18n'
 
-const bearer: TBearer = (token: string, options?: Partial<TBearerOptions>) => new Bearer(token, options)
+const bearer: TBearer = (token: string, options?: Partial<TBearerOptions>) => {
+  bearer.instance = new Bearer(token, options)
+  return bearer.instance
+}
 
 bearer.version = 'BEARER_VERSION'
 bearer.i18n = i18n
 
 export type TBearer = {
   (token: string, options?: Partial<TBearerOptions>): Bearer
+  instance?: Bearer
   version: string
   i18n: I18n
 }
