155f6d70eb57c9f91458f5cdbc347389e54a8979
Corentin Brossault <corentin.brossault@bearer.sh>
2019-03-04 18:21:31 +0100

    fix: placeholder on API Key FieldSet

--- packages/ui/src/components/setup/setup-types/key.ts
+++ packages/ui/src/components/setup/setup-types/key.ts
@@ -3,7 +3,8 @@ import { FieldType } from '../../Forms/Fieldset'
 export const KeySetupType = [
   {
     type: 'text' as FieldType,
-    label: 'Enter your API Key',
+    label: 'API Key',
+    placeholder: 'Enter your API Key',
     controlName: 'key',
     required: true
   }
