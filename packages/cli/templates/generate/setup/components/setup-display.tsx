648886b46176431d9cfa2d0182c52cc835cfe880
Corentin Brossault <corentin.brossault@bearer.sh>
2019-03-07 23:22:18 +0100

    refactor: use of input/output instead of events

--- packages/cli/templates/generate/setup/components/setup-display.tsx
+++ packages/cli/templates/generate/setup/components/setup-display.tsx
@@ -1,18 +1,19 @@
 /*
-  The purpose of this component is to save scenario credentials.
+  The purpose of this component is to display the integration reference ID.
+  This file has been generated automatically. Edit it at your own risk :-)
 */
 
-import { RootComponent } from "@bearer/core";
-import "@bearer/ui";
+import { RootComponent, Input } from '@bearer/core'
+import '@bearer/ui'
 
 @RootComponent({
-  group: "setup",
+  group: 'setup',
   role: 'display'
 })
 export class SetupDisplay {
+  @Input() setup: any
+
   render() {
-    return (
-      <bearer-setup-display scenarioId="BEARER_SCENARIO_ID" setupId={(this as any).setupId} />
-    )
+    return <bearer-setup-display integrationId="BEARER_INTEGRATION_ID" setupId={this.setup && this.setup.referenceId} />
   }
 }
