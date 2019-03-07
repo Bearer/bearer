af40a8b3d317a2f677a691338b12f526f2603ffe
numidiasoft <tarik@bearer.sh>
2019-03-07 18:34:12 +0100

    Fix logging

--- packages/x-ray/src/index.ts
+++ packages/x-ray/src/index.ts
@@ -3,15 +3,16 @@ import { overrideRequestMethod, overrideGetMethod } from './http-overrides'
 const logger = debug('intents')
 
 export const captureHttps = (module: any, event: any) => {
+  logger(`Ìnject ${event.params.clientId} and ${event.params.scenarioUuid}`)
+  process.env.clientId = event.params.clientId
+  process.env.scenarioUuid = event.params.scenarioUuid
+
   if (module._request || module._get) {
     // This because the cold lambda start
     // Avoid overriding methods for next calls
     return
   }
 
-  logger(`Ìnject ${event.params.clientId} and ${event.params.scenarioUuid}`)
-  process.env.clientId = event.params.clientId
-  process.env.scenarioUuid = event.params.scenarioUuid
   overrideRequestMethod(module)
   overrideGetMethod(module)
 }
