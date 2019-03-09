1a186fc0f8076c454405b7280e8194a3a838ea08
Tanguy Antoine <antoine@bearer.sh>
2019-03-09 21:51:23 +0100

    feat(cli): add data and file option

--- packages/cli/src/commands/invoke.ts
+++ packages/cli/src/commands/invoke.ts
@@ -1,6 +1,7 @@
 import { flags } from '@oclif/command'
 
 import * as path from 'path'
+import * as fs from 'fs'
 import * as tsNode from 'ts-node'
 import * as cosmiconfig from 'cosmiconfig'
 import BaseCommand from '../base-command'
@@ -20,7 +21,7 @@ export default class Invoke extends BaseCommand {
   static args = [{ name: 'Function_Name', required: true }]
 
   async run() {
-    const { args } = this.parse(Invoke)
+    const { args, flags } = this.parse(Invoke)
     const funcName = args.Function_Name
     this.debug(funcName)
     tsNode.register({
@@ -38,26 +39,55 @@ export default class Invoke extends BaseCommand {
       this.error(e.message)
     }
 
-    const explorer = cosmiconfig(LOCAL_DEV_CONFIGURATION, {
-      searchPlaces: [`config.${LOCAL_DEV_CONFIGURATION}.js`]
-    })
-    const { config: devFunctionsContext = {} } = (await explorer.search(this.locator.integrationRoot)) || {}
+    // manually injected data
+    const { config = {} }: TDevConfig = (await this.devConfig.search(this.locator.integrationRoot)) || {}
+    // data sent to the event
+    const body = flags.data || (flags.file && this.getFilecontent(flags.file)) || '{}'
+    // injected within the function (let's remove it?)
     const bearerBaseURL = 'ok'
-    // TODO: access sqlite database and load defined data
+    // TODO: access sqlite database and load defined data from params sent
     const userDefinedData = {}
-    const datum = await func.init()(
-      {
-        context: {
-          ...devFunctionsContext.global,
-          ...devFunctionsContext[funcName],
-          bearerBaseURL,
-          ...userDefinedData
-        },
-        body: JSON.stringify({})
-      },
-      {}
-    )
+    this.ensureJson(body)
 
+    const datum = await func.init()({
+      body,
+      context: {
+        ...config.global,
+        ...config[funcName],
+        bearerBaseURL,
+        ...userDefinedData
+      }
+    })
+    // print output
     console.log(JSON.stringify(datum, null, 2))
   }
+
+  get devConfig() {
+    return cosmiconfig(LOCAL_DEV_CONFIGURATION, {
+      searchPlaces: [`config.${LOCAL_DEV_CONFIGURATION}.js`]
+    })
+  }
+
+  getFilecontent = (filePath: string): string | undefined => {
+    const location = path.resolve(filePath)
+    if (filePath) {
+      return fs.existsSync(location)
+        ? fs.readFileSync(location, { encoding: 'utf8' })
+        : this.error(`File not found: ${location}`)
+    }
+    return undefined
+  }
+
+  ensureJson = (maybeJson: string) => {
+    try {
+      JSON.parse(maybeJson)
+    } catch (e) {
+      this.error('Invalid JSON provided')
+    }
+  }
+}
+
+type TDevConfig = {
+  global?: any
+  [key: string]: any
 }
