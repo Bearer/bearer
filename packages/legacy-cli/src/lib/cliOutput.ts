b10efe3a8942a1129d19f81a3ed4e8297bccb67b
Tanguy Antoine <antoine@bearer.sh>
2019-03-20 19:04:02 +0100

    refactor(cli): remove unnecessary prefix

--- packages/legacy-cli/src/lib/cliOutput.ts
+++ packages/legacy-cli/src/lib/cliOutput.ts
@@ -13,16 +13,13 @@ function outputError(error) {
 
 export default emitter => {
   emitter.on('developerPortalUpdate:failed', error => {
-    term.white('Bearer: ')
     term.red('Failed pushing to developer portal.\n')
-    term.white('Bearer: ')
     term.yellow('error message: ').red(error)
     term('\n')
     outputError(error)
   })
 
   emitter.on('developerPortalUpdate:error', error => {
-    term.white('Bearer: ')
     term.red('There was an error while pushing to developer portal.')
     term('\n')
     outputError(error)
@@ -31,36 +28,30 @@ export default emitter => {
   /* ********* Start output ********* */
 
   emitter.on('start:prepare:installingDependencies', () => {
-    term.white('Bearer: ')
     term.yellow('Installing views dependencies.')
     term('\n')
   })
 
   emitter.on('start:watchers', () => {
-    term.white('Bearer: ')
     term.yellow('Starting watchers')
     term('\n')
   })
 
   emitter.on('start:watchers:stdout', ({ name, data }) => {
-    term.white('Bearer: ')
     term.yellow(`[watcher:${name}] `)
     term.green(data)
   })
 
   emitter.on('start:watchers:stderr', ({ name, data }) => {
-    term.white('Bearer: ')
     term.yellow(`[watcher:${name}] `)
     term.green(data)
   })
 
   emitter.on('start:watchers:close', ({ name, code }) => {
-    term.white('Bearer: ')
     term.yellow(`[watcher:${name}] closed exit code: ${code}\n`)
   })
 
   emitter.on('start:prepare:failed', ({ error }) => {
-    term.white('Bearer: ')
     term.red('Prepare : An error occured')
     term('\n')
     term.white('    Error: ')
@@ -69,7 +60,6 @@ export default emitter => {
   })
 
   emitter.on('start:failed', ({ error }) => {
-    term.white('Bearer: ')
     term.red('An error occured')
     term('\n')
     term.white('    Error: ')
@@ -78,7 +68,6 @@ export default emitter => {
   })
 
   emitter.on('start:localServer:start', ({ port }) => {
-    term.white('Bearer: ')
     term.yellow('[local:functionServer] ')
     term.yellow('Serving: ')
     term(`http://127.0.0.1:${port}`)
@@ -87,7 +76,6 @@ export default emitter => {
 
   emitter.on('start:localServer:customPort', ({ port, host }) => {
     term('*************** Action required *****************\n')
-    term.white('Bearer: ')
     term.red('[local:functionServer] You have specified a custom port.\n')
     term.yellow('You must update the views/index.html to match this setting as follow:\n\n')
     term(`<script> bearer("CLIENT_ID", { integrationHost: "${host}" }) </script>\n\n`)
@@ -95,7 +83,6 @@ export default emitter => {
   })
 
   emitter.on('start:localServer:endpoints', ({ endpoints }) => {
-    term.white('Bearer: ')
     term.yellow('[local:functionServer] ')
     term.yellow('paths:')
     term('\n\t* ')
@@ -104,21 +91,18 @@ export default emitter => {
   })
 
   emitter.on('start:localServer:generatingFunctions:start', () => {
-    term.white('Bearer: ')
     term.yellow('[local:functionServer] ')
     term.yellow('Reloading functions')
     term('\n')
   })
 
   emitter.on('start:localServer:generatingFunctions:stop', () => {
-    term.white('Bearer: ')
     term.yellow('[local:functionServer] ')
     term.yellow('Functions reloaded')
     term('\n')
   })
 
   emitter.on('start:localServer:generatingFunctions:failed', ({ error }) => {
-    term.white('Bearer: ')
     term.yellow('[local:functionServer] ')
     term.red('Functions building error\n')
     logger(
