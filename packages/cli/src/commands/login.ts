b25f2b348937648c183402051cc598ec6fdf24d6
Tanguy Antoine <antoine@bearer.sh>
2019-04-01 16:08:18 +0200

    feat(cli): fetch access token after login

--- packages/cli/src/commands/login.ts
+++ packages/cli/src/commands/login.ts
@@ -1,14 +1,22 @@
 import { flags } from '@oclif/command'
 import getPort from 'get-port'
-import BaseCommand from '../base-command'
 import * as http from 'http'
+import axios from 'axios'
+import * as opn from 'opn'
+import * as crypto from 'crypto'
 
-// const tokenUrl = 'https://app.bearer.sh/settings'
+import BaseCommand from '../base-command'
+
+// const domain = 'login.bearer.sh'
 
 const BEARER_LOGIN_PORT = 56789
+const domain = 'https://login.bearer.sh'
+const callbackurl = `http://localhost:4444`
 
 export default class Login extends BaseCommand {
   _server?: http.Server
+  _verifier!: string
+  _challenge!: string
 
   static description = 'Login to Bearer platform'
 
@@ -20,45 +28,24 @@ export default class Login extends BaseCommand {
   static args = []
 
   async run() {
-    const port = await getPort({ port: BEARER_LOGIN_PORT })
-    if (port !== BEARER_LOGIN_PORT) {
-      this.error(`bearer login requires port ${BEARER_LOGIN_PORT} to be available`)
+    this._server = await this.startServer()
+    this._verifier = base64URLEncode(crypto.randomBytes(32))
+    this._challenge = base64URLEncode(sha256(this._verifier))
+    this.ux.action.start('Logging you in')
+    const scopes = 'profile+offline_access'
+    const audience = `cli-${process.env.BEARER_ENV || 'production'}`
+    const params = {
+      audience,
+      scope: scopes,
+      response_type: 'code',
+      client_id: this.clientId,
+      code_challenge: this._challenge,
+      code_challenge_method: 'S256',
+      redirect_uri: callbackurl
     }
-    this.debug('starting server')
-    this._server = http
-      .createServer((request, response) => {
-        let body = ''
-        request.on('data', chunk => {
-          body += chunk
-        })
-        request.on('end', () => {
-          try {
-            const data = JSON.parse(body)
-            this.debug(data)
-            response.setHeader('Access-Control-Allow-Origin', this.bearerConfig.DeveloperPortalUrl)
-            response.write('OK')
-            response.end()
-            this.stopServer()
-          } catch (e) {
-            this.debug(e)
-          }
-        })
-      })
-      .listen(BEARER_LOGIN_PORT)
-
-    // start server on specific port
-    //   if not started raise
-    // open browser
-    // on response save code
-    // ensure server is shutdown
-
-    // const { flags } = this.parse(Login)
-    // const email = process.env.BEARER_EMAIL || flags.email || (await this.askForString('Enter your email'))
-    // const token = process.env.BEARER_TOKEN || (await this.askToken())
-    // this.ux.action.start('Logging you in')
-    // const status = await this.logUser(email, token)
-    // this.ux.action.stop()
-    // this.log(status)
+    this.debug('authoriwe params %j', params)
+    const url = `${domain}/authorize?${toParams(params)}`
+    opn(url)
   }
 
   private stopServer = () => {
@@ -70,46 +57,83 @@ export default class Login extends BaseCommand {
     }
   }
 
-  // async logUser(username: string, accessToken: string): Promise<string> {
-  //   try {
-  //     const { statusCode, body } = await this.serviceClient.login({ Username: username, Password: accessToken })
-  //     switch (statusCode as number) {
-  //       case 200: {
-  //         this.bearerConfig.storeBearerConfig({
-  //           ...body.user,
-  //           ExpiresAt: Date.now() + body.authorization.AuthenticationResult.ExpiresIn * 1000,
-  //           authorization: body.authorization
-  //         })
-  //         return `Successfully logged in as ${username}! 🤘`
-  //       }
+  private startServer = async (): Promise<http.Server> => {
+    const port = await getPort({ port: BEARER_LOGIN_PORT })
+    return new Promise((resolve, reject) => {
+      if (port !== BEARER_LOGIN_PORT) {
+        this.error(`bearer login requires port ${BEARER_LOGIN_PORT} to be available`)
+        reject()
+      }
+      this.debug('starting server')
+      const server = http
+        .createServer((request, response) => {
+          let body = ''
+          request.on('data', chunk => {
+            body += chunk
+          })
+          request.on('end', () => {
+            try {
+              const data: { code: string } = JSON.parse(body)
+              this.debug(data)
+              response.setHeader('Access-Control-Allow-Origin', this.bearerConfig.DeveloperPortalUrl)
+              response.write('OK')
+              response.end()
+              this.getToken(data.code)
+              this.stopServer()
+            } catch (e) {
+              this.debug(e)
+            }
+          })
+        })
+        .listen(BEARER_LOGIN_PORT, () => resolve(server))
+    })
+  }
 
-  //       case 401: {
-  //         this.error('Unauthorized: Invalid credentials')
-  //         return 'There was an error while trying to login to bearer'
-  //       }
+  getToken = async (code: string) => {
+    try {
+      const token = await axios.post<IToken>(`${domain}/oauth/token`, {
+        code,
+        grant_type: 'authorization_code',
+        client_id: `${this.clientId}`,
+        code_verifier: `${this._verifier}`,
+        redirect_uri: callbackurl
+      })
+      this.debug(token)
+      this.ux.action.stop()
+    } catch (e) {
+      this.error(e)
+    }
+  }
 
-  //       default: {
-  //         this.error('Unhandled status')
-  //         this.debug('status:', statusCode, 'body:', JSON.stringify(body))
-  //         return 'An error occured. Please retry'
-  //       }
-  //     }
-  //   } catch (e) {
-  //     this.error(e)
-  //     return 'An error occured'
-  //   }
-  // }
+  get clientId(): string {
+    return 'yL4bcogYQCa7pwE2Xkc0MfDocDBBI3G3'
+  }
+}
 
-  // async askToken(): Promise<string> {
-  //   this.log('')
-  //   this.log(this.colors.italic(`Find your token at this location: ${this.colors.bold(tokenUrl)}`))
-  //   const { token } = await this.inquirer.prompt<{ token: string }>([
-  //     {
-  //       message: 'Enter your token:',
-  //       type: 'password',
-  //       name: 'token'
-  //     }
-  //   ])
-  //   return token
-  // }
+function toParams(obj: Record<string, string | number>) {
+  return Object.keys(obj)
+    .map(key => [key, obj[key]].join('='))
+    .join('&')
+}
+
+function base64URLEncode(str: Buffer) {
+  return str
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '')
+}
+function sha256(str: string) {
+  return crypto
+    .createHash('sha256')
+    .update(str)
+    .digest()
+}
+
+interface IToken {
+  access_token: string
+  refresh_token: string
+  scope: string
+  expires_in: number
+  token_type: string
 }
