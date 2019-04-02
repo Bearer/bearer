3f6a108b8c6e81b5bcd6d82e5d4935fa7e87f910
Tanguy Antoine <antoine@bearer.sh>
2019-04-02 16:37:33 +0200

    feat(cli): rework bearer link to choose integration from list

--- packages/cli/src/commands/link.ts
+++ packages/cli/src/commands/link.ts
@@ -1,5 +1,6 @@
 import BaseCommand from '../base-command'
 import { RequireIntegrationFolder } from '../utils/decorators'
+import { getIntegrations, Integration } from '../utils/devPortal'
 
 export default class Link extends BaseCommand {
   static description = 'Link your local integration to a remote one'
@@ -8,13 +9,29 @@ export default class Link extends BaseCommand {
     ...BaseCommand.flags
   }
 
-  static args = [{ name: 'Integration_Identifier', required: true }]
+  static args = [{ name: 'Integration_Identifier' }]
 
   @RequireIntegrationFolder()
   async run() {
     const { args } = this.parse(Link)
-    const identifier = args.Integration_Identifier
-    const { integrationTitle } = this.bearerConfig as { integrationTitle: string }
+    let identifier = args.Integration_Identifier
+    let { integrationTitle } = this.bearerConfig as { integrationTitle: string }
+    if (!identifier) {
+      const { integrations } = await getIntegrations(this)
+      const { integration } = await this.inquirer.prompt<{ integration: Integration }>([
+        {
+          name: 'integration',
+          type: 'list',
+          choices: integrations.map(int => ({
+            name: `${int.uuid} - ${int.name}`,
+            value: int
+          }))
+        }
+      ])
+      integrationTitle = integration.name
+      identifier = integration.deprecated_uuid
+    }
+
     const [orgId, integrationId] = identifier.replace(/\-/, '|').split('|')
     const integrationRc = { orgId, integrationId, integrationTitle }
     this.bearerConfig.setIntegrationConfig(integrationRc)
