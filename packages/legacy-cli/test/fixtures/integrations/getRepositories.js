b552bc26767dd71597202f644baf0a9306edee8c
Tanguy Antoine <antoine@bearer.sh>
2019-04-09 14:13:33 +0200

    test(legacy-cli): remove dead code

--- packages/legacy-cli/test/fixtures/integrations/getRepositories.js
+++
@@ -1,6 +0,0 @@
-const { getRepositories } = require('./github')
-const { FetchData } = require('@bearer/functions')
-
-module.exports.action = getRepositories
-module.exports.functionType = FetchData
-module.exports.functionName = 'getRepositories'
