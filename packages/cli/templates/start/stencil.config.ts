ca43e43461cfc0d8a9850e560608f884389f9125
Tanguy Antoine <antoine@bearer.sh>
2019-04-10 12:55:55 +0200

    fix(cli): prefix namespace

--- packages/cli/templates/start/stencil.config.ts
+++ packages/cli/templates/start/stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core'
 const { plugins } = require('@bearer/core/lib/plugins')
 
 export const config: Config = {
-  namespace: process.env.BEARER_INTEGRATION_ID,
+  namespace: 'bearer-' + process.env.BEARER_INTEGRATION_ID,
   enableCache: false,
   copy: [
     {
