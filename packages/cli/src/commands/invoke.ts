12defdf904687754c950935e2c36c9a8f32ed246
Guillaume Montard <guillaume.montard@gmail.com>
2019-04-12 16:36:02 -0700

    Update invoke.ts

--- packages/cli/src/commands/invoke.ts
+++ packages/cli/src/commands/invoke.ts
@@ -3,7 +3,7 @@ import { flags } from '@oclif/command'
 import BaseLegacyCommand from '../base-legacy-command'
 
 export default class Invoke extends BaseLegacyCommand {
-  static description = 'invoke Function locally'
+  static description = 'invoke function locally'
 
   static flags = {
     help: flags.help({ char: 'h' }),
