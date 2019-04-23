7f47e167d3a09f2dbfe3606d442a9cb5eee70ceb
Tanguy Antoine <antoine@bearer.sh>
2019-04-23 17:07:24 +0200

    fix(cli): add support for local redirect
    all browser do not support XHR to localhost while being on https website, with the current implementation we don't have anymore issue since the page return by IS redirect to localhost

--- packages/cli/src/commands/setup/auth.ts
+++ packages/cli/src/commands/setup/auth.ts
@@ -1,15 +1,16 @@
-import * as http from 'http'
 import * as fs from 'fs'
+import * as http from 'http'
 import axios from 'axios'
 import { modify, applyEdits } from 'jsonc-parser'
-
+import * as express from 'express'
 import { TConfig, Authentications, configs } from '@bearer/types/lib/authentications'
 import { contexts } from '@bearer/functions/lib/declaration'
-import BaseCommand from '../../base-command'
-// @ts-ignore
-import * as open from 'open'
 import getPort from 'get-port'
 
+// @ts-ignore
+import * as open from 'open'
+
+import BaseCommand from '../../base-command'
 import { RequireIntegrationFolder } from '../../utils/decorators'
 import { BEARER_AUTH_PORT } from '../../utils/constants'
 import { askForString, askForPassword } from '../../utils/prompts'
@@ -26,15 +27,18 @@ export default class SetupAuth extends BaseCommand {
   _server?: http.Server
   _verifier!: string
   _challenge!: string
-  private _listerners!: Record<Event, ((data: any) => void)[]>
+  private _listerners: {
+    success: ((data: TBase64EncodedString) => void)[]
+    error: ((data: any) => void)[]
+    shutdown: ((data: any) => void)[]
+  } = {
+    success: [],
+    error: [],
+    shutdown: []
+  }
 
   @RequireIntegrationFolder()
   async run() {
-    this._listerners = {
-      success: [],
-      error: [],
-      shutdown: []
-    }
     const config: TConfig = JSON.parse(
       fs.readFileSync(this.locator.authConfigPath, {
         encoding: 'utf8'
@@ -48,9 +52,20 @@ export default class SetupAuth extends BaseCommand {
         const clientSecret = BEARER_AUTH_CLIENT_SECRET || (await askForString('Client secret', { type: 'password' }))
         const newConfig = { ...config, clientID, clientSecret }
         this.debug('Your credentials:\n%j', { ...config, clientID, clientSecret: clientSecret.replace(/./g, '*') })
-        const { token: accessToken } = await this.fetchAuthToken(newConfig as configs.TOAuth2Config)
-
-        await this.persistSetup({ accessToken } as contexts.OAuth2)
+        const token = await this.fetchAuthToken(newConfig as configs.TOAuth2Config)
+        const setup = JSON.parse(Buffer.from(token, 'base64').toString('ascii')) as contexts.OAuth2
+        await this.persistSetup(setup)
+        break
+      }
+      case Authentications.OAuth1: {
+        const consumerKey = process.env.BEARER_AUTH_CONSUMER_KEY || (await askForString('Consumer key'))
+        const consumerSecret = process.env.BEARER_AUTH_CONSUMER_SECRET || (await askForPassword('Consumer secret'))
+        this.debug('Your credentials:\n%j', { consumerKey, consumerSecret: consumerSecret.replace(/./g, '*') })
+        const newConfig = { ...config, consumerKey, consumerSecret }
+        const token = await this.fetchAuthToken(newConfig as configs.TOAuth1Config)
+        const setup = JSON.parse(Buffer.from(token, 'base64').toString('ascii')) as contexts.OAuth1
+        // TODO: fix this when context.OAuth1 is fixed  and well defined
+        await this.persistSetup(setup)
         break
       }
 
@@ -63,16 +78,6 @@ export default class SetupAuth extends BaseCommand {
       case Authentications.ApiKey: {
         const apiKey = await askForPassword('API Key')
         await this.persistSetup({ apiKey } as contexts.ApiKey)
-        break
-      }
-      case Authentications.OAuth1: {
-        const consumerKey = process.env.BEARER_AUTH_CONSUMER_KEY || (await askForString('Consumer key'))
-        const consumerSecret = process.env.BEARER_AUTH_CONSUMER_SECRET || (await askForPassword('Consumer secret'))
-        this.debug('Your credentials:\n%j', { consumerKey, consumerSecret: consumerSecret.replace(/./g, '*') })
-        const newConfig = { ...config, consumerKey, consumerSecret }
-        const { token: accessToken } = await this.fetchAuthToken(newConfig as configs.TOAuth1Config)
-        // TODO: fix this when context.OAuth1 is fixed  and well defined
-        await this.persistSetup({ accessToken, consumerKey } as any)
         break
       }
       case Authentications.Custom:
@@ -89,15 +94,15 @@ export default class SetupAuth extends BaseCommand {
     }
   }
 
-  fetchAuthToken = async (config: configs.TOAuth2Config | configs.TOAuth1Config): Promise<{ token: string }> => {
+  fetchAuthToken = async (config: configs.TOAuth2Config | configs.TOAuth1Config): Promise<TBase64EncodedString> => {
     return new Promise(async (resolve, reject) => {
       this._server = await this.startServer()
-      const location = await axios
+      const redirectLocation = await axios
         .post(`${this.constants.IntegrationServiceHost}v2/auth/local-auth`, { config }, { maxRedirects: 0 })
         .catch(e => e.response.headers.location)
-      this.debug(config)
 
-      this.on<{ token: string }>('success', data => {
+      // add listeners so that we can handle redirect and fallback the same way
+      this.on<TBase64EncodedString>('success', data => {
         resolve(data)
       })
 
@@ -105,19 +110,24 @@ export default class SetupAuth extends BaseCommand {
         this.debug(data)
         reject('Error while receiving token')
       })
-      const url = `${this.constants.IntegrationServiceHost}v2/auth/${location.replace('./', '')}&clientId=NONE`
+
+      const { url, fallback } = getOpeningUrls(
+        `${this.constants.IntegrationServiceHost}v2/auth/${redirectLocation.replace('./', '')}&clientId=NONE`
+      )
+      this.debug('config: %j , location: %s', config, url)
       const a = await open(url)
       a.on('close', (code: any, signal: any) => {
         if (code !== 0) {
+          // we are no able to open a browser so we ask the user to fill with the token
           this.stopServer()
           this.warn(
             this.colors.yellow(`Unable to open a browser. If you want to retrieve a token please follow these steps\n`)
           )
-          this.log(this.colors.bold('1/ access the url below  and follow the login process:\n\n'), url)
+          this.log(this.colors.bold('1/ access the url below  and follow the login process:\n\n'), fallback)
           this.log()
           this.log(this.colors.bold(`2/ when you access the success page copy the token and paste it here`))
-          askForString('Token').then(token => {
-            resolve({ token })
+          askForString('Token').then((token: TBase64EncodedString) => {
+            resolve(token)
           })
         }
       })
@@ -129,7 +139,7 @@ export default class SetupAuth extends BaseCommand {
     if (this._server) {
       this._server.close(() => {
         this.debug('server stopped')
-        this._listerners['shutdown'].map(cb => cb({}))
+        this._listerners.shutdown.map(cb => cb({}))
       })
     }
   }
@@ -142,42 +152,25 @@ export default class SetupAuth extends BaseCommand {
         reject()
       }
       this.debug('starting server')
-      const server = http
-        .createServer((request, response) => {
-          let body = ''
-          request.on('data', chunk => {
-            body += chunk
-          })
-          request.on('end', () => {
-            try {
-              const data: { token: string } = JSON.parse(body)
-              this.debug(data)
-              response.setHeader(
-                'Access-Control-Allow-Origin',
-                process.env.AUTH_ALLOWED_ORIGIN || this.constants.IntegrationServiceUrl
-              )
-              response.setHeader('Connection', 'close')
-              response.write('OK')
-              response.end()
-              this._listerners['success'].map(cb => cb(data))
-            } catch (e) {
-              this._listerners['error'].map(cb => cb(e))
-            }
-            this.stopServer()
-          })
-        })
-        .listen(BEARER_AUTH_PORT, () => {
-          this.debug('server started')
-          resolve(server)
-        })
+      const app = express()
+      app.get('/setup_auth_callback', (req: express.Request, res: express.Response) => {
+        const token: TBase64EncodedString = req.query.token || ''
+        try {
+          this._listerners.success.map(cb => cb(token))
+          res.setHeader('Connection', 'close')
+          res.send('OK;').end()
+          this.stopServer()
+        } catch (e) {}
+      })
+
+      const server = app.listen(BEARER_AUTH_PORT, () => {
+        this.debug('server started')
+        resolve(server)
+      })
     })
   }
 
-  on = <T>(event: Event, callback: (data: T) => void) => {
-    this._listerners[event] = [...this._listerners[event], callback as any]
-  }
-
-  persistSetup(config: contexts.OAuth2 | contexts.OAuth1 | contexts.ApiKey | contexts.Basic) {
+  private persistSetup(config: contexts.OAuth2 | contexts.OAuth1 | contexts.ApiKey | contexts.Basic) {
     const setupRc = this.locator.localConfigPath
     if (!fs.existsSync(setupRc)) {
       fs.writeFileSync(setupRc, '{}', { encoding: 'utf8' })
@@ -195,10 +188,22 @@ export default class SetupAuth extends BaseCommand {
     fs.writeFileSync(setupRc, newSetupRc, { encoding: 'utf8' })
     this.success(`Auth credentials have been saved to ${this.locator.toRelative(setupRc)}`)
   }
+
+  private on = <T>(event: Event, callback: (data: T) => void) => {
+    this._listerners[event] = [...this._listerners[event], callback as any]
+  }
 }
 
+function getOpeningUrls(url: string) {
+  return {
+    url: `${url}&localHostRedirectSupported=inline`,
+    fallback: `${url}&localHostRedirectSupported=inline`
+  }
+}
 class UnreachableCaseError extends Error {
   constructor(val: never) {
     super(`Unreachable case: ${val}`)
   }
 }
+
+type TBase64EncodedString = string
