21013e3d4ccdcc5b6dded776ad2badc2075e6c5d
elsapet <elizabeth@bearer.sh>
2019-05-28 09:58:19 +0100

    fix: look for Bearer API key in ENV vars

--- packages/cli/src/utils/devPortal.ts
+++ packages/cli/src/utils/devPortal.ts
@@ -16,18 +16,35 @@ export const devPortalClient = (command: BaseCommand) => {
     }
   )
   async function requestFunction<DataReturned>(data: { query: string; variables?: any }) {
+    return await instance.post<{ data?: DataReturned; errors?: any }>('', data, {
+      headers: {
+        Authorization: `Bearer ${access_token}`
+      }
+    })
+  }
+
+  async function access_token() {
     let token = await command.bearerConfig.getToken()
+
+    // If no token is configured, do we have an ENV var
+    // available to access the Bearer API?
+    if (!token && process.env.BEARER_API_KEY) {
+      return process.env.BEARER_API_KEY
+    }
+
+    // No token configured and no API key
+    // Let's ask the user to log in
     if (!token) {
       command.debug('no token found, trying to log you in')
       await promptToLogin(command)
       token = await command.bearerConfig.getToken()
     }
 
-    return await instance.post<{ data?: DataReturned; errors?: any }>('', data, {
-      headers: {
-        Authorization: `Bearer ${token!.access_token}`
-      }
-    })
+    if (token) {
+      return token.access_token
+    }
+
+    return ''
   }
 
   return {
