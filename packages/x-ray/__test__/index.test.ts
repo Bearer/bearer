cc18034cf65f75b6dbdc8830198bc955a9203b1d
Tanguy Antoine <antoine@bearer.sh>
2019-06-24 10:54:12 +0200

    test: ensure modules are correctly overriden

--- packages/x-ray/__test__/index.test.ts
+++ packages/x-ray/__test__/index.test.ts
@@ -1,44 +1,26 @@
 import { captureHttps, setupFunctionIdentifiers } from '../src/index'
-import { httpClient, event } from './helpers/utils'
-import http from 'http'
+import { event } from './helpers/utils'
 
 jest.mock('../src/constants')
 
 describe('captureHttp', () => {
-  it('captures https without identifiers', async () => {
-    captureHttps(httpClient)
-    expect((httpClient as any)._request).toBeDefined()
-    await new Promise((res, _rej) => {
-      httpClient.request('http://www.google.com/', (data: http.IncomingMessage) => {
-        res(data)
-        data.resume()
-      })
-    })
+  it('overrides http and https modules', async () => {
+    expect(require('http')._bearerLoading).toBeUndefined()
+    expect(require('https')._bearerLoading).toBeUndefined()
 
-    expect(process.env.clientId).toEqual(undefined)
-    expect(process.env.scenarioUuid).toEqual(undefined)
-  })
+    captureHttps()
 
-  it('captures https with identifiers', async () => {
-    const functionEvent = event()
-    captureHttps(httpClient)
-    setupFunctionIdentifiers(functionEvent)
-    await new Promise((res, _rej) => {
-      httpClient.request('http://www.google.com/', (data: http.IncomingMessage) => {
-        res(data)
-        data.resume()
-      })
-    })
-
-    expect(process.env.clientId).toEqual(functionEvent.context.clientId)
-    expect(process.env.scenarioUuid).toEqual(functionEvent.context.integrationUuid)
+    expect(require('http')._bearerLoading).toBeDefined()
+    expect(require('https')._bearerLoading).toBeDefined()
   })
 })
 
 describe('setupFunctionIdentifiers', () => {
   it('setup all function identifiers', () => {
     const functionEvent = event()
+
     setupFunctionIdentifiers(functionEvent)
+
     expect(process.env.clientId).toEqual(functionEvent.context.clientId)
     expect(process.env.scenarioUuid).toEqual(functionEvent.context.integrationUuid)
   })
--- packages/x-ray/src/index.ts
+++ packages/x-ray/src/index.ts
@@ -1,20 +1,21 @@
 import logger from './logger'
 import { overrideRequestMethod } from './http-overrides'
 
-let check: number
+let check: string
 
 export const captureHttps = () => {
   const httpModule = require('http')
+  const httpsModule = require('https')
+
   if (httpModule._bearerLoading === check && check) {
     logger('%j', { message: 'http module has already been loaded', application: 'x-ray' })
     return
   }
-  httpModule._bearerLoading = check = Math.random()
+  httpsModule._bearerLoading = httpModule._bearerLoading = check = [Math.random(), Date.now()].join('/')
   logger('%j', { message: 'Overriding request and get methods', application: 'x-ray' })
+
   overrideRequestMethod(httpModule)
-  // It seems like https is using http behind the scenes
-  // const httpsModule = require('https')
-  // overrideRequestMethod(httpsModule)
+  overrideRequestMethod(httpsModule)
 }
 
 export const setupFunctionIdentifiers = function(event: any) {
