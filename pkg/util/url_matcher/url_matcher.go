97789d3a9b77fa7e2f39cbe1dc30f283b80463a3
elsapet <elizabeth@bearer.sh>
2022-10-12 12:35:40 +0200

    feat: add URL matcher util

---
+++ pkg/util/url_matcher/url_matcher.go
@@ -0,0 +1,67 @@
+package url_matcher
+
+import (
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/weppos/publicsuffix-go/publicsuffix"
+)
+
+const prefixPattern = "(?P<match>\\A(?:[^:]+://)?(?:[^/]+\\.)?"
+const suffixPattern = "(?:/|\\z)"
+
+type ComparableUrls struct {
+	DetectionURL string
+	RecipeURL    string
+}
+
+func UrlMatcher(urls ComparableUrls) (string, error) {
+	parsedURL, err := url.Parse(urls.RecipeURL)
+	if err != nil {
+		return "", err
+	}
+
+	parsedDomain, err := publicsuffix.Parse(parsedURL.Host)
+	if err != nil {
+		return "", err
+	}
+
+	matcher, _ := regexp.Compile(prefixPattern + domainPattern(parsedDomain) + pathPattern(parsedURL) + ")" + suffixPattern)
+	match := matcher.FindStringSubmatch(urls.DetectionURL)
+	if match != nil {
+		for i, name := range matcher.SubexpNames() {
+			if name == "match" {
+				return match[i], nil
+			}
+		}
+	}
+	return "", nil
+}
+
+func domainPattern(parsedDomain *publicsuffix.DomainName) string {
+	var domainPatterns []string
+	if parsedDomain.TRD != "" {
+		domainPatterns = append(domainPatterns, wildcardPattern(parsedDomain.TRD, "."))
+	}
+	if parsedDomain.SLD != "" {
+		domainPatterns = append(domainPatterns, regexp.QuoteMeta(parsedDomain.SLD))
+	}
+	if parsedDomain.TLD != "" {
+		domainPatterns = append(domainPatterns, regexp.QuoteMeta(parsedDomain.TLD))
+	}
+
+	return strings.Join(domainPatterns, "\\.")
+}
+
+func pathPattern(parsedURL *url.URL) string {
+	return wildcardPattern(strings.TrimSuffix(parsedURL.Path, "/"), "/")
+}
+
+func wildcardPattern(myString string, separator string) string {
+	var parts []string
+	for _, part := range strings.Split(myString, separator) {
+		parts = append(parts, "("+strings.ReplaceAll(regexp.QuoteMeta(part), "\\*", ".+")+"|\\*)")
+	}
+	return strings.Join(parts, regexp.QuoteMeta(separator))
+}
---
+++ pkg/util/url_matcher/url_matcher_test.go
@@ -0,0 +1,216 @@
+package url_matcher_test
+
+import (
+	"testing"
+
+	"github.com/bearer/curio/pkg/util/url_matcher"
+	"github.com/stretchr/testify/assert"
+)
+
+type urlMatcherTestCase struct {
+	Name         string
+	DetectionURL string
+	RecipeURL    string
+	Want         string
+	Skip         bool
+}
+
+func TestUrlMatcher(t *testing.T) {
+	tests := []urlMatcherTestCase{
+		{
+			Name:         "when the urls are just domains and match exactly",
+			RecipeURL:    "https://api.bearer.com",
+			DetectionURL: "https://api.bearer.com",
+			Want:         "https://api.bearer.com",
+			Skip:         false,
+		},
+		{
+			Name:         "when the urls are just domains and don't match",
+			RecipeURL:    "https://api.bearer.com",
+			DetectionURL: "https://docs.bearer.com",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url has a subdomain prefix",
+			RecipeURL:    "https://api.bearer.com",
+			DetectionURL: "https://test.api.bearer.com",
+			Want:         "https://test.api.bearer.com",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url has a non-subdomain prefix",
+			RecipeURL:    "https://api.bearer.com",
+			DetectionURL: "https://testapi.bearer.com",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the url has a subdomain prefix",
+			RecipeURL:    "https://test.api.bearer.com",
+			DetectionURL: "https://api.bearer.com",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url contains a wildcard for an entire domain segment",
+			RecipeURL:    "https://s3.eu.amazonaws.com",
+			DetectionURL: "https://s3.*.amazonaws.com",
+			Want:         "https://s3.*.amazonaws.com",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url contains a wildcard for the tld",
+			RecipeURL:    "https://api.bearer.com",
+			DetectionURL: "https://api.bearer.*",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url contains a wildcard for the sld",
+			RecipeURL:    "https://api.bearer.com",
+			DetectionURL: "https://api.*.com",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url contains a wildcard for part of a domain segment",
+			RecipeURL:    "https://s3.eu-west.amazonaws.com",
+			DetectionURL: "https://s3.eu-*.amazonaws.com",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the url contains a wildcard for an entire domain segment",
+			RecipeURL:    "https://s3.*.amazonaws.com",
+			DetectionURL: "https://s3.eu.amazonaws.com",
+			Want:         "https://s3.eu.amazonaws.com",
+			Skip:         false,
+		},
+		{
+			Name:         "when the url contains a wildcard for part of a domain segment",
+			RecipeURL:    "https://s3.eu-*.amazonaws.com",
+			DetectionURL: "https://s3.eu-west.amazonaws.com",
+			Want:         "https://s3.eu-west.amazonaws.com",
+			Skip:         false,
+		},
+		{
+			Name:         "when the url contains a wildcard matching across detection url segments",
+			RecipeURL:    "https://api.*.amazonaws.com",
+			DetectionURL: "https://api.s3.eu-west.amazonaws.com",
+			Want:         "https://api.s3.eu-west.amazonaws.com",
+			Skip:         false,
+		},
+		{
+			Name:         "when the urls include a path and match exactly",
+			RecipeURL:    "https://api.bearer.com/path",
+			DetectionURL: "https://api.bearer.com/path",
+			Want:         "https://api.bearer.com/path",
+			Skip:         false,
+		},
+		{
+			Name:         "when the urls include a path and the path doesn't match",
+			RecipeURL:    "https://api.bearer.com/path",
+			DetectionURL: "https://api.bearer.com/otherpath",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the urls match but the url has a trailing slash",
+			RecipeURL:    "https://api.bearer.com/path/",
+			DetectionURL: "https://api.bearer.com/path",
+			Want:         "https://api.bearer.com/path",
+			Skip:         false,
+		},
+		{
+			Name:         "when the urls match but the detection url has a trailing slash",
+			RecipeURL:    "https://api.bearer.com/path",
+			DetectionURL: "https://api.bearer.com/path/",
+			Want:         "https://api.bearer.com/path",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url has a path segment suffix",
+			RecipeURL:    "https://api.bearer.com/path",
+			DetectionURL: "https://api.bearer.com/path/other",
+			Want:         "https://api.bearer.com/path",
+			Skip:         false,
+		},
+		{
+			Name:         "when the url has a path segment suffix",
+			RecipeURL:    "https://api.bearer.com/path/other",
+			DetectionURL: "https://api.bearer.com/path",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url has a non-segment path suffix",
+			RecipeURL:    "https://api.bearer.com/path",
+			DetectionURL: "https://api.bearer.com/pathother",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url contains a wildcard for an entire path segment",
+			RecipeURL:    "https://bearer.com/api/v2",
+			DetectionURL: "https://bearer.com/api/*",
+			Want:         "https://bearer.com/api/*",
+			Skip:         false,
+		},
+		{
+			Name:         "when the detection url contains a wildcard for part of a path segment",
+			RecipeURL:    "https://bearer.com/api/v2",
+			DetectionURL: "https://bearer.com/api/v*",
+			Want:         "",
+			Skip:         false,
+		},
+		{
+			Name:         "when the url contains a wildcard for an entire path segment",
+			RecipeURL:    "https://bearer.com/api/*",
+			DetectionURL: "https://bearer.com/api/v2",
+			Want:         "https://bearer.com/api/v2",
+			Skip:         false,
+		},
+		{
+			Name:         "when the url contains a wildcard for part of a path segment",
+			RecipeURL:    "https://bearer.com/api/v*",
+			DetectionURL: "https://bearer.com/api/v2",
+			Want:         "https://bearer.com/api/v2",
+			Skip:         false,
+		},
+		{
+			Name:         "when the url contains a wildcard matching multiple path segments",
+			RecipeURL:    "https://bearer.com/*/api",
+			DetectionURL: "https://bearer.com/eu/west/api/fred",
+			Want:         "https://bearer.com/eu/west/api",
+			Skip:         false,
+		},
+		{
+			Name:         "when the urls match exactly except the scheme",
+			RecipeURL:    "http://api.bearer.com",
+			DetectionURL: "https://api.bearer.com",
+			Want:         "https://api.bearer.com",
+			Skip:         false,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.Name, func(t *testing.T) {
+			if testCase.Skip {
+				t.Skip("interfaces not implemented")
+			}
+
+			output, err := url_matcher.UrlMatcher(
+				url_matcher.ComparableUrls{
+					RecipeURL:    testCase.RecipeURL,
+					DetectionURL: testCase.DetectionURL,
+				},
+			)
+			if err != nil {
+				t.Errorf("UrlMatcher returned error %s", err)
+			}
+
+			assert.Equal(t, testCase.Want, output)
+		})
+	}
+}
