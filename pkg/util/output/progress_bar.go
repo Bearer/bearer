743f0c15c444300843964577295867860632fc51
Vjeran Fistric <27707350+vjerci@users.noreply.github.com>
2022-10-21 15:18:20 +0200

    Update pkg/util/output/progress_bar.go
    Co-authored-by: Cédric Fabianski <cedric@bearer.sh>

--- pkg/util/output/progress_bar.go
+++ pkg/util/output/progress_bar.go
@@ -19,7 +19,7 @@ func GetProgressBar(filesLength int, config settings.Config) *progressbar.Progre
 		}),
 		progressbar.OptionShowIts(),
 		progressbar.OptionSetItsString("files"),
-		progressbar.OptionSetDescription("scanning..."),
+		progressbar.OptionSetDescription("Scanning target..."),
 		progressbar.OptionSetTheme(progressbar.Theme{
 			Saucer:        "=",
 			SaucerHead:    ">",
