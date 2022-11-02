729a696e6a80cca533dd08d8324b8f269e6cc641
Mark Michon <hello@markmichon.com>
2022-11-02 15:47:19 -0700

    fix(docs): resolve live site path issue

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,27 +1,34 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
-const yaml = require("js-yaml")
-const markdownIt = require("markdown-it")
-const markdownItEmoji = require("markdown-it-emoji")
-const now = String(Date.now())
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const yaml = require("js-yaml");
+const markdownIt = require("markdown-it");
+const markdownItEmoji = require("markdown-it-emoji");
+const now = String(Date.now());
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./_src/styles/tailwind.config.js")
-  eleventyConfig.addWatchTarget("./_src/styles/tailwind.css")
-  eleventyConfig.addPassthroughCopy("assets/img")
-  eleventyConfig.addPassthroughCopy("assets/fonts")
-  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" })
-  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents))
+const pathPrefix = (module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./_src/styles/tailwind.config.js");
+  eleventyConfig.addWatchTarget("./_src/styles/tailwind.css");
+  eleventyConfig.addPassthroughCopy("assets/img");
+  eleventyConfig.addPassthroughCopy("assets/fonts");
+  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
   eleventyConfig.addShortcode("version", function () {
-    return now
-  })
+    return now;
+  });
 
-  eleventyConfig.addPlugin(syntaxHighlight)
-  eleventyConfig.setLibrary("md", markdownIt({html: true}).use(markdownItEmoji))
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
+    baseHref: process.env.ELEVENTY_PRODUCTION ? "/curio/" : "/",
+  });
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({ html: true }).use(markdownItEmoji)
+  );
 
   return {
     dir: {
       includes: "_src/_includes",
       output: "_site",
     },
-  }
-}
+  };
+});
