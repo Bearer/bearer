7ddcf88e6eff269e1810e27a43df9cb61cf165f2
vjerci <27707350+vjerci@users.noreply.github.com>
2022-11-07 13:26:18 +0100

    fix: export custom as custom classified

--- pkg/report/output/dataflow/risks/risks.go
+++ pkg/report/output/dataflow/risks/risks.go
@@ -13,15 +13,6 @@ import (
 	"github.com/bearer/curio/pkg/util/maputil"
 )
 
-// - detector_id: "rails_logger_detector"
-// data_types:
-//   - name: "email"
-// 	locations:
-// 	  - filename: "app/models/user.rb"
-// 		line_number: 5
-// 	  - filename: "app/models/employee.rb"
-// 		line_number: 5
-
 type Holder struct {
 	detectors map[string]detectorHolder // group datatypeHolders by name
 }
