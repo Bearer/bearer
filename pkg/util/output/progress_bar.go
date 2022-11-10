Bearer/bearer

commit 6f09058bfbd0b658124f16ce36d639a220df8c33

chore: Consolidate output and error stream usage (#105)
* Replace global outputWriter with OutOrStdout of cobra Command

It looks like we don't need the global outputWriter, as we can
always get to the configured output writer of the cobra command
of interest.

* Remove unnecessary output stream argument passing

* Use Cobra command output and error streams for pre-built loggers

* Use global output writer (hooked up to Cobra command) for progress bar

* Use Cobra output and error streams in integration test helper

pkg/util/output/progress_bar.go
@@ -1,8 +1,6 @@
 package output
 
 import (
-	"os"
-
 	"github.com/bearer/curio/pkg/commands/process/settings"
 	"github.com/schollz/progressbar/v3"
 )
@@ -10,7 +8,7 @@ import (
 func GetProgressBar(filesLength int, config settings.Config) *progressbar.ProgressBar {
 	return progressbar.NewOptions(filesLength,
 		progressbar.OptionSetVisibility(!config.Scan.Debug),
-		progressbar.OptionSetWriter(os.Stderr),
+		progressbar.OptionSetWriter(outputWriter),
 		progressbar.OptionShowCount(),
 		progressbar.OptionEnableColorCodes(false),
 		progressbar.OptionShowElapsedTimeOnFinish(),

integration/flags/.snapshots/TestMetadataFlags-version
@@ -1,5 +1,5 @@
-
---
 curio version: dev
 sha: devSHA
+
+--
 

integration/internal/testhelper/testhelper.go
@@ -46,18 +46,8 @@ func executeApp(arguments []string, port int) (string, error) {
 		panic(err)
 	}
 
-	stdout := os.Stdout
-	os.Stdout = stdoutWriter
-	defer func() {
-		os.Stdout = stdout
-	}()
-
-	stderr := os.Stderr
-	os.Stderr = stderrWriter
-	defer func() {
-		os.Stderr = stderr
-	}()
-
+	app.SetOut(stdoutWriter)
+	app.SetErr(stderrWriter)
 	app.SetArgs(arguments)
 
 	if err := app.Execute(); err != nil {

pkg/commands/app.go
@@ -2,8 +2,6 @@ package commands
 
 import (
 	"fmt"
-	"io"
-	"os"
 
 	"github.com/bearer/curio/pkg/commands/artifact"
 	"github.com/bearer/curio/pkg/flag"
@@ -14,15 +12,6 @@ import (
 // VersionInfo holds the curio version
 type VersionInfo struct {
 	Version string `json:",omitempty"`
-}
-
-var (
-	outputWriter io.Writer = os.Stdout
-)
-
-// SetOut overrides the destination for messages
-func SetOut(out io.Writer) {
-	outputWriter = out
 }
 
 // NewApp is the factory method to return Curio CLI
@@ -89,7 +78,7 @@ func NewConfigCommand() *cobra.Command {
 			if err := configFlags.Bind(cmd); err != nil {
 				return xerrors.Errorf("flag bind error: %w", err)
 			}
-			options, err := configFlags.ToOptions(args, outputWriter)
+			options, err := configFlags.ToOptions(args)
 			if err != nil {
 				return xerrors.Errorf("flag error: %w", err)
 			}

pkg/commands/init.go
@@ -19,7 +19,7 @@ func NewInitCommand() *cobra.Command {
 				return fmt.Errorf("flag bind error: %w", err)
 			}
 
-			options, err := scanFlags.ToOptions(args, outputWriter)
+			options, err := scanFlags.ToOptions(args)
 			if err != nil {
 				return xerrors.Errorf("flag error: %w", err)
 			}

pkg/commands/processing_worker.go
@@ -24,12 +24,12 @@ func NewProcessingWorkerCommand() *cobra.Command {
 				return fmt.Errorf("flag bind error: %w", err)
 			}
 
-			generalOptions, err := flags.ToOptions(args, outputWriter)
+			generalOptions, err := flags.ToOptions(args)
 			if err != nil {
 				return fmt.Errorf("options binding error: %w", err)
 			}
 
-			output.Setup(generalOptions)
+			output.Setup(cmd, generalOptions)
 
 			processOptions, err := flags.ProcessFlagGroup.ToOptions()
 			if err != nil {

pkg/commands/scan.go
@@ -62,12 +62,12 @@ func NewScanCommand() *cobra.Command {
 				return err
 			}
 
-			options, err := scanFlags.ToOptions(args, outputWriter)
+			options, err := scanFlags.ToOptions(args)
 			if err != nil {
 				return xerrors.Errorf("flag error: %w", err)
 			}
 
-			output.Setup(options)
+			output.Setup(cmd, options)
 
 			if options.Target == "" {
 				return cmd.Help()

pkg/flag/options.go
@@ -2,7 +2,6 @@ package flag
 
 import (
 	"fmt"
-	"io"
 	"strings"
 	"time"
 
@@ -232,7 +231,7 @@ func (f *Flags) bind(cmd *cobra.Command, supportIgnoreConfig bool) error {
 }
 
 // nolint: gocyclo
-func (f *Flags) ToOptions(args []string, output io.Writer) (Options, error) {
+func (f *Flags) ToOptions(args []string) (Options, error) {
 	var err error
 	opts := Options{}
 
@@ -241,7 +240,7 @@ func (f *Flags) ToOptions(args []string, output io.Writer) (Options, error) {
 	}
 
 	if f.ReportFlagGroup != nil {
-		opts.ReportOptions = f.ReportFlagGroup.ToOptions(output)
+		opts.ReportOptions = f.ReportFlagGroup.ToOptions()
 	}
 
 	if f.WorkerFlagGroup != nil {

commit eeac589d6c8c3c1206dc8d650b013ae4f684ebea

fix: Output progress bar to stderr, not to stdout
This was broken in #105

pkg/util/output/progress_bar.go
@@ -8,7 +8,7 @@ import (
 func GetProgressBar(filesLength int, config settings.Config) *progressbar.ProgressBar {
 	return progressbar.NewOptions(filesLength,
 		progressbar.OptionSetVisibility(!config.Scan.Debug),
-		progressbar.OptionSetWriter(outputWriter),
+		progressbar.OptionSetWriter(errorWriter),
 		progressbar.OptionShowCount(),
 		progressbar.OptionEnableColorCodes(false),
 		progressbar.OptionShowElapsedTimeOnFinish(),