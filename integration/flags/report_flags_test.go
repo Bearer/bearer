ed7fbdc8f6237daffe8266bf2a878e3d8a21b960
Simon Dawson <spdawson@gmail.com>
2022-11-11 11:58:12 +0000

    chore: Integration tests for scan format/report/output flags
    This adds snapshot-based integration testing for the following
    command and option flag combinations.
    
    * `curio scan --format=json`
    * `curio scan --format=yaml`
    * `curio scan --report=detectors`
    * `curio scan --report=dataflow`
    * `curio scan --output ./some/path`
    * `curio scan --context=health`
    * `curio scan --disable-domain-resolution=true`
    * `curio scan --skip-path="users/*.go,users/admin.sql"`

---
+++ integration/flags/report_flags_test.go
@@ -0,0 +1,39 @@
+package integration_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/bearer/curio/integration/internal/testhelper"
+	"github.com/bearer/curio/pkg/util/tmpfile"
+)
+
+func newScanTest(name string, arguments []string, outputPath string) *testhelper.TestCase {
+	arguments = append([]string{"scan", filepath.Join("testdata", "simple")}, arguments...)
+	options := testhelper.TestCaseOptions{
+		OutputPath:  outputPath,
+		StartWorker: true,
+	}
+	return testhelper.NewTestCase(name, arguments, options)
+}
+
+func TestReportFlags(t *testing.T) {
+	outputPath := tmpfile.Create("", "integration_test.jsonl")
+	t.Cleanup(func() {
+		os.Remove(outputPath)
+	})
+
+	tests := []testhelper.TestCase{
+		*newScanTest("format-json", []string{"--format=json"}, ""),
+		*newScanTest("format-yaml", []string{"--format=yaml"}, ""),
+		*newScanTest("report-detectors", []string{"--report=detectors"}, ""),
+		*newScanTest("report-dataflow", []string{"--report=dataflow"}, ""),
+		*newScanTest("output", []string{"--output=" + outputPath}, outputPath),
+		*newScanTest("health-context", []string{"--context=health"}, ""),
+		*newScanTest("domain-resolution-disabled", []string{"--disable-domain-resolution=true"}, ""),
+		*newScanTest("skipped-paths", []string{"--skip-path=\"users/*.go,users/admin.sql\""}, ""),
+	}
+
+	testhelper.RunTests(t, tests)
+}
