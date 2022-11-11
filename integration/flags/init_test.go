dca27aebf88e82b5f50cd7ae38dfd799af7850be
Simon Dawson <spdawson@gmail.com>
2022-11-11 09:24:46 +0000

    test: Add integration test for init command (#114)
    * test: Allow integration tests to specify an output file path
    
    The contents of the specified output file path will be used for
    the snapshot, instead of the combined standard output/error.
    
    * test: Start worker only for integration tests that request a worker
    
    Not all integration tests require that a worker process be started;
    make this behaviour conditional on an optional flag in the
    integration test structure.
    
    * test: Add integration test for init command
    
    * test: Convert testhelper.TestCase to have private properties

---
+++ integration/flags/init_test.go
@@ -0,0 +1,19 @@
+package integration_test
+
+import (
+	"testing"
+
+	"github.com/bearer/curio/integration/internal/testhelper"
+)
+
+func TestInitCommand(t *testing.T) {
+	options := testhelper.TestCaseOptions{
+		RunInTempDir: true,
+		OutputPath:   "curio.yml",
+	}
+	testCase := testhelper.NewTestCase("init", []string{"init"}, options)
+
+	tests := []testhelper.TestCase{*testCase}
+
+	testhelper.RunTests(t, tests)
+}
