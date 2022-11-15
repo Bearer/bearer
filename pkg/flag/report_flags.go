cb594ece8be93fb261479dc71c701b3f665c9a1b
vjerci <27707350+vjerci@users.noreply.github.com>
2022-11-15 14:19:55 +0100

    docs: update docs

--- pkg/flag/report_flags.go
+++ pkg/flag/report_flags.go
@@ -24,7 +24,7 @@ var (
 		Name:       "report",
 		ConfigName: "report.report",
 		Value:      ReportDetectors,
-		Usage:      "specify the kind of report (detectors, dataflow)",
+		Usage:      "specify the kind of report (detectors, dataflow, policies, stats)",
 	}
 	OutputFlag = Flag{
 		Name:       "output",
