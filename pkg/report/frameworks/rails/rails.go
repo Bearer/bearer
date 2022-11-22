f7fc96a94af72ae382cab0d81d2b77ffd88c10cf
Simon Dawson <spdawson@gmail.com>
2022-11-22 15:24:42 +0000

    feat: Backport remaining framework classifiers from Bearer Rails app (#155)
    * chore: Introduce an interface for framework classifiers
    
    * chore: Switch Rails framework detection to use recipe UUIDs
    
    * feat: Backport remaining framework classifiers from Bearer API app

--- pkg/report/frameworks/rails/rails.go
+++ pkg/report/frameworks/rails/rails.go
@@ -25,36 +25,36 @@ type Storage struct {
 	Encryption string `json:"encryption" yaml:"encryption"`
 }
 
-func (value *Cache) GetTechnologyKey() string {
+func (value Cache) GetTechnologyKey() string {
 	switch value.Type {
 	case "memory_store", "null_store":
 		// Ignored cache types
 		return ""
 	case "file_store":
-		return "Disk"
+		return "39747024-c306-4a95-a0df-7e585a33a86f"
 	case "mem_cache_store":
-		return "Memcached"
+		return "42908ccc-4f0f-419e-9ba0-f25121fd15b7"
 	case "redis_cache_store":
-		return "Redis"
+		return "62c20409-c1bf-4be9-a859-6fe6be7b11e3"
 	default:
 		return "unidentified_data_store"
 	}
 }
 
-func (value *Database) GetTechnologyKey() string {
+func (value Database) GetTechnologyKey() string {
 	switch strings.ToLower(value.Adapter) {
 	case "mysql2", "jdbcmysql":
-		return "MySQL"
+		return "ffa70264-2b19-445d-a5c9-be82b64fe750"
 	case "postgresql", "jdbcpostgresql":
-		return "PostgreSQL"
+		return "428ff7dd-22ea-4e80-8755-84c70cf460db"
 	case "sqlite3", "jdbcsqlite3":
-		return "SQLite"
+		return "aa706b3c-0f6d-4a7b-a7a5-71ee0c5b6c00"
 	default:
 		return "unidentified_data_store"
 	}
 }
 
-func (value *Storage) GetTechnologyKey() string {
+func (value Storage) GetTechnologyKey() string {
 	if strings.Contains(value.Name, "test") {
 		// Ignored storage types
 		return ""
@@ -65,13 +65,13 @@ func (value *Storage) GetTechnologyKey() string {
 		// Ignored storage types
 		return ""
 	case "AzureStorage":
-		return "Azure Storage"
+		return "f0f43ee7-7f6b-4572-aaa0-6b207146912b"
 	case "Disk":
-		return "Disk"
+		return "39747024-c306-4a95-a0df-7e585a33a86f"
 	case "GCS":
-		return "Google Cloud Storage"
+		return "3a154582-174f-4ef7-90a2-f654435c23cb"
 	case "S3":
-		return "AWS S3"
+		return "4e5a3a3a-47cd-4b0e-b0a6-fa30a0a62499"
 	default:
 		return "unidentified_data_store"
 	}
