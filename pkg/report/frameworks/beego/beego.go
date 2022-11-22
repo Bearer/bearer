0ee29cf1684c96396bc2370af25904280c6286f1
Simon Dawson <spdawson@gmail.com>
2022-11-22 13:25:50 +0000

    feat: Backport remaining framework classifiers from Bearer API app

--- pkg/report/frameworks/beego/beego.go
+++ pkg/report/frameworks/beego/beego.go
@@ -12,3 +12,41 @@ type Database struct {
 	Package      string `json:"package" yaml:"package"`
 	TypeConstant string `json:"type_constant" yaml:"type_constant"`
 }
+
+func (value Database) GetTechnologyKey() string {
+	if value.Package != "" {
+		return technologyForDriverLib(value.Package, value.TypeConstant)
+	}
+
+	return technologyForDriverName(value.DriverName)
+}
+
+func technologyForDriverLib(packageName string, typeConstant string) string {
+	if packageName != "github.com/beego/beego/v2/client/orm" {
+		return "unidentified_data_store"
+	}
+
+	switch typeConstant {
+	case "DRMySQL", "DR_MySQL":
+		return "ffa70264-2b19-445d-a5c9-be82b64fe750"
+	case "DRPostgres", "DR_Postgres":
+		return "428ff7dd-22ea-4e80-8755-84c70cf460db"
+	case "DRSqlite", "DR_Sqlite":
+		return "aa706b3c-0f6d-4a7b-a7a5-71ee0c5b6c00"
+	default:
+		return "unidentified_data_store"
+	}
+}
+
+func technologyForDriverName(driverName string) string {
+	switch driverName {
+	case "mysql":
+		return "ffa70264-2b19-445d-a5c9-be82b64fe750"
+	case "postgres":
+		return "428ff7dd-22ea-4e80-8755-84c70cf460db"
+	case "sqlite3":
+		return "aa706b3c-0f6d-4a7b-a7a5-71ee0c5b6c00"
+	default:
+		return "unidentified_data_store"
+	}
+}
