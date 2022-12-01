db9879c16d7491cbdd84cd63afec12fc9bd7c292
elsapet <elizabeth@bearer.sh>
2022-12-01 11:31:25 +0200

    feat: expand cookie patterns

--- integration/custom_detectors/testdata/ruby/detect_rails_cookies.rb
+++ integration/custom_detectors/testdata/ruby/detect_rails_cookies.rb
@@ -1,10 +1,20 @@
-# detected
+# Detected
 cookies[:user_name] = user.name
+cookies.signed[:user_email] = current_user.email
+cookies.encrypted[:full_name] = customer.name
+cookies.permanent[:first_name] = current_user.first_name
+cookies.signed.permanent[:user_email] = current_user.email
+cookies.permanent.encrypted[:full_name] = customer.name
+cookies.signed.permanent[:first_name] = current_user.first_name
+
 # Not detected
 cookies[:user_name] = "david"
+cookies.signed[:user_email] = "mish@bearer.sh"
+cookies.encrypted[:full_name] = "John Doe"
+cookies.permanent[:first_name] = "John"
+cookies[:lat_lon] = JSON.generate([47.68, -122.37])
 
-# cookies[:lat_lon] = JSON.generate([47.68, -122.37])
-
+# Not detected (yet - variable reconciliation)
 # user = {
 #   first_name: "John",
 # 	last_name: "Doe"
@@ -12,18 +22,8 @@ cookies[:user_name] = "david"
 # cookies[:login] = { value: user.to_json, expires: 1.hour, secure: true }
 # cookies[:login] = { value: user.login, expires: 1.hour, secure: true }
 
-# # Signed
-# cookies.signed[:user_id] = current_user.id
-
-# # Encrypted
-# cookies.encrypted[:full_name] = "John Doe"
-
-# # Permanent
-# cookies.permanent[:first_name] = "John"
-
-# # Permanent & Encrypted
-# cookies.signed.permanent[:user] = JSON.generate({first_name: "John", last_name: "Doe"})
-# cookies.signed.permanent[:user] = {
-# 	first_name: "John",
-# 	last_name: "Doe"
-# }.to_json
+cookies.signed.permanent[:user] = JSON.generate({first_name: "John", last_name: "Doe"})
+cookies.signed.permanent[:user] = {
+	first_name: "John",
+	last_name: "Doe"
+}.to_json
