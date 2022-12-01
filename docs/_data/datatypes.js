433f0ff0b6eb44871aca5e78b1e296698b427849
Mark Michon <hello@markmichon.com>
2022-11-30 17:09:54 -0800

    docs: types sorted by grouping

--- docs/_data/datatypes.js
+++ docs/_data/datatypes.js
@@ -1,14 +1,19 @@
 const { readdir, readFile } = require("node:fs/promises");
 const path = require("path");
 
+async function fetchFile(location) {
+  return readFile(location, { encoding: "utf8" }).then((file) =>
+    JSON.parse(file)
+  );
+}
+
 async function fetchData(dir) {
   try {
     const files = await readdir(dir);
+    console.log(files.length);
     let result = await Promise.all(
       files.map(async (file) => {
-        let data = await readFile(path.join(dir, file), { encoding: "utf8" });
-        // console.log(data);
-        return JSON.parse(data);
+        return await fetchFile(path.join(dir, file));
       })
     );
     return result;
@@ -17,25 +22,45 @@ async function fetchData(dir) {
   }
 }
 
-function sortData(types, cats) {
-  let workingData = {};
-  let output = [];
-  cats.forEach((cat) => {
-    workingData[cat.uuid] = { name: cat.name, types: [] };
-  });
+function sortData(typesFile, catsFile, groupsFile) {
+  let output = {};
+  let counts = {
+    types: typesFile.length,
+  };
 
-  types.forEach((item) => {
-    workingData[item.category_uuid].types.push(item);
-  });
-
-  for (const key in workingData) {
-    output.push({ ...workingData[key] });
+  // setup groups
+  for (const key in groupsFile.groups) {
+    output[key] = { uuid: key, name: groupsFile.groups[key], categories: {} };
   }
-  return output;
+
+  // add categories to each group
+  for (const key in groupsFile.category_mapping) {
+    output[groupsFile.category_mapping[key].group_uuid].categories[key] = {
+      types: [],
+      uuid: key,
+      ...groupsFile.category_mapping[key],
+    };
+  }
+
+  // add types to each category
+  // output[group uuid].categories[category uuid].types[]
+  // note: inefficient, needs rewrite
+  for (const key in output) {
+    typesFile.forEach((item) => {
+      if (output[key].categories[item.category_uuid]) {
+        output[key].categories[item.category_uuid].types.push(item);
+      }
+    });
+  }
+
+  return { output, counts };
 }
 // example();
 module.exports = async function () {
   let dataTypes = await fetchData("../pkg/classification/db/data_types/");
   let dataCats = await fetchData("../pkg/classification/db/data_categories/");
-  return sortData(dataTypes, dataCats);
+  let groupings = await fetchFile(
+    "../pkg/classification/db/category_grouping.json"
+  );
+  return sortData(dataTypes, dataCats, groupings);
 };
