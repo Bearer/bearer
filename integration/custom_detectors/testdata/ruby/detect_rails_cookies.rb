60e132ac170888e253e386e3d356dffbb0308667
elsapet <elizabeth@bearer.sh>
2022-12-05 11:03:54 +0200

    fix: add variable reconciliation spec

--- integration/custom_detectors/testdata/ruby/detect_rails_cookies.rb
+++ integration/custom_detectors/testdata/ruby/detect_rails_cookies.rb
@@ -1,11 +1,17 @@
 # Detected
 cookies[:user_name] = user.name
-cookies.signed[:user_email] = current_user.email
-cookies.encrypted[:full_name] = customer.name
-cookies.permanent[:first_name] = current_user.first_name
-cookies.signed.permanent[:user_email] = current_user.email
-cookies.permanent.encrypted[:full_name] = customer.name
-cookies.signed.permanent[:first_name] = current_user.first_name
+cookies.signed[:user_email] = user.email
+cookies.encrypted[:full_name] = user.name
+cookies.permanent[:first_name] = user.first_name
+cookies.signed.permanent[:user_email] = user.email
+cookies.permanent.encrypted[:full_name] = user.name
+cookies.signed.permanent[:first_name] = user.first_name
+
+user_1 = {
+  first_name: "John",
+	last_name: "Doe"
+}
+cookies[:login] = { value: user_1.to_json, expires: 1.hour, secure: true }
 
 # Not detected
 cookies[:user_name] = "david"
@@ -13,14 +19,6 @@ cookies.signed[:user_email] = "mish@bearer.sh"
 cookies.encrypted[:full_name] = "John Doe"
 cookies.permanent[:first_name] = "John"
 cookies[:lat_lon] = JSON.generate([47.68, -122.37])
-
-# Not detected (yet - variable reconciliation)
-# user = {
-#   first_name: "John",
-# 	last_name: "Doe"
-# }
-# cookies[:login] = { value: user.to_json, expires: 1.hour, secure: true }
-# cookies[:login] = { value: user.login, expires: 1.hour, secure: true }
 
 cookies.signed.permanent[:user] = JSON.generate({first_name: "John", last_name: "Doe"})
 cookies.signed.permanent[:user] = {
