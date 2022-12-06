fcea3bb164b4597cbd1dd144874137996daee3ae
Simon Dawson <spdawson@gmail.com>
2022-12-06 07:12:43 +0000

    feat: Ruby policy for insecure FTP when sensitive data is processed (#193)

---
+++ integration/policies/testdata/ruby/insecure_ftp/with_sensitive_data.rb
@@ -0,0 +1,22 @@
+# Insecure FTP
+
+class User
+  attr_reader :name, :email, :password, :ethnicity
+end
+
+## Detected
+require "net/ftp"
+
+ftp = Net::FTP::new("ftp.ruby-lang.org")
+ftp.login("anonymous", "matz@ruby-lang.org")
+ftp.chdir("/pub/ruby")
+tgz = ftp.list("ruby-*.tar.gz").sort.last
+ftp.getbinaryfile(tgz, tgz)
+ftp.close
+
+## Not detected
+require "net/sftp"
+
+Net::SFTP.start("localhost", "user") do |sftp|
+  sftp.upload! "/local/file.tgz", "/remote/file.tgz"
+end
