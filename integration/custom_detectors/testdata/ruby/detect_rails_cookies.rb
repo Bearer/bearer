cec91e568533cfb093b396c24dfd3f377c4dfa14
elsapet <elizabeth@bearer.sh>
2022-12-07 11:55:42 +0200

    fix: prefer pattern with variable

--- integration/custom_detectors/testdata/ruby/detect_rails_cookies.rb
+++ integration/custom_detectors/testdata/ruby/detect_rails_cookies.rb
@@ -1,17 +1,12 @@
 # Detected
-cookies[:user_name] = user.name
-cookies.signed[:user_email] = user.email
-cookies.encrypted[:full_name] = user.name
-cookies.permanent[:first_name] = user.first_name
-cookies.signed.permanent[:user_email] = user.email
-cookies.permanent.encrypted[:full_name] = user.name
-cookies.signed.permanent[:first_name] = user.first_name
-
+cookies.signed[:info] = user.email
+cookies.permanent.encrypted[:secret] = user.address
 user_1 = {
   first_name: "John",
 	last_name: "Doe"
 }
 cookies[:login] = { value: user_1.to_json, expires: 1.hour, secure: true }
+
 
 # Not detected
 cookies[:user_name] = "david"
@@ -19,7 +14,6 @@ cookies.signed[:user_email] = "mish@bearer.sh"
 cookies.encrypted[:full_name] = "John Doe"
 cookies.permanent[:first_name] = "John"
 cookies[:lat_lon] = JSON.generate([47.68, -122.37])
-
 cookies.signed.permanent[:user] = JSON.generate({first_name: "John", last_name: "Doe"})
 cookies.signed.permanent[:user] = {
 	first_name: "John",
