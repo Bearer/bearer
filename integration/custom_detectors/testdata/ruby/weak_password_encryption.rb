e6251694a31e9a67e0050387d4f7d9901c7124c6
elsapet <elizabeth@bearer.sh>
2022-12-12 11:08:16 +0200

    feat: add key encryptions

--- integration/custom_detectors/testdata/ruby/weak_password_encryption.rb
+++ integration/custom_detectors/testdata/ruby/weak_password_encryption.rb
@@ -4,8 +4,18 @@ Digest::MD5.hexdigest(user.password)
 RC4.new("insecure").encrypt(user.password)
 Crypt::Blowfish.new("insecure").encrypt_block({ |u| user.password })
 
+OpenSSL::PKey::RSA.new(File.read('rsa.pem')).private_encrypt(customer.password)
+
+cipher = OpenSSL::Cipher.new('aes-128-cbc')
+OpenSSL::PKey::DSA.new(2048).export(cipher, customer.password)
+OpenSSL::PKey::RSA.new(2048).to_pem(cipher, customer.password)
+
 # Not detected (not a password)
 Digest::SHA1.hexidigest(user.email)
 Digest::MD5.hexdigest(user.first_name)
 RC4.new("insecure").encrypt(user.address)
 Crypt::Blowfish.new("insecure").encrypt_block({ |u| user.gender_identity })
+
+# Not detected (variable assignment)
+pkey = OpenSSL::PKey::RSA.new(File.read('rsa.pem'))
+pkey.private_encrypt(customer.password)
