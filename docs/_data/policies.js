e705e9531523ba827a657fba6a079c087124b567
Mark Michon <mark.michon@bearer.sh>
2022-12-15 05:57:07 -0800

    docs: update policy names, descriptions, doc output, and fix IDs (#251)
    * update policy names
    * update policy doc page
    * fix IDs and expand descriptions

--- docs/_data/policies.js
+++ docs/_data/policies.js
@@ -1,10 +1,23 @@
 const { readFile } = require("node:fs/promises");
 const yaml = require("js-yaml");
 
+function convertAndSort(obj) {
+  let list = [];
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      list.push({
+        ...obj[key],
+      });
+    }
+  }
+  return list.sort((a, b) => (a.display_id > b.display_id ? 1 : -1));
+}
+
 async function fetchFile(location) {
-  return readFile(location, { encoding: "utf8" }).then((file) =>
-    yaml.load(file)
-  );
+  return readFile(location, { encoding: "utf8" }).then((file) => {
+    let out = yaml.load(file);
+    return convertAndSort(out);
+  });
 }
 
 module.exports = async function () {
