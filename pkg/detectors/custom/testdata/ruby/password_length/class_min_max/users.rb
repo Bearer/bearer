a08d1d8b462dd5f285af5338f65e7bd26c11441f
Cédric Fabianski <cfabianski@me.com>
2022-12-16 11:57:16 +0100

    chore: fix typo for devise and improve test coverage

--- pkg/detectors/custom/testdata/ruby/password_length/class_min_max/users.rb
+++ pkg/detectors/custom/testdata/ruby/password_length/class_min_max/users.rb
@@ -1,15 +1,19 @@
 # it should match since max is lower than required
 class User < ApplicationRecord
-	device password_length: 11..32
+	devise password_length: 11..32
 end
 
+# it should match since minimum is lower than required
+class Employee < ApplicationRecord
+	validates :password, length: { minimum: 6 }
+end
 
 # it should match since min is lower than required
 class Student < ApplicationRecord
-	device password_length: 6..36
+	devise password_length: 6..36
 end
 
 # it shouldn't match since max and min are withing boundaries
 class Admin < ApplicationRecord
-	device password_length: 11..36
+	devise password_length: 11..36
 end
