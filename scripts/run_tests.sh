b73e6487873ba44e4144508b48fba95e33aaf272
Simon Dawson <spdawson@gmail.com>
2022-12-16 14:25:02 +0000

    fix: Ensure that cleanup occurs in test wrapper script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,8 +2,6 @@
 #
 # Run the Curio test suite
 
-set -e
-
 DEFAULT_TEST_ARGS="-count=1 -v ./..."
 
 do_info() {
@@ -14,6 +12,13 @@ do_error() {
   printf "ERROR: $*\n" 1>&2
   exit 1
 }
+
+do_cleanup() {
+  do_info "Cleaning up"
+  rm -f ./curio || do_error "Failed to clean up"
+}
+
+trap do_cleanup 1 2 3 6
 
 do_info "Building Curio binary..."
 go build ./cmd/curio || do_error "Failed to build Curio binary"
@@ -26,7 +31,6 @@ do_info "Running tests..."
 CURIO_BINARY=1 GITHUB_WORKSPACE=`pwd` go test $TEST_ARGS
 TEST_STATUS=$?
 
-do_info "Cleaning up"
-rm -f ./curio || do_error "Failed to clean up"
+do_cleanup
 
 [ $TEST_STATUS -eq 0 ] || do_error "Tests failed"
