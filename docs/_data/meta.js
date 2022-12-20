b7e162b189841cd756edcb9f77339b82da751d80
Mark Michon <hello@markmichon.com>
2022-12-19 16:46:39 -0800

    docs: remove blocking from docs and set up sitemap

--- docs/_data/meta.js
+++ docs/_data/meta.js
@@ -1,4 +1,9 @@
 module.exports = {
   sourcePath: "https://github.com/bearer/curio",
   homePage: "/",
+  name: "Curio",
+  url: "https://curio.sh",
+  description:
+    "Curio is a static code analysis tool (SAST) that scans your source code to discover security risks and vulnerabilities that put your sensitive data at risk (PHI, PD, PII).",
+  favicon: "https://curio.sh/assets/img/favicon.svg",
 };
