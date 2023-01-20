430c10187149c1e7db2e18ce9ebb57f71e02901e
elsapet <elizabeth@bearer.sh>
2023-01-20 16:39:24 +0200

    fix: update rule metadata

--- pkg/commands/process/settings/rules/ruby/lang/encrypt_testdata/structure_sql/db/structure.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.users (
-  id bigint NOT NULL,
-  name character varying,
-  password character varying,
-  created_at timestamp(6) without time zone NOT NULL,
-  updated_at timestamp(6) without time zone NOT NULL,
-  email character varying DEFAULT ''::character varying NOT NULL
-);
