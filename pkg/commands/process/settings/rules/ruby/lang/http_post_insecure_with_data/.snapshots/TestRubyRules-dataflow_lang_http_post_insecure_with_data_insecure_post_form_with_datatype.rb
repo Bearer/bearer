de681c51711c3b09efd9d93d8938db8495b2fb74
elsapet <elizabeth@bearer.sh>
2023-01-25 10:06:02 +0200

    fix: ruby http insecure post policy

---
+++ pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/.snapshots/TestRubyRules-dataflow_lang_http_post_insecure_with_data_insecure_post_form_with_datatype.rb
@@ -0,0 +1,52 @@
+data_types:
+    - name: Email Address
+      detectors:
+        - name: ruby
+          locations:
+            - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_form_with_datatype.rb
+              line_number: 1
+              field_name: email
+              object_name: user
+risks:
+    - detector_id: ruby_lang_http_post_insecure_with_data
+      data_types:
+        - name: Email Address
+          stored: false
+          locations:
+            - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_form_with_datatype.rb
+              line_number: 1
+              parent:
+                line_number: 1
+                content: 'Net::HTTP.post_form("http://my.api.com/users/search", { email: user.email })'
+              field_name: email
+              object_name: user
+        - name: Unique Identifier
+          stored: false
+          locations:
+            - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_form_with_datatype.rb
+              line_number: 1
+              parent:
+                line_number: 1
+                content: 'Net::HTTP.post_form("http://my.api.com/users/search", { email: user.email })'
+              object_name: user
+    - detector_id: ruby_lang_http_insecure
+      locations:
+        - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_form_with_datatype.rb
+          line_number: 1
+          parent:
+            line_number: 1
+            content: 'Net::HTTP.post_form("http://my.api.com/users/search", { email: user.email })'
+          content: |
+            Net::HTTP.post_form($<INSECURE_URL>)
+        - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_form_with_datatype.rb
+          line_number: 1
+          parent:
+            line_number: 1
+            content: 'Net::HTTP.post_form("http://my.api.com/users/search", { email: user.email })'
+          content: |
+            Net::HTTP.post_form($<INSECURE_URL>)
+components: []
+
+
+--
+
---
+++ pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/.snapshots/TestRubyRules-dataflow_lang_http_post_insecure_with_data_insecure_post_with_datatype.rb
@@ -0,0 +1,45 @@
+data_types:
+    - name: Email Address
+      detectors:
+        - name: ruby
+          locations:
+            - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_with_datatype.rb
+              line_number: 1
+              field_name: email
+              object_name: user
+risks:
+    - detector_id: ruby_lang_http_post_insecure_with_data
+      data_types:
+        - name: Email Address
+          stored: false
+          locations:
+            - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_with_datatype.rb
+              line_number: 1
+              parent:
+                line_number: 1
+                content: 'HTTParty.post("http://my.api.com/users/search", body: { email: user.email })'
+              field_name: email
+              object_name: user
+        - name: Unique Identifier
+          stored: false
+          locations:
+            - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_with_datatype.rb
+              line_number: 1
+              parent:
+                line_number: 1
+                content: 'HTTParty.post("http://my.api.com/users/search", body: { email: user.email })'
+              object_name: user
+    - detector_id: ruby_lang_http_insecure
+      locations:
+        - filename: pkg/commands/process/settings/rules/ruby/lang/http_post_insecure_with_data/testdata/insecure_post_with_datatype.rb
+          line_number: 1
+          parent:
+            line_number: 1
+            content: 'HTTParty.post("http://my.api.com/users/search", body: { email: user.email })'
+          content: |
+            $<CLIENT>.post($<INSECURE_URL>)
+components: []
+
+
+--
+
