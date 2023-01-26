8b51b048073c90d69b8e8dbee605abc915d4e7b9
elsapet <elizabeth@bearer.sh>
2023-01-26 14:03:36 +0200

    fix: merge flags tests

--- integration/flags/report_flags_test.go
+++ integration/flags/report_flags_test.go
@@ -1,10 +1,13 @@
 package flags_test
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/bearer/curio/integration/internal/testhelper"
+	"github.com/bearer/curio/pkg/util/tmpfile"
+	"github.com/bradleyjkemp/cupaloy"
 )
 
 func newScanTest(name string, arguments []string) testhelper.TestCase {
@@ -33,4 +36,27 @@ func TestReportFlagsShouldFail(t *testing.T) {
 
 	testhelper.RunTests(t, tests)
 
+}
+
+func TestOuputFlag(t *testing.T) {
+	outputPath := tmpfile.Create("", "test_output.jsonl")
+	defer func() {
+		err := os.Remove(outputPath)
+		if err != nil {
+			t.Fatalf("failed to clean up created output file %s", err)
+		}
+	}()
+
+	tests := []testhelper.TestCase{
+		newScanTest("output", []string{"--report=detectors", "--output=" + outputPath}),
+	}
+
+	testhelper.RunTests(t, tests)
+
+	fileContent, err := os.ReadFile(outputPath)
+	if err != nil {
+		t.Fatalf("failed to read created output file, err: %s", err)
+	}
+
+	cupaloy.SnapshotT(t, string(fileContent))
 }
