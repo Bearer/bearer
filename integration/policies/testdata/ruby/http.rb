bf2bcb951c96193bd69536126fce2c44aa71ea21
elsapet <elizabeth@bearer.sh>
2023-01-26 11:49:42 +0200

    chore: remove outdated policies integration tests

--- integration/policies/testdata/ruby/http.rb
+++
@@ -1,7 +0,0 @@
-uri = URI('http://my.api.com/users/search')
-user = { x: 42 }
-uri.query = URI.encode_www_form(user)
-
-response = Net::HTTP.post_form(uri, { x: "abc" })
-
-uri = URI('https://my.api.com/users/search')
