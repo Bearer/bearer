b49764e6a620e6309361b8fe1a4f8889a7f4948e
elsapet <elizabeth@bearer.sh>
2023-01-26 11:23:42 +0200

    fix: update ssl snapshots

--- pkg/commands/process/settings/rules/ruby/lang/ssl_verification/.snapshots/TestRubyRules-dataflow_lang_ssl_verification_verification_disabled.rb
+++ pkg/commands/process/settings/rules/ruby/lang/ssl_verification/.snapshots/TestRubyRules-dataflow_lang_ssl_verification_verification_disabled.rb
@@ -8,6 +8,16 @@ risks:
             content: http.verify_mode = OpenSSL::SSL::VERIFY_NONE
           content: |
             $<_>.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        - filename: pkg/commands/process/settings/rules/ruby/lang/ssl_verification/testdata/verification_disabled.rb
+          line_number: 4
+          parent:
+            line_number: 4
+            content: |-
+                Net::HTTP.start(uri.host, uri.port, :use_ssl => true, :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+                  Net::HTTP::Get.new uri
+                end
+          content: |
+            Net::HTTP.start(:verify_mode => OpenSSL::SSL::VERIFY_NONE)$<...>
 components: []
 
 
--- pkg/commands/process/settings/rules/ruby/lang/ssl_verification/.snapshots/TestRubyRules-policy_lang_ssl_verification_verification_disabled.rb
+++ pkg/commands/process/settings/rules/ruby/lang/ssl_verification/.snapshots/TestRubyRules-policy_lang_ssl_verification_verification_disabled.rb
@@ -2,11 +2,22 @@ low:
     - policy_name: ""
       policy_dsrid: DSR-2
       policy_display_id: ruby_lang_ssl_verification
-      policy_description: Do not disable SSL certificate verification.
+      policy_description: Enable SSL Certificate Verification.
       line_number: 1
       filename: pkg/commands/process/settings/rules/ruby/lang/ssl_verification/testdata/verification_disabled.rb
       parent_line_number: 1
       parent_content: http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    - policy_name: ""
+      policy_dsrid: DSR-2
+      policy_display_id: ruby_lang_ssl_verification
+      policy_description: Enable SSL Certificate Verification.
+      line_number: 4
+      filename: pkg/commands/process/settings/rules/ruby/lang/ssl_verification/testdata/verification_disabled.rb
+      parent_line_number: 4
+      parent_content: |-
+        Net::HTTP.start(uri.host, uri.port, :use_ssl => true, :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+          Net::HTTP::Get.new uri
+        end
 
 
 --
