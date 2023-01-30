c5a9d668aa3f496c9130afb142ec650213b09982
David Roe <dave.roe@bearer.sh>
2023-01-30 16:18:00 +0000

    feat: add more sentry rules

--- pkg/commands/process/settings/rules/ruby/third_parties/sentry/.snapshots/TestRubyThirdPartiesSentryDataflow-dataflow_ruby_third_parties_sentry_datatype_in_set_user.rb
+++ pkg/commands/process/settings/rules/ruby/third_parties/sentry/.snapshots/TestRubyThirdPartiesSentryDataflow-dataflow_ruby_third_parties_sentry_datatype_in_set_user.rb
@@ -7,6 +7,14 @@ data_types:
               line_number: 3
               field_name: email
               object_name: user
+            - filename: pkg/commands/process/settings/rules/ruby/third_parties/sentry/testdata/datatype_in_set_user.rb
+              line_number: 6
+              field_name: email
+              object_name: user
+            - filename: pkg/commands/process/settings/rules/ruby/third_parties/sentry/testdata/datatype_in_set_user.rb
+              line_number: 10
+              field_name: email
+              object_name: user
 risks:
     - detector_id: ruby_third_parties_sentry
       data_types:
@@ -20,6 +28,20 @@ risks:
                 content: 'Sentry.set_user(email: user.email)'
               field_name: email
               object_name: user
+            - filename: pkg/commands/process/settings/rules/ruby/third_parties/sentry/testdata/datatype_in_set_user.rb
+              line_number: 6
+              parent:
+                line_number: 6
+                content: 'scope.set_user(email: user.email)'
+              field_name: email
+              object_name: user
+            - filename: pkg/commands/process/settings/rules/ruby/third_parties/sentry/testdata/datatype_in_set_user.rb
+              line_number: 10
+              parent:
+                line_number: 10
+                content: 'scope.set_user(email: user.email)'
+              field_name: email
+              object_name: user
         - name: Unique Identifier
           stored: false
           locations:
@@ -28,6 +50,18 @@ risks:
               parent:
                 line_number: 3
                 content: 'Sentry.set_user(email: user.email)'
+              object_name: user
+            - filename: pkg/commands/process/settings/rules/ruby/third_parties/sentry/testdata/datatype_in_set_user.rb
+              line_number: 6
+              parent:
+                line_number: 6
+                content: 'scope.set_user(email: user.email)'
+              object_name: user
+            - filename: pkg/commands/process/settings/rules/ruby/third_parties/sentry/testdata/datatype_in_set_user.rb
+              line_number: 10
+              parent:
+                line_number: 10
+                content: 'scope.set_user(email: user.email)'
               object_name: user
 components: []
 
---
+++ pkg/commands/process/settings/rules/ruby/third_parties/sentry/.snapshots/TestRubyThirdPartiesSentrySummary-summary_ruby_third_parties_sentry_datatype_in_set_tag.rb
@@ -0,0 +1,25 @@
+critical:
+    - policy_name: ""
+      policy_dsrid: DSR-1
+      policy_display_id: ruby_third_parties_sentry
+      policy_description: Do not send sensitive data to Sentry.
+      line_number: 2
+      filename: pkg/commands/process/settings/rules/ruby/third_parties/sentry/testdata/datatype_in_set_tag.rb
+      category_groups:
+        - PII
+      parent_line_number: 2
+      parent_content: scope.set_tag(:email, user.email)
+    - policy_name: ""
+      policy_dsrid: DSR-1
+      policy_display_id: ruby_third_parties_sentry
+      policy_description: Do not send sensitive data to Sentry.
+      line_number: 6
+      filename: pkg/commands/process/settings/rules/ruby/third_parties/sentry/testdata/datatype_in_set_tag.rb
+      category_groups:
+        - PII
+      parent_line_number: 6
+      parent_content: scope.set_tag(:email, user.email)
+
+
+--
+
