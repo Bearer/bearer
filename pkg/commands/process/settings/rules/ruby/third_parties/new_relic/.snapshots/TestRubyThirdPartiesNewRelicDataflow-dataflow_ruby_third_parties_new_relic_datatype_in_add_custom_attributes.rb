fab9bd6bfb3199c94c522d143a5970320ffef183
elsapet <elizabeth@bearer.sh>
2023-01-31 17:52:40 +0200

    fix: update new relic snapshots

--- pkg/commands/process/settings/rules/ruby/third_parties/new_relic/.snapshots/TestRubyThirdPartiesNewRelicDataflow-dataflow_ruby_third_parties_new_relic_datatype_in_add_custom_attributes.rb
+++ pkg/commands/process/settings/rules/ruby/third_parties/new_relic/.snapshots/TestRubyThirdPartiesNewRelicDataflow-dataflow_ruby_third_parties_new_relic_datatype_in_add_custom_attributes.rb
@@ -7,6 +7,7 @@ data_types:
               line_number: 1
               field_name: email
               object_name: user
+              subject_name: User
 risks:
     - detector_id: ruby_third_parties_new_relic
       data_types:
@@ -20,6 +21,7 @@ risks:
                 content: NewRelic::Agent.add_custom_attributes(user)
               field_name: email
               object_name: user
+              subject_name: User
             - filename: pkg/commands/process/settings/rules/ruby/third_parties/new_relic/testdata/datatype_in_add_custom_attributes.rb
               line_number: 3
               parent:
@@ -27,21 +29,7 @@ risks:
                 content: 'NewRelic::Agent.add_custom_attributes(a: "test", user: { email: "user@example.com" }, other: 42)'
               field_name: email
               object_name: user
-        - name: Unique Identifier
-          stored: false
-          locations:
-            - filename: pkg/commands/process/settings/rules/ruby/third_parties/new_relic/testdata/datatype_in_add_custom_attributes.rb
-              line_number: 1
-              parent:
-                line_number: 2
-                content: NewRelic::Agent.add_custom_attributes(user)
-              object_name: user
-            - filename: pkg/commands/process/settings/rules/ruby/third_parties/new_relic/testdata/datatype_in_add_custom_attributes.rb
-              line_number: 3
-              parent:
-                line_number: 3
-                content: 'NewRelic::Agent.add_custom_attributes(a: "test", user: { email: "user@example.com" }, other: 42)'
-              object_name: user
+              subject_name: User
 components: []
 
 
--- pkg/commands/process/settings/rules/ruby/third_parties/new_relic/.snapshots/TestRubyThirdPartiesNewRelicDataflow-dataflow_ruby_third_parties_new_relic_datatype_in_notice_error.rb
+++ pkg/commands/process/settings/rules/ruby/third_parties/new_relic/.snapshots/TestRubyThirdPartiesNewRelicDataflow-dataflow_ruby_third_parties_new_relic_datatype_in_notice_error.rb
@@ -7,6 +7,7 @@ data_types:
               line_number: 1
               field_name: first_name
               object_name: user
+              subject_name: User
 risks:
     - detector_id: ruby_third_parties_new_relic
       data_types:
@@ -20,6 +21,7 @@ risks:
                 content: 'NewRelic::Agent.notice_error(exception, { custom_params: user })'
               field_name: first_name
               object_name: user
+              subject_name: User
         - name: Lastname
           stored: false
           locations:
@@ -30,15 +32,6 @@ risks:
                 content: 'NewRelic::Agent.notice_error(exception, expected: true, custom_params: { last_name: "foo" }, metric: "test")'
               field_name: last_name
               object_name: custom_params
-        - name: Unique Identifier
-          stored: false
-          locations:
-            - filename: pkg/commands/process/settings/rules/ruby/third_parties/new_relic/testdata/datatype_in_notice_error.rb
-              line_number: 1
-              parent:
-                line_number: 2
-                content: 'NewRelic::Agent.notice_error(exception, { custom_params: user })'
-              object_name: user
 components: []
 
 
