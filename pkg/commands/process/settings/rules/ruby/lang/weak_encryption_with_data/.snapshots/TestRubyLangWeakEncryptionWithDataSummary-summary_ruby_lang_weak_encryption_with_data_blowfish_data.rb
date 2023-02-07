95b77473fd9430b2fbb89f5760ebd3e3c0aa627b
David Roe <dave.roe@bearer.sh>
2023-02-07 08:45:56 +0000

    test: update remaining snapshots

--- pkg/commands/process/settings/rules/ruby/lang/weak_encryption_with_data/.snapshots/TestRubyLangWeakEncryptionWithDataSummary-summary_ruby_lang_weak_encryption_with_data_blowfish_data.rb
+++ pkg/commands/process/settings/rules/ruby/lang/weak_encryption_with_data/.snapshots/TestRubyLangWeakEncryptionWithDataSummary-summary_ruby_lang_weak_encryption_with_data_blowfish_data.rb
@@ -9,6 +9,19 @@ critical:
         - PII
       parent_line_number: 1
       parent_content: |-
+        Crypt::Blowfish.new("insecure").encrypt_block { |user|
+          user.password
+        }
+    - policy_name: ""
+      policy_dsrid: DSR-7
+      policy_display_id: ruby_lang_weak_encryption_with_data
+      policy_description: Do not use weak encryption libraries to encrypt sensitive data.
+      line_number: 6
+      filename: pkg/commands/process/settings/rules/ruby/lang/weak_encryption_with_data/testdata/blowfish_data.rb
+      category_groups:
+        - PII
+      parent_line_number: 5
+      parent_content: |-
         Crypt::Blowfish.new("insecure").encrypt_block do |user|
           user.password
         end
@@ -16,11 +29,11 @@ critical:
       policy_dsrid: DSR-7
       policy_display_id: ruby_lang_weak_encryption_with_data
       policy_description: Do not use weak encryption libraries to encrypt sensitive data.
-      line_number: 5
+      line_number: 9
       filename: pkg/commands/process/settings/rules/ruby/lang/weak_encryption_with_data/testdata/blowfish_data.rb
       category_groups:
         - PII
-      parent_line_number: 5
+      parent_line_number: 9
       parent_content: Crypt::Blowfish.new("your-key").encrypt_string(user.email)
 
 
