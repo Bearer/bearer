1a0a6f9c181539d9783e731b635cf41f3ffd0371
vjerci <27707350+vjerci@users.noreply.github.com>
2023-02-09 16:08:46 +0100

    feat: update test snapshots

--- pkg/commands/process/settings/rules/javascript/lang/weak_encryption/.snapshots/TestJavascriptWeakEncryptionSummary-summary_javascript_lang_weak_encryption_md5.js
+++ pkg/commands/process/settings/rules/javascript/lang/weak_encryption/.snapshots/TestJavascriptWeakEncryptionSummary-summary_javascript_lang_weak_encryption_md5.js
@@ -1,18 +1,18 @@
 critical:
-    - policy_name: ""
-      policy_dsrid: DSR-5
-      policy_display_id: javascript_weak_encryption
-      policy_description: Do not weak encrypt sensitive information
+    - rule_dsrid: DSR-5
+      rule_display_id: javascript_weak_encryption
+      rule_description: Do not weak encrypt sensitive information
+      rule_documentation_url: https://curio.sh/reference/rules/javascript_weak_encryption
       line_number: 4
       filename: pkg/commands/process/settings/rules/javascript/lang/weak_encryption/testdata/md5.js
       category_groups:
         - PII
       parent_line_number: 4
       parent_content: crypto.createHmac("md5", key).update(user.password)
-    - policy_name: ""
-      policy_dsrid: DSR-5
-      policy_display_id: javascript_weak_encryption
-      policy_description: Do not weak encrypt sensitive information
+    - rule_dsrid: DSR-5
+      rule_display_id: javascript_weak_encryption
+      rule_description: Do not weak encrypt sensitive information
+      rule_documentation_url: https://curio.sh/reference/rules/javascript_weak_encryption
       line_number: 5
       filename: pkg/commands/process/settings/rules/javascript/lang/weak_encryption/testdata/md5.js
       category_groups:
