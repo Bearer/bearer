e226b871cdd6095b17cf61c45288d37203f78f6f
Mark Michon <mark.michon@bearer.sh>
2023-02-13 02:31:51 -0800

    docs(fix): Update remediation messages for js/ruby rules (#556)
    * docs(fix): Update remediation messages for js/ruby
    
    * chore: update snapshots
    
    ---------
    
    Co-authored-by: gotbadger <p.j.h@hey.com>

--- pkg/commands/process/settings/rules/javascript/express/insecure_cookie/.snapshots/TestExpressSecureCookieSummary-summary_javascript_express_insecure_cookie_insecure_cookie.js
+++ pkg/commands/process/settings/rules/javascript/express/insecure_cookie/.snapshots/TestExpressSecureCookieSummary-summary_javascript_express_insecure_cookie_insecure_cookie.js
@@ -1,7 +1,7 @@
 low:
     - rule_dsrid: DSR-5
       rule_display_id: express_insecure_cookie
-      rule_description: Ensure cookies are sent over https.
+      rule_description: Ensure cookies are sent over HTTPS.
       rule_documentation_url: https://curio.sh/reference/rules/express_insecure_cookie
       line_number: 9
       filename: pkg/commands/process/settings/rules/javascript/express/insecure_cookie/testdata/insecure_cookie.js
