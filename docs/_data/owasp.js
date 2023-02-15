ffaa272ac6ee3fb7f19178a1a4941c1270010c72
Mark Michon <mark.michon@bearer.sh>
2023-02-14 17:23:33 -0800

    WIP: owasp id rules logic

---
+++ docs/_data/owasp.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+  return {
+    "A01:2021": "https://owasp.org/Top10/A01_2021-Broken_Access_Control/",
+    "A02:2021": "https://owasp.org/Top10/A02_2021-Cryptographic_Failures/",
+    "A03:2021": "https://owasp.org/Top10/A03_2021-Injection/",
+    "A04:2021": "https://owasp.org/Top10/A04_2021-Insecure_Design/",
+    "A05:2021": "https://owasp.org/Top10/A05_2021-Security_Misconfiguration/",
+    "A06:2021":
+      "https://owasp.org/Top10/A06_2021-Vulnerable_and_Outdated_Components/",
+    "A07:2021":
+      "https://owasp.org/Top10/A07_2021-Identification_and_Authentication_Failures/",
+    "A08:2021":
+      "https://owasp.org/Top10/A08_2021-Software_and_Data_Integrity_Failures/",
+    "A09:2021":
+      "https://owasp.org/Top10/A09_2021-Security_Logging_and_Monitoring_Failures/",
+    "A10:2021":
+      "https://owasp.org/Top10/A10_2021-Server-Side_Request_Forgery_%28SSRF%29/",
+  };
+};
