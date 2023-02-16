1b4b82601a9043bd44dfa3657482113c7ee1d646
vjerci <27707350+vjerci@users.noreply.github.com>
2023-02-16 12:09:33 +0100

    feat: add rollbar

--- integration/rules/javascript_test.go
+++ integration/rules/javascript_test.go
@@ -91,3 +91,8 @@ func TestJavascriptSegmentSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/third_parties/segment", "summary", "javascript_third_parties_segment", t)
 }
+
+func TestJavascriptRollbarSummary(t *testing.T) {
+	t.Parallel()
+	runRulesTest("javascript/third_parties/rollbar", "summary", "javascript_rollbar", t)
+}
---
+++ pkg/commands/process/settings/rules/javascript/third_parties/rollbar/testdata/browser_unsecure.js
@@ -0,0 +1,3 @@
+const user = { name: "jhon", email: "jhon@gmail.com" };
+
+Rollbar.critical("Connection error from remote Payments API", user);
