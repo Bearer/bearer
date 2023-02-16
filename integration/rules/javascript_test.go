457310c48f085d52cf96d3bf1ae166679f324234
Vjeran Fistric <27707350+vjerci@users.noreply.github.com>
2023-02-16 11:54:57 +0100

    chore: remove dataflow tests (#589)

--- integration/rules/javascript_test.go
+++ integration/rules/javascript_test.go
@@ -7,24 +7,9 @@ func TestJavascriptLangLoggerSummary(t *testing.T) {
 	runRulesTest("javascript/lang/logger", "summary", "javascript_lang_logger", t)
 }
 
-func TestJavascriptLangLoggerDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/lang/logger", "dataflow", "javascript_lang_logger", t)
-}
-
 func TestJavascriptLangSessionSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/lang/session", "summary", "javascript_session", t)
-}
-
-func TestJavascriptLangSessionDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/lang/session", "dataflow", "javascript_session", t)
-}
-
-func TestJavascriptWeakEncryptionDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/lang/weak_encryption", "dataflow", "javascript_weak_encryption", t)
 }
 
 func TestJavascriptWeakEncryptionSummary(t *testing.T) {
@@ -32,29 +17,14 @@ func TestJavascriptWeakEncryptionSummary(t *testing.T) {
 	runRulesTest("javascript/lang/weak_encryption", "summary", "javascript_weak_encryption", t)
 }
 
-func TestExpressSecureCookieDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/express/insecure_cookie", "dataflow", "express_insecure_cookie", t)
-}
-
 func TestExpressSecureCookieSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/express/insecure_cookie", "summary", "express_insecure_cookie", t)
 }
 
-func TestJavascriptJWTDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/lang/jwt", "dataflow", "javascript_jwt", t)
-}
-
 func TestJavascriptJWTSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/lang/jwt", "summary", "javascript_jwt", t)
-}
-
-func TestJavascriptHTTPInsecureDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/lang/http_insecure", "dataflow", "javascript_http_insecure", t)
 }
 
 func TestJavascriptHTTPInsecureSummary(t *testing.T) {
@@ -67,19 +37,9 @@ func TestJavascriptThirdPartySentrySummary(t *testing.T) {
 	runRulesTest("javascript/third_parties/sentry", "summary", "javascript_third_parties_sentry", t)
 }
 
-func TestJavascriptThirdPartySentryDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/third_parties/sentry", "dataflow", "javascript_third_parties_sentry", t)
-}
-
 func TestJavascriptLangExceptionSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/lang/exception", "summary", "javascript_lang_exception", t)
-}
-
-func TestJavascriptLangExceptionDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/lang/exception", "dataflow", "javascript_lang_exception", t)
 }
 
 func TestJavascriptLangFileGenerationSummary(t *testing.T) {
@@ -87,24 +47,9 @@ func TestJavascriptLangFileGenerationSummary(t *testing.T) {
 	runRulesTest("javascript/lang/file_generation", "summary", "javascript_lang_file_generation", t)
 }
 
-func TestJavascriptLangFileGenerationDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/lang/file_generation", "dataflow", "javascript_lang_file_generation", t)
-}
-
-func TestJavascriptGTMDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/third_parties/google_tag_manager", "dataflow", "javascript_google_tag_manager", t)
-}
-
 func TestJavascriptGTMSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/third_parties/google_tag_manager", "summary", "javascript_google_tag_manager", t)
-}
-
-func TestJavascriptGoogleAnalyticsDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/third_parties/google_analytics", "dataflow", "javascript_google_analytics", t)
 }
 
 func TestJavascriptGoogleAnalyticsSummary(t *testing.T) {
@@ -112,19 +57,9 @@ func TestJavascriptGoogleAnalyticsSummary(t *testing.T) {
 	runRulesTest("javascript/third_parties/google_analytics", "summary", "javascript_google_analytics", t)
 }
 
-func TestJavascriptReactGoogleAnalyticsDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/react/google_analytics", "dataflow", "javascript_react_google_analytics", t)
-}
-
 func TestJavascriptReactGoogleAnalyticsSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/react/google_analytics", "summary", "javascript_react_google_analytics", t)
-}
-
-func TestJavascriptAlgoliaDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/third_parties/algolia", "dataflow", "javascript_third_parties_algolia", t)
 }
 
 func TestJavascriptAlgoliaSummary(t *testing.T) {
@@ -132,28 +67,14 @@ func TestJavascriptAlgoliaSummary(t *testing.T) {
 	runRulesTest("javascript/third_parties/algolia", "summary", "javascript_third_parties_algolia", t)
 }
 
-func TestJavascriptDataDogDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/third_parties/datadog", "dataflow", "javascript_third_parties_datadog", t)
-}
-
 func TestJavascriptDataDogSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/third_parties/datadog", "summary", "javascript_third_parties_datadog", t)
 }
 
-func TestJavascriptDataDogBrowserDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/third_parties/datadog_browser", "dataflow", "javascript_third_parties_datadog_browser", t)
-}
-
 func TestJavascriptDataDogBrowserSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("javascript/third_parties/datadog_browser", "summary", "javascript_third_parties_datadog_browser", t)
-}
-func TestJavascriptElasticSearchDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("javascript/third_parties/elasticsearch", "dataflow", "javascript_elasticsearch", t)
 }
 
 func TestJavascriptElasticSearchSummary(t *testing.T) {
--- integration/rules/ruby_test.go
+++ integration/rules/ruby_test.go
@@ -9,29 +9,13 @@ func TestRubyLangCookiesSummary(t *testing.T) {
 	runRulesTest("ruby/lang/cookies", "summary", "ruby_lang_cookies", t)
 }
 
-func TestRubyLangCookiesDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/cookies", "dataflow", "ruby_lang_cookies", t)
-}
-
 func TestRubyLangDeserializationOfUserInputSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/lang/deserialization_of_user_input", "summary", "ruby_lang_deserialization_of_user_input", t)
 }
-
-func TestRubyLangDeserializationOfUserInputDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/deserialization_of_user_input", "dataflow", "ruby_lang_deserialization_of_user_input", t)
-}
-
 func TestRubyLangFileGenerationSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/lang/file_generation", "summary", "ruby_lang_file_generation", t)
-}
-
-func TestRubyLangFileGenerationDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/file_generation", "dataflow", "ruby_lang_file_generation", t)
 }
 
 func TestRubyLangHttpGetParamsSummary(t *testing.T) {
@@ -39,19 +23,9 @@ func TestRubyLangHttpGetParamsSummary(t *testing.T) {
 	runRulesTest("ruby/lang/http_get_params", "summary", "ruby_lang_http_get_params", t)
 }
 
-func TestRubyLangHttpGetParamsDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/http_get_params", "dataflow", "ruby_lang_http_get_params", t)
-}
-
 func TestRubyLangHttpInsecureSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/lang/http_insecure", "summary", "ruby_lang_http_insecure", t)
-}
-
-func TestRubyLangHttpInsecureDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/http_insecure", "dataflow", "ruby_lang_http_insecure", t)
 }
 
 func TestRubyLangHttpPostInsecureWithDataSummary(t *testing.T) {
@@ -59,19 +33,9 @@ func TestRubyLangHttpPostInsecureWithDataSummary(t *testing.T) {
 	runRulesTest("ruby/lang/http_post_insecure_with_data", "summary", "ruby_lang_http_post_insecure_with_data", t)
 }
 
-func TestRubyLangHttpPostInsecureWithDataDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/http_post_insecure_with_data", "dataflow", "ruby_lang_http_post_insecure_with_data", t)
-}
-
 func TestRubyLangInsecureFtpSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/lang/insecure_ftp", "summary", "ruby_lang_insecure_ftp", t)
-}
-
-func TestRubyLangInsecureFtpDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/insecure_ftp", "dataflow", "ruby_lang_insecure_ftp", t)
 }
 
 func TestRubyLangJwtSummary(t *testing.T) {
@@ -79,19 +43,9 @@ func TestRubyLangJwtSummary(t *testing.T) {
 	runRulesTest("ruby/lang/jwt", "summary", "ruby_lang_jwt", t)
 }
 
-func TestRubyLangJwtDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/jwt", "dataflow", "ruby_lang_jwt", t)
-}
-
 func TestRubyLangLoggerSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/lang/logger", "summary", "ruby_lang_logger", t)
-}
-
-func TestRubyLangLoggerDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/logger", "dataflow", "ruby_lang_logger", t)
 }
 
 func TestRubyLangExceptionSummary(t *testing.T) {
@@ -99,19 +53,9 @@ func TestRubyLangExceptionSummary(t *testing.T) {
 	runRulesTest("ruby/lang/exception", "summary", "ruby_lang_exception", t)
 }
 
-func TestRubyLangExceptionDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/exception", "dataflow", "ruby_lang_exception", t)
-}
-
 func TestRubyLangSslVerificationSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/lang/ssl_verification", "summary", "ruby_lang_ssl_verification", t)
-}
-
-func TestRubyLangSslVerificationDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/ssl_verification", "dataflow", "ruby_lang_ssl_verification", t)
 }
 
 func TestRubyLangWeakEncryptionSummary(t *testing.T) {
@@ -119,19 +63,9 @@ func TestRubyLangWeakEncryptionSummary(t *testing.T) {
 	runRulesTest("ruby/lang/weak_encryption", "summary", "ruby_lang_weak_encryption", t)
 }
 
-func TestRubyLangWeakEncryptionDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/weak_encryption", "dataflow", "ruby_lang_weak_encryption", t)
-}
-
 func TestRubyLangWeakEncryptionWithDataSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/lang/weak_encryption_with_data", "summary", "ruby_lang_weak_encryption_with_data", t)
-}
-
-func TestRubyLangWeakEncryptionWithDataDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/lang/weak_encryption_with_data", "dataflow", "ruby_lang_weak_encryption_with_data", t)
 }
 
 func TestRubyRailsDefaultEncryptionSummary(t *testing.T) {
@@ -139,19 +73,9 @@ func TestRubyRailsDefaultEncryptionSummary(t *testing.T) {
 	runRulesTest("ruby/rails/default_encryption", "summary", "ruby_rails_default_encryption", t)
 }
 
-func TestRubyRailsDefaultEncryptionDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/rails/default_encryption", "dataflow", "ruby_rails_default_encryption", t)
-}
-
 func TestRubyRailsInsecureCommunicationSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/rails/insecure_communication", "summary", "ruby_rails_insecure_communication", t)
-}
-
-func TestRubyRailsInsecureCommunicationDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/rails/insecure_communication", "dataflow", "ruby_rails_insecure_communication", t)
 }
 
 func TestRubyRailsInsecureSmtpSummary(t *testing.T) {
@@ -159,19 +83,9 @@ func TestRubyRailsInsecureSmtpSummary(t *testing.T) {
 	runRulesTest("ruby/rails/insecure_smtp", "summary", "ruby_rails_insecure_smtp", t)
 }
 
-func TestRubyRailsInsecureSmtpDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/rails/insecure_smtp", "dataflow", "ruby_rails_insecure_smtp", t)
-}
-
 func TestRubyRailsLoggerSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/rails/logger", "summary", "ruby_rails_logger", t)
-}
-
-func TestRubyRailsLoggerDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/rails/logger", "dataflow", "ruby_rails_logger", t)
 }
 
 func TestRubyRailsPasswordLengthSummary(t *testing.T) {
@@ -179,19 +93,9 @@ func TestRubyRailsPasswordLengthSummary(t *testing.T) {
 	runRulesTest("ruby/rails/password_length", "summary", "ruby_rails_password_length", t)
 }
 
-func TestRubyRailsPasswordLengthDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/rails/password_length", "dataflow", "ruby_rails_password_length", t)
-}
-
 func TestRubyRailsSessionSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/rails/session", "summary", "ruby_rails_session", t)
-}
-
-func TestRubyRailsSessionDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/rails/session", "dataflow", "ruby_rails_session", t)
 }
 
 func TestRubyThirdPartiesAlgoliaSummary(t *testing.T) {
@@ -199,19 +103,9 @@ func TestRubyThirdPartiesAlgoliaSummary(t *testing.T) {
 	runRulesTest("ruby/third_parties/algolia", "summary", "ruby_third_parties_algolia", t)
 }
 
-func TestRubyThirdPartiesAlgoliaDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/algolia", "dataflow", "ruby_third_parties_algolia", t)
-}
-
 func TestRubyThirdPartiesBigQuerySummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/third_parties/bigquery", "summary", "ruby_third_parties_bigquery", t)
-}
-
-func TestRubyThirdPartiesBigQueryDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/bigquery", "dataflow", "ruby_third_parties_bigquery", t)
 }
 
 func TestRubyThirdPartiesDatadogSummary(t *testing.T) {
@@ -219,19 +113,9 @@ func TestRubyThirdPartiesDatadogSummary(t *testing.T) {
 	runRulesTest("ruby/third_parties/datadog", "summary", "ruby_third_parties_datadog", t)
 }
 
-func TestRubyThirdPartiesDatadogDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/datadog", "dataflow", "ruby_third_parties_datadog", t)
-}
-
 func TestRubyThirdPartiesElasticsearchSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/third_parties/elasticsearch", "summary", "ruby_third_parties_elasticsearch", t)
-}
-
-func TestRubyThirdPartiesElasticsearchDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/elasticsearch", "dataflow", "ruby_third_parties_elasticsearch", t)
 }
 
 func TestRubyThirdPartiesNewRelicSummary(t *testing.T) {
@@ -239,19 +123,9 @@ func TestRubyThirdPartiesNewRelicSummary(t *testing.T) {
 	runRulesTest("ruby/third_parties/new_relic", "summary", "ruby_third_parties_new_relic", t)
 }
 
-func TestRubyThirdPartiesNewRelicDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/new_relic", "dataflow", "ruby_third_parties_new_relic", t)
-}
-
 func TestRubyThirdPartiesRollbarSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/third_parties/rollbar", "summary", "ruby_third_parties_rollbar", t)
-}
-
-func TestRubyThirdPartiesRollbarDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/rollbar", "dataflow", "ruby_third_parties_rollbar", t)
 }
 
 func TestRubyThirdPartiesScoutAPMSummary(t *testing.T) {
@@ -259,19 +133,9 @@ func TestRubyThirdPartiesScoutAPMSummary(t *testing.T) {
 	runRulesTest("ruby/third_parties/scout_apm", "summary", "ruby_third_parties_scout_apm", t)
 }
 
-func TestRubyThirdPartiesScoutAPMDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/scout_apm", "dataflow", "ruby_third_parties_scout_apm", t)
-}
-
 func TestRubyThirdPartiesSentrySummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/third_parties/sentry", "summary", "ruby_third_parties_sentry", t)
-}
-
-func TestRubyThirdPartiesSentryDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/sentry", "dataflow", "ruby_third_parties_sentry", t)
 }
 
 func TestRubyThirdPartiesBugsnagSummary(t *testing.T) {
@@ -279,19 +143,9 @@ func TestRubyThirdPartiesBugsnagSummary(t *testing.T) {
 	runRulesTest("ruby/third_parties/bugsnag", "summary", "ruby_third_parties_bugsnag", t)
 }
 
-func TestRubyThirdPartiesBugsnagDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/bugsnag", "dataflow", "ruby_third_parties_bugsnag", t)
-}
-
 func TestRubyThirdPartiesHoneybadgerSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/third_parties/honeybadger", "summary", "ruby_third_parties_honeybadger", t)
-}
-
-func TestRubyThirdPartiesHoneybadgerDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/honeybadger", "dataflow", "ruby_third_parties_honeybadger", t)
 }
 
 func TestRubyThirdPartiesAirbrakeSummary(t *testing.T) {
@@ -299,19 +153,9 @@ func TestRubyThirdPartiesAirbrakeSummary(t *testing.T) {
 	runRulesTest("ruby/third_parties/airbrake", "summary", "ruby_third_parties_airbrake", t)
 }
 
-func TestRubyThirdPartiesAirbrakeDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/airbrake", "dataflow", "ruby_third_parties_airbrake", t)
-}
-
 func TestRubyThirdPartiesOpenTelemetrySummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/third_parties/open_telemetry", "summary", "ruby_third_parties_open_telemetry", t)
-}
-
-func TestRubyThirdPartiesOpenTelemetryDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/open_telemetry", "dataflow", "ruby_third_parties_open_telemetry", t)
 }
 
 func TestRubyThirdPartiesSegmentSummary(t *testing.T) {
@@ -319,19 +163,9 @@ func TestRubyThirdPartiesSegmentSummary(t *testing.T) {
 	runRulesTest("ruby/third_parties/segment", "summary", "ruby_third_parties_segment", t)
 }
 
-func TestRubyThirdPartiesSegmentDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/segment", "dataflow", "ruby_third_parties_segment", t)
-}
-
 func TestRubyThirdPartiesGoogleDataflowSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/third_parties/google_dataflow", "summary", "ruby_third_parties_google_dataflow", t)
-}
-
-func TestRubyThirdPartiesGoogleDataflowDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/google_dataflow", "dataflow", "ruby_third_parties_google_dataflow", t)
 }
 
 func TestRubyThirdPartiesGoogleAnalyticsSummary(t *testing.T) {
@@ -339,17 +173,7 @@ func TestRubyThirdPartiesGoogleAnalyticsSummary(t *testing.T) {
 	runRulesTest("ruby/third_parties/google_analytics", "summary", "ruby_third_parties_google_analytics", t)
 }
 
-func TestRubyThirdPartiesGoogleAnalyticsDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/google_analytics", "dataflow", "ruby_third_parties_google_analytics", t)
-}
-
 func TestRubyThirdPartiesClickHouseSummary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("ruby/third_parties/clickhouse", "summary", "ruby_third_parties_clickhouse", t)
-}
-
-func TestRubyThirdPartiesClickHouseDataflow(t *testing.T) {
-	t.Parallel()
-	runRulesTest("ruby/third_parties/clickhouse", "dataflow", "ruby_third_parties_clickhouse", t)
 }
