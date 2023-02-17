f3f75f698bd48d1cd644d2f568268c549ece18cc
Mark Michon <mark.michon@bearer.sh>
2023-02-17 15:35:49 -0800

    fix favicon url

--- docs/_data/meta.js
+++ docs/_data/meta.js
@@ -5,7 +5,7 @@ module.exports = {
   url: "https://docs.bearer.com",
   description:
     "Bearer is an open Source code security scanning tool that natively filters and prioritizes security risks by business impact",
-  favicon: "https://bearer.sh/assets/img/favicon.svg",
+  favicon: "/assets/img/favicon.svg",
   links: {
     discord: "https://discord.gg/eaHZBJUXRF",
     issues: "https://github.com/Bearer/bearer/issues",
