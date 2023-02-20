83e1de8702cf9a141a706171169ceaf9af3027da
vjerci <27707350+vjerci@users.noreply.github.com>
2023-02-20 15:48:32 +0100

    feat: update simple rule

--- e2e/rules/rules_test.go
+++ e2e/rules/rules_test.go
@@ -8,3 +8,8 @@ func TestAuxilary(t *testing.T) {
 	t.Parallel()
 	runRulesTest("auxilary", "javascript_third_parties_datadog_test", t)
 }
+
+func TestSimpleRuby(t *testing.T) {
+	t.Parallel()
+	runRulesTest("simple_ruby", "ruby_rails_insecure_communication_test", t)
+}
