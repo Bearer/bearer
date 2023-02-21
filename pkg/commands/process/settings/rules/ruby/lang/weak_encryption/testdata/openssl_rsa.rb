94ae054100827512aabc3de6ad30fc739c190b21
vjerci <27707350+vjerci@users.noreply.github.com>
2023-02-21 17:47:31 +0100

    feat: implement rsa min length

--- pkg/commands/process/settings/rules/ruby/lang/weak_encryption/testdata/openssl_rsa.rb
+++ pkg/commands/process/settings/rules/ruby/lang/weak_encryption/testdata/openssl_rsa.rb
@@ -1,13 +1,8 @@
 OpenSSL::PKey::RSA.new(File.read('rsa.pem')).private_encrypt("test")
 
-size = 2048
 cipher = OpenSSL::Cipher.new('aes-128-cbc')
-rsa_encrypt = OpenSSL::PKey::RSA.new(size)
+rsa_encrypt = OpenSSL::PKey::RSA.new(2048) # secure
 rsa_encrypt.export(cipher, "hello world") # secure
 
-low_size = 512
 OpenSSL::PKey::RSA.new(2048).to_pem(cipher, "hello world") # secure
-OpenSSL::PKey::RSA.new(512)
-OpenSSL::PKey::RSA.new(low_size)
 OpenSSL::PKey::RSA.new(512).to_pem(cipher, "hello world") # unsecure
-OpenSSL::PKey::RSA.new(low_size).to_pem(cipher, "hello world") #unsecure
--- pkg/commands/process/settings/rules/ruby/lang/weak_encryption_with_data/testdata/openssl_rsa_data.rb
+++ pkg/commands/process/settings/rules/ruby/lang/weak_encryption_with_data/testdata/openssl_rsa_data.rb
@@ -1,7 +1,7 @@
 OpenSSL::PKey::RSA.new(File.read('rsa.pem')).private_encrypt(user.password)
 
 cipher = OpenSSL::Cipher.new('aes-128-cbc')
-rsa_encrypt = OpenSSL::PKey::RSA.new(2048)
+rsa_encrypt = OpenSSL::PKey::RSA.new(1024)
 rsa_encrypt.export(cipher, user.password)
 
-OpenSSL::PKey::RSA.new(2048).to_pem(cipher, user.first_name)
+OpenSSL::PKey::RSA.new(1024).to_pem(cipher, user.first_name)
