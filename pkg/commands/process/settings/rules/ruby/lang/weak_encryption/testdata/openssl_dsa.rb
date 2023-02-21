2235c8652a5634b2f30206330aa9aeb43d95da9a
vjerci <27707350+vjerci@users.noreply.github.com>
2023-02-21 14:05:44 +0100

    feat: built test case that should work

--- pkg/commands/process/settings/rules/ruby/lang/weak_encryption/testdata/openssl_dsa.rb
+++ pkg/commands/process/settings/rules/ruby/lang/weak_encryption/testdata/openssl_dsa.rb
@@ -1,11 +1,5 @@
-size = 2048
 cipher = OpenSSL::Cipher.new('aes-128-cbc')
-rsa_encrypt = OpenSSL::PKey::RSA.new(size)
+dsa_encrypt = OpenSSL::PKey::DSA.new(2048)
 dsa_encrypt.export(cipher, "hello world")
 
-low_size = 512
-OpenSSL::PKey::RSA.new(2048).to_pem(cipher, "hello world")
-OpenSSL::PKey::RSA.new(512)
-OpenSSL::PKey::RSA.new(low_size)
-OpenSSL::PKey::RSA.new(512).to_pem(cipher, "hello world")
-OpenSSL::PKey::RSA.new(low_size).to_pem(cipher, "hello world")
+OpenSSL::PKey::DSA.new(2048).to_pem(cipher, "hello world")
