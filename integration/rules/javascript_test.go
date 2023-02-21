d10628b22eaf6b3975e359ae9f9fe1f35119d13e
vjerci <27707350+vjerci@users.noreply.github.com>
2023-02-21 18:53:00 +0100

    feat: improve tests

--- integration/rules/javascript_test.go
+++ integration/rules/javascript_test.go
@@ -5,105 +5,131 @@ import "testing"
 const javascriptRulesPath string = "../../pkg/commands/process/settings/rules/javascript/"
 
 func TestJavascriptLangLogger(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"lang/logger")
 }
 
 func TestJavascriptLangSession(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"lang/session")
 }
 
 func TestJavascriptWeakEncryption(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"lang/weak_encryption")
 }
 
 func TestJavascriptJWT(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"lang/jwt")
 }
 
 func TestJavascriptHTTPInsecure(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"lang/http_insecure")
 }
 
 func TestJavascriptLangException(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"lang/exception")
 }
 
 func TestJavascriptLangFileGeneration(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"lang/file_generation")
 }
 
 func TestJavascriptExpressUnsafeDeserialization(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"express/unsafe_deserialization")
 }
 
 func TestJavascriptExpressExposedDirListing(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"express/exposed_dir_listing")
 }
 
 func TestExpressSecureCookie(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"express/insecure_cookie")
 }
 
 func TestExpressInsecureXmlRef(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"express/insecure_xml_ref")
 }
 
 func TestJavascriptReactGoogleAnalytics(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"react/google_analytics")
 }
 
 func TestJavascriptThirdPartySentry(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/sentry")
 }
 
 func TestJavascriptGTM(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/google_tag_manager")
 }
 
 func TestJavascriptGoogleAnalytics(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/google_analytics")
 }
 
 func TestJavascriptAlgolia(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/algolia")
 }
 
 func TestJavascriptDataDog(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/datadog")
 }
 
 func TestJavascriptDataDogBrowser(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/datadog_browser")
 }
 
 func TestJavascriptElasticSearch(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/elasticsearch")
 }
 
 func TestJavascriptSegmentDataflow(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/segment")
 }
 
 func TestJavascriptNewRelic(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/new_relic")
 }
 
 func TestJavascriptRollbar(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/rollbar")
 }
 
 func TestJavascriptHoneybadger(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/honeybadger")
 }
 
 func TestJavascriptAirbrake(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/airbrake")
 }
 
 func TestJavascriptOpenTelemetry(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/open_telemetry")
 }
 
 func TestJavascriptBugsnag(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"third_parties/bugsnag")
 }
--- integration/rules/ruby_test.go
+++ integration/rules/ruby_test.go
@@ -7,161 +7,201 @@ import (
 const rubyRulesPath string = "../../pkg/commands/process/settings/rules/ruby/"
 
 func TestRubyLangCookies(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/cookies")
 }
 
 func TestRubyLangDeserializationOfUserInput(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/deserialization_of_user_input")
 }
 
 func TestRubyLangEvalUsingUserInput(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/eval_using_user_input")
 }
 
 func TestRubyLangFileGeneration(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/file_generation")
 }
 
 func TestRubyLangFtpUsingUserInput(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/ftp_using_user_input")
 }
 
 func TestRubyLangHttpGetParams(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/http_get_params")
 }
 
 func TestRubyLangHttpInsecure(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/http_insecure")
 }
 
 func TestRubyLangHttpPostInsecureWithData(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/http_post_insecure_with_data")
 }
 
 func TestRubyLangInsecureFtp(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/insecure_ftp")
 }
 
 func TestRubyLangJwt(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/jwt")
 }
 
 func TestRubyLangLogger(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/logger")
 }
 
 func TestRubyLangException(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/exception")
 }
 
 func TestRubyLangPathUsingUserInput(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/path_using_user_input")
 }
 
 func TestRubyLangSslVerification(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/ssl_verification")
 }
 
 func TestRubyLangWeakEncryption(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/weak_encryption")
 }
 
 func TestRubyLangWeakEncryptionWithData(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"lang/weak_encryption_with_data")
 }
 
 func TestRubyRailsDefaultEncryption(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"rails/default_encryption")
 }
 
 func TestRubyRailsInsecureCommunication(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"rails/insecure_communication")
 }
 
 func TestRubyRailsInsecureHTTPPassowrd(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"rails/insecure_http_password")
 }
 
 func TestRubyRailsInsecureSmtp(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"rails/insecure_smtp")
 }
 
 func TestRubyRailsLogger(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"rails/logger")
 }
 
 func TestRubyRailsPasswordLength(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"rails/password_length")
 }
 
 func TestRubyRailsSession(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"rails/session")
 }
 
 func TestRubyRailsSessionKeyUsingUserInput(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"rails/session_key_using_user_input")
 }
 
 func TestRubyThirdPartiesAlgolia(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/algolia")
 }
 
 func TestRubyThirdPartiesBigQuery(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/bigquery")
 }
 
 func TestRubyThirdPartiesDatadog(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/datadog")
 }
 
 func TestRubyThirdPartiesElasticsearch(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/elasticsearch")
 }
 
 func TestRubyThirdPartiesNewRelic(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/new_relic")
 }
 
 func TestRubyThirdPartiesRollbar(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/rollbar")
 }
 
 func TestRubyThirdPartiesScoutAPM(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/scout_apm")
 }
 
 func TestRubyThirdPartiesSentry(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/sentry")
 }
 
 func TestRubyThirdPartiesBugsnag(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/bugsnag")
 }
 
 func TestRubyThirdPartiesHoneybadger(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/honeybadger")
 }
 
 func TestRubyThirdPartiesAirbrake(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/airbrake")
 }
 
 func TestRubyThirdPartiesOpenTelemetry(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/open_telemetry")
 }
 
 func TestRubyThirdPartiesSegment(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/segment")
 }
 
 func TestRubyThirdPartiesGoogleDataflow(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/google_dataflow")
 }
 
 func TestRubyThirdPartiesGoogleAnalytics(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/google_analytics")
 }
 
 func TestRubyThirdPartiesClickHouse(t *testing.T) {
+	t.Parallel()
 	getRunner(t).runTest(t, rubyRulesPath+"third_parties/clickhouse")
 }
