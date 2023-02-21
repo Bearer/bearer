a2b08e9e368f0a6eeeb0957eb8ed603a92185374
vjerci <27707350+vjerci@users.noreply.github.com>
2023-02-21 12:29:18 +0100

    feat: update weak encryption rule

--- pkg/commands/process/settings/rules/ruby/lang/weak_encryption/testdata/openssl_dsa.rb
+++ pkg/commands/process/settings/rules/ruby/lang/weak_encryption/testdata/openssl_dsa.rb
@@ -1,5 +1,11 @@
+size = 2048
 cipher = OpenSSL::Cipher.new('aes-128-cbc')
-dsa_encrypt = OpenSSL::PKey::DSA.new(2048)
+rsa_encrypt = OpenSSL::PKey::RSA.new(size)
 dsa_encrypt.export(cipher, "hello world")
 
-OpenSSL::PKey::DSA.new(2048).to_pem(cipher, "hello world")
+low_size = 512
+OpenSSL::PKey::RSA.new(2048).to_pem(cipher, "hello world")
+OpenSSL::PKey::RSA.new(512)
+OpenSSL::PKey::RSA.new(low_size)
+OpenSSL::PKey::RSA.new(512).to_pem(cipher, "hello world")
+OpenSSL::PKey::RSA.new(low_size).to_pem(cipher, "hello world")
