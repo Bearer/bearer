c286ad03c54ef45866d37ac30b7d81957c712a25
elsapet <elizabeth@bearer.sh>
2023-02-23 11:45:09 +0200

    feat(JS rules): add express rule for sql injection (#648)
    feat: add js express rule for sql injection

---
+++ pkg/commands/process/settings/rules/javascript/express/sql_injection/testdata/sequelize_sql_injection.js
@@ -0,0 +1,7 @@
+import { Sequelize } from "sequelize";
+
+module.exports.fooBar = function(req, _res) {
+  var sqlite = new Sequelize('sqlite::memory:')
+  var customerQuery = "SELECT * FROM customers WHERE status = " + req.params.customer.status
+  sqlite.query(customerQuery)
+}
