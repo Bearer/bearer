b4b60b826240d6d8f889c52b5d1c1026dfe7e597
David Roe <dave.roe@bearer.sh>
2023-02-23 16:47:29 +0000

    feat: support other usage of sendFile

--- pkg/commands/process/settings/rules/javascript/express/external_file_upload/testdata/ok_send_file.js
+++ pkg/commands/process/settings/rules/javascript/express/external_file_upload/testdata/ok_send_file.js
@@ -3,4 +3,5 @@ var app = express();
 
 app.get('/', function(_, res){
     res.sendFile('index.js')
+    res.sendFile(req.params.file, { root: path.join(__dirname, 'public') })
 });
--- pkg/commands/process/settings/rules/javascript/express/external_file_upload/testdata/send_file_with_external_input.js
+++ pkg/commands/process/settings/rules/javascript/express/external_file_upload/testdata/send_file_with_external_input.js
@@ -7,4 +7,6 @@ app.get('/', function(req, res){
 
     res.sendFile(file);
     res.sendFile(path.resolve(file));
+    res.sendFile(req.params.file, {}, () => { })
+    res.sendFile("file.txt", { root: path.join(__dirname, req.params.root) })
 });
