eca4acae1e40fa0d08feb6db2b380f9f64e5530d
elsapet <elizabeth@bearer.sh>
2023-02-23 17:28:13 +0200

    feat: add msql2

---
+++ pkg/commands/process/settings/rules/javascript/aws_lambda/sql_injection/testdata/mysql2_sql_injection.js
@@ -0,0 +1,16 @@
+const connection = mysql.createConnection({});
+const asyncConn = await mysql.createConnection({});
+
+exports.handler = async function(event, context) {
+  connection.query("SELECT * FROM `user` WHERE name = " + event.customer.name);
+
+  await asyncConn.execute("SELECT * FROM `admin_users` WHERE ID = " + event.admin.id)
+
+  // pool query
+  var pool = mysql.createPool()
+  pool.query("SELECT * FROM users WHERE name = " + event.user_name, function() {})
+  pool.getConnection(function(_err, conn) {
+    conn.query("SELECT * FROM users WHERE name = " + event.user_name, function() {})
+    pool.releaseConnection(conn)
+  })
+}
--- pkg/commands/process/settings/rules/javascript/aws_lambda/sql_injection/testdata/ok_no_injection.js
+++ pkg/commands/process/settings/rules/javascript/aws_lambda/sql_injection/testdata/ok_no_injection.js
@@ -7,12 +7,17 @@ const knex = require('knex')({
   client: 'mysql',
 })
 
+const connection = mysql.createConnection({});
+const asyncConn = await mysql.createConnection({});
+
 exports.handler =  async function(_event, _context) {
   var user = getCurrentUser()
   var userRes = pgClient.query('SELECT * FROM users WHERE user.name = ' + user.name)
   var res = knex.select('user.cart_details')
     .from('users')
     .whereRaw('id = '+ userRes.id)
+
+  connection.query("SELECT * FROM `user` WHERE name = " + userRes.name);
 
   return res
 }
