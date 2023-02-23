ce76b0b7f0e7888293fa3db0b2877bac3bfc98a2
elsapet <elizabeth@bearer.sh>
2023-02-23 15:51:36 +0200

    feat: add knex lib

--- integration/rules/javascript_test.go
+++ integration/rules/javascript_test.go
@@ -49,6 +49,11 @@ func TestJavascriptLangFileGeneration(t *testing.T) {
 	getRunner(t).runTest(t, javascriptRulesPath+"lang/file_generation")
 }
 
+func TestJavascriptAwsLambdaSqlInjection(t *testing.T) {
+	t.Parallel()
+	getRunner(t).runTest(t, javascriptRulesPath+"aws_lambda/sql_injection")
+}
+
 func TestJavascriptExpressOpenRedirect(t *testing.T) {
 	t.Parallel()
 	getRunner(t).runTest(t, javascriptRulesPath+"express/open_redirect")
