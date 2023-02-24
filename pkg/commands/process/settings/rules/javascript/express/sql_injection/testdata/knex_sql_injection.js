c86f79b0a5a48f77a944142cdf21e89bba029311
vjerci <27707350+vjerci@users.noreply.github.com>
2023-02-24 13:43:14 +0100

    chore: merge knex with other sql injection rules

--- pkg/commands/process/settings/rules/javascript/express/sql_injection/testdata/knex_sql_injection.js
+++ pkg/commands/process/settings/rules/javascript/express/sql_injection/testdata/knex_sql_injection.js
@@ -1,11 +1,12 @@
-const knex = require('knex')({
-  client: 'mysql',
-})
+const knex = require("knex")({
+	client: "mysql",
+});
 
-module.exports.badQuery = function(req, res) {
-  var cartDetails = knex.select('user.cart_details')
-    .from('users')
-    .whereRaw('name = '+ req.query.user.name)
+module.exports.badQuery = function (req, res) {
+	var cartDetails = knex
+		.select("user.cart_details")
+		.from("users")
+		.whereRaw("name = " + req.query.user.name);
 
-  res.send(prepareJson(cartDetails))
-}
+	res.send(prepareJson(cartDetails));
+};
--- pkg/commands/process/settings/rules/javascript/express/sql_injection/testdata/sequelize_sql_injection.js
+++ pkg/commands/process/settings/rules/javascript/express/sql_injection/testdata/sequelize_sql_injection.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
-module.exports.fooBar = function(req, _res) {
-  var sqlite = new Sequelize('sqlite::memory:')
-  var customerQuery = "SELECT * FROM customers WHERE status = " + req.params.customer.status
-  sqlite.query(customerQuery)
-}
+module.exports.fooBar = function (req, _res) {
+	var sqlite = new Sequelize("sqlite::memory:");
+	var customerQuery =
+		"SELECT * FROM customers WHERE status = " + req.params.customer.status;
+	sqlite.query(customerQuery);
+};
