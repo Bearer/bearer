aca99fd78ae4ddfd7e209e0755a11fc3ab79bad9
elsapet <elizabeth@bearer.sh>
2023-02-27 10:44:06 +0200

    fix: stricter match for req body

--- pkg/commands/process/settings/rules/javascript/express/cross_site_scripting/testdata/no_xss.js
+++ pkg/commands/process/settings/rules/javascript/express/cross_site_scripting/testdata/no_xss.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/goos", (_, res) => {
-  res.send("<p>hello world</p>")
+app.get("/good", (_, res) => {
+  return res.send("<p>hello world</p>")
+})
+
+app.get("/good-2", (req, res) => {
+  return res.send(JSON.stringify({
+    view: goodView,
+    success: true,
+    text: 'Good practice'
+  }))
+})
+
+app.get("/good-3", () => {
+  const results = newResult(search);
+
+  res.send(results);
+})
+
+app.get("/good-4", () => {
+  return res.send({ success: false, text: `User ${req.params.user_id} not found` });
 })
