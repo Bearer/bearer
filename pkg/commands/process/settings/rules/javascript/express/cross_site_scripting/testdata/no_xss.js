96b92ca4e741accba62522cc8306d4e94473ea4f
elsapet <elizabeth@bearer.sh>
2023-02-27 11:27:55 +0200

    fix: update test data

--- pkg/commands/process/settings/rules/javascript/express/cross_site_scripting/testdata/no_xss.js
+++ pkg/commands/process/settings/rules/javascript/express/cross_site_scripting/testdata/no_xss.js
@@ -5,20 +5,13 @@ app.get("/good", (_, res) => {
   return res.send("<p>hello world</p>")
 })
 
-app.get("/good-2", (req, res) => {
-  return res.send(JSON.stringify({
-    view: goodView,
-    success: true,
-    text: 'Good practice'
-  }))
+app.get("/good-2", () => {
+  // don't match on req params within strings
+  return res.send({ success: false, text: `User ${req.params.user_id} not found` });
 })
 
 app.get("/good-3", () => {
-  const results = newResult(search);
-
-  res.send(results);
-})
-
-app.get("/good-4", () => {
-  return res.send({ success: false, text: `User ${req.params.user_id} not found` });
+  // don't match on custom req attributes
+  const userSettings = req.user.settings
+  return res.send(userSettings)
 })
