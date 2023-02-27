54de8e549b9f08f330af2674bffb4435b258143a
elsapet <elizabeth@bearer.sh>
2023-02-27 12:06:40 +0200

    fix: handle plain cases

--- pkg/commands/process/settings/rules/javascript/express/server_side_request_forgery/testdata/ok_no_ssrf.js
+++ pkg/commands/process/settings/rules/javascript/express/server_side_request_forgery/testdata/ok_no_ssrf.js
@@ -10,3 +10,11 @@ app.get("/safety", async (_req, res) => {
 
   res.send("success")
 });
+
+app.get("/safety-2", async(req, res) => {
+  var token = req.user.tokens.find((token) => token.kind === "safe");
+  axios.get(`https://mish.com/bears?access_token=${token.accessToken}`)
+  axios.get("https://mish.com/bears?access_token=" + token.accessToken)
+
+  res.send("success")
+})
