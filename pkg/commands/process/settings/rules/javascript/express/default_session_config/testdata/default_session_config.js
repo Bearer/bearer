8f893cddb8cde1f80505883fee05fdd3cf2dde67
elsapet <elizabeth@bearer.sh>
2023-03-01 17:11:57 +0200

    fix: fix session rule

--- pkg/commands/process/settings/rules/javascript/express/default_session_config/testdata/default_session_config.js
+++ pkg/commands/process/settings/rules/javascript/express/default_session_config/testdata/default_session_config.js
@@ -2,15 +2,4 @@ const session = require("express-session");
 const express = require("express");
 const app = express();
 
-app.use(
-	session({
-    name: "my-custom-session-name",
-		cookie: {
-			domain: "example.com",
-			secure: true,
-			httpOnly: false,
-			maxAge: 24 * 60 * 60 * 1000,
-			path: "/some-path"
-		},
-	})
-);
+app.use(session({}));
--- pkg/commands/process/settings/rules/javascript/express/default_session_config/testdata/ok_session_configured.js
+++ pkg/commands/process/settings/rules/javascript/express/default_session_config/testdata/ok_session_configured.js
@@ -3,14 +3,5 @@ const express = require("express");
 const app = express();
 
 app.use(
-	session({
-    name: "my-custom-session-name",
-		cookie: {
-			domain: "example.com",
-			secure: true,
-			httpOnly: false,
-			maxAge: 24 * 60 * 60 * 1000,
-			path: "/some-path"
-		},
-	})
+	session({ name: "my-custom-session-name" })
 );
