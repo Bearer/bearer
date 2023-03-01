0d94455227733340401d864a72c1351249d6594b
elsapet <elizabeth@bearer.sh>
2023-03-01 12:04:08 +0200

    feat(JS rules): add CWE-525 bad caching policy for expressjs JWT not revoked (#695)
    feat: add cwe-525 bad caching policy / express JWT not revoked

---
+++ pkg/commands/process/settings/rules/javascript/express/jwt_not_revoked/testdata/ok_express_jwt_revoked.js
@@ -0,0 +1,10 @@
+import { expressjwt } from "express-jwt";
+
+app.get(
+  "/revoked",
+  expressjwt({ secret: currentUser.email, isRevoked: this.customRevokeCall(), algorithms: ["HS256"] }),
+  function (req, res) {
+    if (!req.auth.admin) return res.sendStatus(401);
+    res.sendStatus(200);
+  }
+);
