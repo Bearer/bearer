76f1ea3f9e79c47b12269aef9ece8fdf408d0d7a
elsapet <elizabeth@bearer.sh>
2023-03-01 13:20:15 +0200

    feat: extend expressjs insecure cookie rule

--- pkg/commands/process/settings/rules/javascript/express/insecure_cookie/testdata/insecure_cookie.js
+++ pkg/commands/process/settings/rules/javascript/express/insecure_cookie/testdata/insecure_cookie.js
@@ -7,6 +7,33 @@ app.use(
 		cookie: {
 			domain: "example.com",
 			secure: false, // Ensures the browser only sends the cookie over HTTPS.
+			httpOnly: false,
+			name: "my-custom-cookie-name",
+			maxAge: 24 * 60 * 60 * 1000,
+			path: "/some-path"
+		},
+	})
+);
+
+app.use(
+	session({
+		cookie: {
+			domain: "example.com",
+			secure: true,
+			httpOnly: true,
+			name: "my-custom-cookie-name",
+			maxAge: 24 * 60 * 60 * 1000,
+			path: "/some-path"
+		},
+	})
+);
+
+app.use(
+	session({
+		cookie: {
+			domain: "example.com",
+			secure: true,
+			httpOnly: false
 		},
 	})
 );
