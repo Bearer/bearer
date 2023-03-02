f4c381dcd7b8c8236820c25d6f246b6f0557c8b1
elsapet <elizabeth@bearer.sh>
2023-03-02 11:09:44 +0200

    feat: include failing test case

--- pkg/commands/process/settings/rules/javascript/aws_lambda/query_injection/testdata/dynamodb_query_injection.js
+++ pkg/commands/process/settings/rules/javascript/aws_lambda/query_injection/testdata/dynamodb_query_injection.js
@@ -17,3 +17,17 @@ var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-12-20'});
 exports.handler = async function(event, _context) {
   docClient.query(event["query"]["params"], function(err, data) {});
 }
+
+exports.handler = async function(event, _context) {
+  const params = {
+    KeyConditionExpression: "Title = " + getTitle(),
+    FilterExpression: "contains (Author, :name)",
+    ExpressionAttributeValues: {
+      ":name": { S: event["user"]["name"] },
+    },
+    ProjectionExpression: "Title, Author",
+    TableName: "BOOKS_TABLE",
+  };
+
+  const data = await ddbClient.send(new QueryCommand(params));
+}
