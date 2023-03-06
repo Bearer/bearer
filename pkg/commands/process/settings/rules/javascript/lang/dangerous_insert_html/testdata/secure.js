a194720e8d6ce12ccba3d9f564fc9e22ba459e31
elsapet <elizabeth@bearer.sh>
2023-03-06 15:08:55 +0200

    fix: fix insert HTML rule

--- pkg/commands/process/settings/rules/javascript/lang/dangerous_insert_html/testdata/secure.js
+++ pkg/commands/process/settings/rules/javascript/lang/dangerous_insert_html/testdata/secure.js
@@ -1,3 +1,10 @@
 function renderListItem(input) {
 	this.ref.insertAdjacentHTML("beforebegin", `<li>fixed list item</li>`);
+	this.ref.insertAdjacentHTML("beforebegin", `<li>fixed list item</li>`);
+	this.ref.innerHTML = "";
+	React.createElement(CssPropTringle, {
+		s: 100,
+		x: 0,
+		y: 0,
+	})
 }
