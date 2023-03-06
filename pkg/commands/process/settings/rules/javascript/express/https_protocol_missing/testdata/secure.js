904c886108b329291304b5649e286139e331121b
elsapet <elizabeth@bearer.sh>
2023-03-06 15:48:29 +0200

    fix: fix require case for https create server

--- pkg/commands/process/settings/rules/javascript/express/https_protocol_missing/testdata/secure.js
+++ pkg/commands/process/settings/rules/javascript/express/https_protocol_missing/testdata/secure.js
@@ -1,3 +1,15 @@
 https.createServer(httpsOptions, app).listen(port, () => {
   console.log("hello world");
 });
+
+require("https").createServer(httpsOptions, app).listen(port, () => {
+  console.log("hello world");
+});
+
+require('https').createServer(httpsOptions, app).listen(port, () => {
+  console.log("hello world");
+});
+
+require(`https`).createServer(httpsOptions, app).listen(port, () => {
+  console.log("hello world");
+});
