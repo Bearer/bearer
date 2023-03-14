f9b0f4f6711061a3a8cfa32e9c45e4289b4148ad
vjerci <27707350+vjerci@users.noreply.github.com>
2023-03-14 16:27:09 +0100

    feat: add test

--- pkg/commands/process/settings/rules/javascript/express/external_resource/testdata/require_destructured_resource.js
+++ pkg/commands/process/settings/rules/javascript/express/external_resource/testdata/require_destructured_resource.js
@@ -1,11 +1,2 @@
-var express = require("express");
-var helmet = require("helmet");
-
-var app = express();
-app.use(helmet());
-app.use(helmet.hidePoweredBy());
-
-app.get("/bad", (req, _res) => {
-	const { path } = req.query;
-	require(path);
-});
+const { path } = req.query;
+requestAnimationFrame(path);
