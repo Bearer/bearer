8e96a77029489a160ce41585e5525ab09fdf35af
Cédric Fabianski <cfabianski@me.com>
2023-03-21 21:10:14 +0100

    Revert "chore: disable api key for now"
    This reverts commit d9727aa665fa26b7d9fe0a345840f6b2d6c50e34.

--- pkg/flag/general_flags.go
+++ pkg/flag/general_flags.go
@@ -1,13 +1,13 @@
 package flag
 
 var (
-	// APIKeyFlag = Flag{
-	// 	Name:            "api-key",
-	// 	ConfigName:      "api-key",
-	// 	Value:           "",
-	// 	Usage:           "Use your Bearer API Key to send the report to Bearer.",
-	// 	DisableInConfig: true,
-	// }
+	APIKeyFlag = Flag{
+		Name:            "api-key",
+		ConfigName:      "api-key",
+		Value:           "",
+		Usage:           "Use your Bearer API Key to send the report to Bearer.",
+		DisableInConfig: true,
+	}
 	ConfigFileFlag = Flag{
 		Name:            "config-file",
 		ConfigName:      "config-file",
@@ -19,19 +19,19 @@ var (
 
 type GeneralFlagGroup struct {
 	ConfigFile *Flag
-	// APIKey     *Flag
+	APIKey     *Flag
 }
 
 // GlobalOptions defines flags and other configuration parameters for all the subcommands
 type GeneralOptions struct {
 	ConfigFile string `json:"config_file" yaml:"config_file"`
-	// APIKey     string `json:"api_key" yaml:"api_key"`
+	APIKey     string `json:"api_key" yaml:"api_key"`
 }
 
 func NewGeneralFlagGroup() *GeneralFlagGroup {
 	return &GeneralFlagGroup{
 		ConfigFile: &ConfigFileFlag,
-		// APIKey:     &APIKeyFlag,
+		APIKey:     &APIKeyFlag,
 	}
 }
 
@@ -42,13 +42,13 @@ func (f *GeneralFlagGroup) Name() string {
 func (f *GeneralFlagGroup) Flags() []*Flag {
 	return []*Flag{
 		f.ConfigFile,
-		// f.APIKey,
+		f.APIKey,
 	}
 }
 
 func (f *GeneralFlagGroup) ToOptions() GeneralOptions {
 	return GeneralOptions{
 		ConfigFile: getString(f.ConfigFile),
-		// APIKey:     getString(f.APIKey),
+		APIKey:     getString(f.APIKey),
 	}
 }
