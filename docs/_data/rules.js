63485dc7765c1820d0a8e4b91bc6bf818c868740
Mark Michon <mark.michon@bearer.sh>
2023-04-06 08:29:24 -0700

    docs: add external rules logic (#860)

--- docs/_data/rules.js
+++ docs/_data/rules.js
@@ -1,41 +1,62 @@
 const { readFile, readdir } = require("node:fs/promises");
 const { statSync } = require("fs");
+const EleventyFetch = require("@11ty/eleventy-fetch");
 const path = require("path");
 const yaml = require("js-yaml");
-const rulesPath = "../pkg/commands/process/settings/rules/";
 const cweList = require("./cweList.json");
+const gitly = require("gitly");
+const source = "bearer/bearer-rules";
+const rulesPath = "_tmp/rules-data";
+const excludeDirectories = [".github", "scripts"];
 
 function isDirectory(dir) {
   const result = statSync(dir);
   return result.isDirectory();
 }
 
+async function fetchRelease() {
+  let latest = {};
+  try {
+    latest = await EleventyFetch(
+      `https://api.github.com/repos/${source}/releases/latest`,
+      {
+        duration: "60m",
+        type: "json",
+      }
+    );
+  } catch (e) {
+    console.error(e);
+  }
+  try {
+    let src = await gitly.download(`${source}#${latest.tag_name}`);
+    await gitly.extract(src, rulesPath);
+  } catch (e) {
+    throw console.error(e);
+  }
+}
+
 async function fetchData(location) {
   let rules = [];
-  let groupedRules = [];
   try {
     const dirs = await readdir(location);
     // ex: looping through rules [ruby, gitleaks, sql]
     dirs.forEach(async (dir) => {
       const dirPath = path.join(rulesPath, dir);
-      if (isDirectory(dirPath)) {
-        const dirData = {
-          name: dir,
-          children: [],
-        };
-
+      if (isDirectory(dirPath) && !excludeDirectories.includes(dir)) {
         const subDirs = await readdir(dirPath);
         // ex. looping through rules/ruby [lang, rails]
         subDirs.forEach(async (subDir) => {
           const subDirPath = path.join(dirPath, subDir);
           if (isDirectory(subDirPath)) {
             const files = await readdir(subDirPath);
-            const children = await fetchAllFiles(subDirPath, files);
-            dirData.children.push(...children);
+            const children = await fetchAllFiles(
+              subDirPath,
+              path.join(dir, subDir),
+              files
+            );
             rules.push(...children);
           }
         });
-        groupedRules.push(dirData);
       }
     });
     return rules;
@@ -44,12 +65,12 @@ async function fetchData(location) {
   }
 }
 
-async function fetchAllFiles(directory, files) {
+async function fetchAllFiles(directory, breadcrumb, files) {
   let result = await Promise.all(
     files.reduce((all, file) => {
       const location = path.join(directory, file);
       if (path.extname(location) === ".yml") {
-        return [...all, fetchFile(path.join(directory, file))];
+        return [...all, fetchFile(path.join(directory, file), breadcrumb)];
       } else {
         return all;
       }
@@ -58,7 +79,7 @@ async function fetchAllFiles(directory, files) {
   return result;
 }
 
-async function fetchFile(location) {
+async function fetchFile(location, breadcrumb) {
   return readFile(location, { encoding: "utf8" }).then((file) => {
     let out = yaml.load(file);
     let owasps = new Set();
@@ -71,7 +92,7 @@ async function fetchFile(location) {
     }
     return {
       name: path.basename(location, ".yml"),
-      location: location.substring(2),
+      location: path.join(breadcrumb, path.basename(location, ".yml")),
       owasp_ids: [...owasps].sort(),
       ...out,
     };
@@ -79,5 +100,6 @@ async function fetchFile(location) {
 }
 
 module.exports = async function () {
-  return await fetchData("../pkg/commands/process/settings/rules/");
+  await fetchRelease();
+  return await fetchData(rulesPath);
 };
