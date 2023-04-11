e1d2e4728c9428c766dac28b0a337c180b5db694
Mark Michon <mark.michon@bearer.sh>
2023-04-11 10:31:19 -0700

    docs: initial recipes setup

---
+++ docs/_data/recipes.js
@@ -0,0 +1,31 @@
+const { readdir, readFile } = require("node:fs/promises");
+const path = require("path");
+
+async function fetchFile(location) {
+  return readFile(location, { encoding: "utf8" }).then((file) =>
+    JSON.parse(file)
+  );
+}
+
+async function fetchData(dir) {
+  try {
+    const files = await readdir(dir);
+    let result = await Promise.all(
+      files.map(async (file) => {
+        let data = await fetchFile(path.join(dir, file));
+        return {
+          ...data,
+          id: path.basename(file, ".json"),
+          source: `/pkg/classification/db/recipes/${file}`,
+        };
+      })
+    );
+    return result;
+  } catch (err) {
+    throw err;
+  }
+}
+module.exports = async function () {
+  let recipes = await fetchData("../pkg/classification/db/recipes/");
+  return recipes;
+};
