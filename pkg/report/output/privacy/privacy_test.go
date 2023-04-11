c41009fc244e66342613ae66ad9b28e84c3cf4f1
elsapet <elizabeth@bearer.sh>
2023-04-11 16:10:39 +0200

    fix: broken privacy rego (#885)
    * fix: broken privacy rego item
    
    * tests: add min test coverage for privacy report

---
+++ pkg/report/output/privacy/privacy_test.go
@@ -0,0 +1,134 @@
+package privacy_test
+
+import (
+	"testing"
+
+	"github.com/bearer/bearer/pkg/commands/process/settings"
+	"github.com/bearer/bearer/pkg/flag"
+	"github.com/bearer/bearer/pkg/report/output/dataflow"
+	"github.com/bearer/bearer/pkg/report/output/dataflow/types"
+	"github.com/bearer/bearer/pkg/report/output/privacy"
+	"github.com/bearer/bearer/pkg/report/schema"
+	"github.com/bradleyjkemp/cupaloy"
+)
+
+func TestBuildCsvString(t *testing.T) {
+	config, err := generateConfig(flag.ReportOptions{Report: "privacy"})
+	config.Rules = map[string]*settings.Rule{
+		"ruby_third_parties_sentry": config.Rules["ruby_third_parties_sentry"],
+	}
+
+	if err != nil {
+		t.Fatalf("failed to generate config:%s", err)
+	}
+
+	dataflow := dummyDataflow()
+
+	stringBuilder, _ := privacy.BuildCsvString(&dataflow, config)
+	cupaloy.SnapshotT(t, stringBuilder.String())
+}
+
+func TestGetOutput(t *testing.T) {
+	config, err := generateConfig(flag.ReportOptions{Report: "privacy"})
+	config.Rules = map[string]*settings.Rule{
+		"ruby_third_parties_sentry": config.Rules["ruby_third_parties_sentry"],
+	}
+
+	if err != nil {
+		t.Fatalf("failed to generate config:%s", err)
+	}
+
+	dataflow := dummyDataflow()
+	results, _, err := privacy.GetOutput(&dataflow, config)
+	if err != nil {
+		t.Fatalf("failed to generate privacy output err:%s", err)
+	}
+
+	cupaloy.SnapshotT(t, results)
+}
+
+func generateConfig(reportOptions flag.ReportOptions) (settings.Config, error) {
+	opts := flag.Options{
+		ScanOptions: flag.ScanOptions{
+			Scanner: []string{"sast"},
+		},
+		RuleOptions:    flag.RuleOptions{},
+		RepoOptions:    flag.RepoOptions{},
+		ReportOptions:  reportOptions,
+		GeneralOptions: flag.GeneralOptions{},
+	}
+
+	return settings.FromOptions(opts, []string{"ruby"})
+}
+
+func dummyDataflow() dataflow.DataFlow {
+	subject := "User"
+	thirdPartyRisk := types.RiskDetector{
+		DetectorID: "ruby_third_parties_sentry",
+		Locations: []types.RiskLocation{
+			{
+				Filename:   "/app/controllers/application_controller.rb",
+				LineNumber: 39,
+				Parent: &schema.Parent{
+					LineNumber: 38,
+					Content:    "Sentry.set_user(email: current_user.email)",
+				},
+				DataTypes: []types.RiskDatatype{
+					{
+						Name:         "Email Address",
+						CategoryUUID: "cef587dd-76db-430b-9e18-7b031e1a193b",
+						Schemas: []types.RiskSchema{
+							{
+								FieldName:   "email",
+								ObjectName:  "current_user",
+								SubjectName: &subject,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// build risk []interface
+	risks := make([]types.RiskDetector, 1)
+	risks[0] = thirdPartyRisk
+
+	return dataflow.DataFlow{
+		Datatypes: []types.Datatype{
+			{
+				Name:         "Email Address",
+				CategoryName: "Contact",
+				Detectors: []types.DatatypeDetector{
+					{
+						Name: "ruby",
+						Locations: []types.DatatypeLocation{
+							{
+								Filename:    "/app/controllers/application_controller.rb",
+								LineNumber:  39,
+								FieldName:   "email",
+								ObjectName:  "current_user",
+								SubjectName: &subject,
+							},
+						},
+					},
+				},
+			},
+		},
+		Risks: risks,
+		Components: []types.Component{
+			{
+				Name:    "Sentry",
+				Type:    "external_service",
+				SubType: "third_party",
+				Locations: []types.ComponentLocation{
+					{
+						Detector:   "gemfile-lock",
+						Filename:   "/Users/elsapet/Bearer/bear-publishing/Gemfile.lock",
+						LineNumber: 204,
+					},
+				},
+			},
+		},
+	}
+}
