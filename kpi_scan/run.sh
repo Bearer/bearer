30f6046dc93331543eefc4e902dbc1cf0aa3f661
David Roe <dave.roe@bearer.sh>
2023-04-13 11:02:36 +0100

    feat: kpi scan workflow

---
+++ kpi_scan/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+
+echo "Finding latest Bearer version"
+curl -H "Accept: application/vnd.github.v3+json" \
+     https://api.github.com/repos/bearer/bearer/releases/latest > /tmp/bearer-release.json
+
+BEARER_URL=$(jq -r '.assets[] | select(.name | test("bearer_.*_linux_amd64.tar.gz")).browser_download_url' /tmp/bearer-release.json)
+if [[ -z "$BEARER_URL" ]]; then
+  echo "Can't find bearer URL"
+  exit 1
+fi
+
+echo
+echo "Downloading Bearer from $BEARER_URL"
+curl --location --output /tmp/bearer.tar.gz "$BEARER_URL"
+tar --extract --gunzip --file /tmp/bearer.tar.gz --directory /tmp/
+
+echo
+echo "Cloning $REPOSITORY_URL"
+git clone "$REPOSITORY_URL" /tmp/repository
+
+echo
+echo "Scanning"
+/tmp/bearer scan --host=my.staging.bearer.sh --api-key "$API_KEY" /tmp/repository
