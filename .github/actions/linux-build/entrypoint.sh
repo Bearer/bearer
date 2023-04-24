c9ed44a9214fa688140cf35d69a46f1e8509192a
David Roe <didroe@gmail.com>
2023-04-24 15:06:06 +0100

    ci: add github docker auth to linux build image (#933)

--- .github/actions/linux-build/entrypoint.sh
+++ .github/actions/linux-build/entrypoint.sh
@@ -4,6 +4,10 @@ set -eu -o pipefail
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
+if [[ ! -z "${GITHUB_USER-}"]]; then
+  echo "$GITHUB_TOKEN" | docker login ghcr.io --username "$GITHUB_USER" --password-stdin
+fi
+
 git config --global --add safe.directory '*'
 
 exec goreleaser "$@"
