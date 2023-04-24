7aabaf47a354b28ca527cc2a2acd59239ade1856
David Roe <dave.roe@bearer.sh>
2023-04-24 15:20:28 +0100

    ci: fix typo in linux build entrypoint

--- .github/actions/linux-build/entrypoint.sh
+++ .github/actions/linux-build/entrypoint.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-if [[ ! -z "${GITHUB_USER-}"]]; then
+if [[ ! -z "${GITHUB_USER-}" ]]; then
   echo "$GITHUB_TOKEN" | docker login ghcr.io --username "$GITHUB_USER" --password-stdin
 fi
 
