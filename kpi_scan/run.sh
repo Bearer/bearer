67efd4ce9092866333a973ed9afdee3fcc256286
Cédric Fabianski <cedric@bearer.sh>
2023-05-04 16:41:16 +0200

    ci: use apt-get instead of binary dl (#963)

--- kpi_scan/run.sh
+++ kpi_scan/run.sh
@@ -2,21 +2,6 @@
 
 set -eu
 
-echo "Finding latest Bearer version"
-curl -H "Accept: application/vnd.github.v3+json" \
-     https://api.github.com/repos/bearer/bearer/releases/latest > /tmp/bearer-release.json
-
-BEARER_URL=$(jq -r '.assets[] | select(.name | test("bearer_.*_linux_amd64.tar.gz")).browser_download_url' /tmp/bearer-release.json)
-if [[ -z "$BEARER_URL" ]]; then
-  echo "Can't find bearer URL"
-  exit 1
-fi
-
-echo
-echo "Downloading Bearer from $BEARER_URL"
-curl --location --output /tmp/bearer.tar.gz "$BEARER_URL"
-tar --extract --gunzip --file /tmp/bearer.tar.gz --directory /tmp/
-
 echo
 echo "Cloning $REPOSITORY_URL"
 git clone --depth=1 --single-branch "$REPOSITORY_URL" /tmp/repository
@@ -24,4 +9,4 @@ cd /tmp/repository
 
 echo
 echo "Scanning"
-/tmp/bearer scan . "--host=$API_HOST" --api-key "$API_KEY"
+bearer scan . "--host=$API_HOST" --api-key "$API_KEY"
