7274a71248a8c7b324d4df56a06438ff4d794518
gotbadger <p.j.h@hey.com>
2023-06-02 10:21:22 +0100

    feat(output): add reviewdog output format for security report

---
+++ pkg/report/output/reviewdog/reviewdog.go
@@ -0,0 +1,58 @@
+package reviewdog
+
+import (
+	reviewdog "github.com/bearer/bearer/pkg/report/output/reviewdog/types"
+	"github.com/bearer/bearer/pkg/report/output/security"
+)
+
+func ReportReviewdog(outputDetections *map[string][]security.Result) (reviewdog.ReviewdogOutput, error) {
+	var reviewdogDiagnostics []reviewdog.Diagnostic
+
+	for _, level := range []string{"critical", "high", "medium", "low", "warning"} {
+		if findings, ok := (*outputDetections)[level]; ok {
+			for _, finding := range findings {
+				var severity string
+				if level == "warning" {
+					severity = "WARNING"
+				} else {
+					severity = "ERROR"
+				}
+
+				message := "# " + finding.Rule.Title + "\n" + finding.Rule.Description
+
+				reviewdogDiagnostics = append(reviewdogDiagnostics, reviewdog.Diagnostic{
+					Message:  message,
+					Severity: severity,
+					Location: reviewdog.Location{
+						Path: finding.Filename,
+						Range: reviewdog.LocationRange{
+							Start: reviewdog.LocationPosition{
+								Line:   finding.Sink.Start,
+								Column: finding.Sink.Column.Start,
+							},
+							End: reviewdog.LocationPosition{
+								Line:   finding.Sink.End,
+								Column: finding.Sink.Column.End,
+							},
+						},
+					},
+					Code: reviewdog.Code{
+						RuleId:           finding.Rule.Id,
+						DocumentationUrl: finding.Rule.DocumentationUrl,
+					},
+					Suggestions: []reviewdog.Suggestion{},
+				})
+			}
+		}
+	}
+
+	output := reviewdog.ReviewdogOutput{
+		Source: reviewdog.Source{
+			Name: "Bearer",
+			Url:  "https://docs.bearer.com/",
+		},
+		Diagnostics: reviewdogDiagnostics,
+	}
+
+	return output, nil
+}
---
+++ pkg/report/output/reviewdog/reviewdog_test.go
@@ -0,0 +1,42 @@
+package reviewdog
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/bearer/bearer/pkg/report/output"
+	"github.com/bearer/bearer/pkg/report/output/security"
+	"github.com/bradleyjkemp/cupaloy"
+)
+
+func TestRailsGoatReviewdog(t *testing.T) {
+	securityOutput, err := os.ReadFile("testdata/rails-goat-security-report.json")
+	if err != nil {
+		t.Fatalf("failed to read file, err: %s", err)
+	}
+
+	var securityResults map[string][]security.Result
+	err = json.Unmarshal(securityOutput, &securityResults)
+	if err != nil {
+		t.Fatalf("couldn't unmarshal file output: %s", err)
+	}
+
+	res, err := ReportReviewdog(&securityResults)
+	if err != nil {
+		t.Fatalf("failed to generate security output, err: %s", err)
+	}
+
+	sarifOutput, err := output.ReportJSON(res)
+	if err != nil {
+		t.Fatalf("failed to generate JSON output, err: %s", err)
+	}
+
+	var prettyJSON bytes.Buffer
+	err = json.Indent(&prettyJSON, []byte(*sarifOutput), "", "\t")
+	if err != nil {
+		t.Fatalf("error indenting output, err: %s", err)
+	}
+	cupaloy.SnapshotT(t, prettyJSON.String())
+}
