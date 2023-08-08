64c7bcefcdb2dd95f8089897489faa10af1d0c5e
elsapet <elizabeth@bearer.sh>
2023-08-08 16:59:01 +0200

    feat: add migrate command

--- pkg/commands/ignore.go
+++ pkg/commands/ignore.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 
 	"github.com/bearer/bearer/pkg/flag"
+	"github.com/bearer/bearer/pkg/util/file"
 	"github.com/bearer/bearer/pkg/util/ignore"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var IgnoreFlags = &flag.Flags{
-	IgnoreFlagGroup: flag.NewIgnoreFlagGroup(),
+	IgnoreFlagGroup:  flag.NewIgnoreFlagGroup(),
+	GeneralFlagGroup: flag.NewGeneralFlagGroup(),
 }
 
 func NewIgnoreCommand() *cobra.Command {
@@ -26,6 +29,7 @@ func NewIgnoreCommand() *cobra.Command {
 	cmd.AddCommand(
 		newIgnoreShowCommand(),
 		newIgnoreAddCommand(),
+		newIgnoreMigrateCommand(),
 	)
 
 	return cmd
@@ -112,4 +116,69 @@ $ bearer ignore add <fingerprint> --author Mish --comment "Possible false positi
 	cmd.SetUsageTemplate(fmt.Sprintf(scanTemplate, IgnoreFlags.Usages(cmd)))
 
 	return cmd
+}
+
+func newIgnoreMigrateCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "migrate",
+		Short: "Migrate ignored fingerprints from bearer.yml to bearer.ignore",
+		Example: `  # Migrate existing ignored (excluded) fingerprints from bearer.yml file to bearer.ignore
+$ bearer ignore migrate`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := IgnoreFlags.Bind(cmd); err != nil {
+				return fmt.Errorf("flag bind error: %w", err)
+			}
+
+			options, err := IgnoreFlags.ToOptions(args)
+			if err != nil {
+				return fmt.Errorf("flag error: %s", err)
+			}
+
+			ignoredFingerprintsFromConfig, err := getIgnoredFingerprintsFromConfig(args)
+			if err != nil {
+				return err
+			}
+
+			if err = ignore.AddToIgnoreFile(ignoredFingerprintsFromConfig, options.IgnoreOptions.Force); err != nil {
+				target := &ignore.DuplicateIgnoredFingerprintError{}
+				if errors.As(err, &target) {
+					// handle expected error (duplicate entry in bearer.ignore)
+					cmd.Printf("%s\n", err.Error())
+					return nil
+				}
+				return err
+			}
+
+			return nil
+		},
+		SilenceErrors: false,
+		SilenceUsage:  false,
+	}
+	IgnoreFlags.AddFlags(cmd)
+	cmd.SetUsageTemplate(fmt.Sprintf(scanTemplate, IgnoreFlags.Usages(cmd)))
+
+	return cmd
+}
+
+func getIgnoredFingerprintsFromConfig(args []string) (ignoredFingerprintsFromConfig map[string]ignore.IgnoredFingerprint, err error) {
+	ignoredFingerprintsFromConfig = make(map[string]ignore.IgnoredFingerprint)
+
+	configPath := viper.GetString(flag.ConfigFileFlag.ConfigName)
+	var defaultConfigPath = ""
+	if len(args) > 0 {
+		defaultConfigPath = file.GetFullFilename(args[0], configPath)
+	}
+
+	if err := readConfig(configPath); err != nil {
+		if err := readConfig(defaultConfigPath); err != nil {
+			return ignoredFingerprintsFromConfig, err
+		}
+		return ignoredFingerprintsFromConfig, err
+	}
+
+	for _, fingerprint := range viper.GetStringSlice("report.exclude-fingerprint") {
+		ignoredFingerprintsFromConfig[fingerprint] = ignore.IgnoredFingerprint{}
+	}
+
+	return ignoredFingerprintsFromConfig, nil
 }
