023b7bc8da7c07706a8618dd058f83909f8a5e2d
elsapet <elizabeth@bearer.sh>
2023-08-17 15:03:55 +0200

    fix: prefer sprintf to concatenation

--- scripts/gen-doc-yaml.go
+++ scripts/gen-doc-yaml.go
@@ -50,7 +50,7 @@ func writeDocs(cmd *cobra.Command, dir string) error {
 	basename := "bearer.yaml"
 
 	if cmd.CommandPath() != "" {
-		basename = "bearer" + strings.Replace(cmd.CommandPath(), " ", "_", -1) + ".yaml"
+		basename = fmt.Sprintf("bearer%s.yaml", strings.Replace(cmd.CommandPath(), " ", "_", -1))
 	}
 
 	filename := filepath.Join(dir, basename)
