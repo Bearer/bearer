3665a73f4729347d4aca8bb32e007feb64d117d7
elsapet <elizabeth@bearer.sh>
2023-09-14 13:29:48 +0200

    fix: bearer ignore path when target is a file (#1260)
    * fix: bearer ignore path when target is a file
    
    * docs: update to remove default flag value
    
    * fix: do not append target to given ignore path

--- internal/util/ignore/ignore.go
+++ internal/util/ignore/ignore.go
@@ -18,35 +18,21 @@ import (
 	pointer "github.com/bearer/bearer/internal/util/pointers"
 )
 
-func GetIgnoredFingerprints(bearerIgnoreFilePath string, target *string) (ignoredFingerprints map[string]types.IgnoredFingerprint, fileExists bool, err error) {
-	if bearerIgnoreFilePath == "" {
-		// nothing to do here
-		return ignoredFingerprints, false, err
-	}
+const DefaultIgnoreFilepath = "bearer.ignore"
 
-	if target != nil {
-		targetPath := ""
-		if targetPath, err = filepath.Abs(*target); err != nil {
-			return ignoredFingerprints, fileExists, err
-		}
-		bearerIgnoreFilePath = filepath.Join(targetPath, bearerIgnoreFilePath)
-	}
-
-	info, err := os.Stat(bearerIgnoreFilePath)
+func GetIgnoredFingerprints(ignoreFilePath string, target *string) (ignoredFingerprints map[string]types.IgnoredFingerprint, fileExists bool, err error) {
+	ignorePath, isDefaultPath, fileExists, err := getIgnoreFilePath(ignoreFilePath, target)
 	if err != nil {
-		if os.IsNotExist(err) {
-			// file does not exist : expected scenario
-			err = nil
+		if isDefaultPath && !fileExists {
+			// bearer.ignore file does not exist: expected scenario
+			return map[string]types.IgnoredFingerprint{}, false, nil
 		}
-		return make(map[string]types.IgnoredFingerprint), false, err
-	}
 
-	if info.IsDir() {
-		return ignoredFingerprints, false, fmt.Errorf("bearer-ignore-file path %s is a dir not a file", bearerIgnoreFilePath)
+		return ignoredFingerprints, fileExists, err
 	}
 
 	// file exists
-	content, err := os.ReadFile(bearerIgnoreFilePath)
+	content, err := os.ReadFile(ignorePath)
 	if err != nil {
 		return ignoredFingerprints, true, err
 	}
@@ -144,4 +130,74 @@ func GetAuthor() (*string, error) {
 	}
 
 	return pointer.String(strings.TrimSuffix(string(nameBytes), "\n")), nil
+}
+
+func getIgnoreFilePath(ignoreFilePath string, target *string) (
+	ignorePath string,
+	isDefaultPath bool,
+	fileExists bool,
+	err error,
+) {
+	if ignoreFilePath == "" {
+		// use default ignore file path
+		isDefaultPath = true
+		ignoreFilePath = DefaultIgnoreFilepath
+	}
+
+	_, err = os.Stat(ignoreFilePath)
+	if err == nil {
+		// file is found (all good)
+		return ignoreFilePath, isDefaultPath, true, err
+	}
+	fileNotFoundErr := os.IsNotExist(err)
+	if !isDefaultPath || !fileNotFoundErr {
+		// custom ignore file is not found (fail early)
+		// or unexpected error has occurred
+		return ignoreFilePath, isDefaultPath, fileExists, err
+	}
+
+	// file not found
+	fileExists = false
+
+	// append default path to target path and try again
+	targetPath, targetErr := targetPath(target)
+	if targetErr != nil {
+		return "", isDefaultPath, fileExists, targetErr
+	}
+
+	ignoreFilePath = filepath.Join(targetPath, ignoreFilePath)
+	info, err := os.Stat(ignoreFilePath)
+	if err != nil {
+		return "", isDefaultPath, fileExists, err
+	}
+
+	if info.IsDir() {
+		return "", isDefaultPath, fileExists, fmt.Errorf("ignore file path %s is a dir not a file", ignoreFilePath)
+	}
+
+	return ignoreFilePath, isDefaultPath, fileExists, nil
+}
+
+// returns target directory from target
+func targetPath(target *string) (string, error) {
+	if target == nil {
+		return "", nil
+	}
+
+	targetPath, err := filepath.Abs(*target)
+	if err != nil {
+		return "", err
+	}
+
+	info, err := os.Stat(targetPath)
+	if err != nil {
+		return "", err
+	}
+
+	if info.IsDir() {
+		return targetPath, nil
+	}
+
+	// not a directory
+	return filepath.Dir(targetPath), nil
 }
