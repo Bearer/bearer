23584ec67334ec12ae77d9e69d7054dbe4aa3593
gotbadger <p.j.h@hey.com>
2024-02-27 13:13:44 +0000

    fix(dataflow): yaml error key was named incorrectly

--- internal/report/output/dataflow/types/file_errors.go
+++ internal/report/output/dataflow/types/file_errors.go
@@ -3,5 +3,5 @@ package types
 type Error struct {
 	Type     string `json:"type" yaml:"type"`
 	Filename string `json:"filename" yaml:"filename"`
-	Error    string `json:"error" yaml:"filename"`
+	Error    string `json:"error" yaml:"error"`
 }
