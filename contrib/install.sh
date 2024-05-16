84098f0e60069108ccff90f1e7cd7cec5da25741
Philip Hayton <gotbadger@users.noreply.github.com>
2024-05-16 11:20:51 +0100

    Revert "ci: add linux arm64 build target" (#1598)
    Revert "ci: add linux arm64 build target (#1596)"
    
    This reverts commit 6277cf1d00264773468b7f4c3d2b311076c6abdb.

--- contrib/install.sh
+++ contrib/install.sh
@@ -57,7 +57,6 @@ get_binaries() {
     darwin/arm64) BINARIES="bearer" ;;
     linux/386) BINARIES="bearer" ;;
     linux/amd64) BINARIES="bearer" ;;
-    linux/arm64) BINARIES="bearer" ;;
     *)
       log_crit "platform $PLATFORM is not supported.  Make sure this script is up-to-date and file request at https://github.com/${PREFIX}/issues/new"
       exit 1
