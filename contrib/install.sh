44d807e3c057a94fdddffe80a2bffd424c78b81e
Philip Hayton <gotbadger@users.noreply.github.com>
2024-05-16 14:01:30 +0100

    ci: update linux builds (#1600)
    * ci: reapply "ci: add linux arm64 build target" (#1598)
    
    This reverts commit 84098f0e60069108ccff90f1e7cd7cec5da25741.
    
    * ci: drop i386

--- contrib/install.sh
+++ contrib/install.sh
@@ -55,8 +55,8 @@ get_binaries() {
   case "$PLATFORM" in
     darwin/amd64) BINARIES="bearer" ;;
     darwin/arm64) BINARIES="bearer" ;;
-    linux/386) BINARIES="bearer" ;;
     linux/amd64) BINARIES="bearer" ;;
+    linux/arm64) BINARIES="bearer" ;;
     *)
       log_crit "platform $PLATFORM is not supported.  Make sure this script is up-to-date and file request at https://github.com/${PREFIX}/issues/new"
       exit 1
