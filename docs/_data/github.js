292f365dda11a396acc8c09a1fde44402b7f504d
Philip Hayton <gotbadger@users.noreply.github.com>
2024-07-15 15:08:31 +0100

    ci: provide version info in docs (#1652)

--- docs/_data/github.js
+++ docs/_data/github.js
@@ -6,6 +6,7 @@ module.exports = async function () {
   //   type: "json",
   // });
   let release = {}
+  let rulesRelease = {}
   try {
     release = await EleventyFetch(
       "https://api.github.com/repos/bearer/bearer/releases/latest",
@@ -16,12 +17,32 @@ module.exports = async function () {
     )
   } catch (err) {
     console.log("Could not fetch release")
+    if (process.env.ELEVENTY_PRODUCTION) {
+      throw err
+    }
+  }
+  try {
+    rulesRelease = await EleventyFetch(
+      "https://api.github.com/repos/bearer/bearer-rules/releases/latest",
+      {
+        duration: "60m",
+        type: "json",
+      },
+    )
+  } catch (err) {
+    console.log("Could not fetch rulesRelease")
+    if (process.env.ELEVENTY_PRODUCTION) {
+      throw err
+    }
   }
   return {
     // stargazers: repo.stargazers_count,
     release: {
       name: release.tag_name || "DEV",
       url: release.html_url || "/",
+    },
+    rules: {
+      name: rulesRelease.tag_name || "DEV_RULES",
     },
   }
 }
