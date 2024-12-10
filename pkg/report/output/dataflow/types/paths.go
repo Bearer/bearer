cc82a8f83b26055fbd7f60ecfed6ea269a8851a8
Cédric Fabianski <cedric@bearer.sh>
2024-12-10 13:17:18 +0530

    feat: add http method to export (#1725)

--- pkg/report/output/dataflow/types/paths.go
+++ pkg/report/output/dataflow/types/paths.go
@@ -9,6 +9,7 @@ type Detection struct {
 	FullFilename string   `json:"full_filename" yaml:"full_filename"`
 	FullName     string   `json:"full_name" yaml:"full_name"`
 	LineNumber   *int     `json:"line_number" yaml:"line_number"`
-	Path         string   `json:"path" yam:"path"`
+	Path         string   `json:"path" yaml:"path"`
+	HttpMethod   string   `json:"http_method" yaml:"http_method"`
 	Urls         []string `json:"urls" yaml:"urls"`
 }
